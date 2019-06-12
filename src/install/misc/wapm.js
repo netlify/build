d6a0d577ee76cdc55ff86bc915b2a5da4386e921
DavidWells <hello@davidwells.io>
2019-06-12 15:52:41 -0700

    add wapm logic

--- src/install/misc/wapm.js
+++ src/install/misc/wapm.js
@@ -1,4 +1,47 @@
+const path = require('path')
+const execa = require('execa')
+const moveCache = require('../utils/moveCache')
+const { fileExists } = require('../utils/fs')
 
+// https://github.com/netlify/build-image/blob/9e0f207a27642d0115b1ca97cd5e8cebbe492f63/run-build-functions.sh#L558-L572
+module.exports = async function installWapm(cwd, cacheDir) {
+  const wapmConfig = path.join(cwd, 'wapm.toml')
+  const wapmLock = path.join(cwd, 'wapm.lock')
+
+  if (await fileExists(wapmConfig) || await fileExists(wapmLock)) {
+    // source $HOME/.wasmer/wasmer.sh
+    try {
+      // @ TODO verify $HOME works here
+      await execa('source', ['$HOME/.wasmer/wasmer.sh'])
+    } catch (err) {
+      console.log('wasmer not found')
+      console.log(err)
+      process.exit(1)
+    }
+
+    // restore_home_cache ".wasmer/cache" "wasmer cache"
+    await moveCache(
+      path.join(cacheDir, '.wasmer/cache'),
+      path.join(cwd, '.wasmer/cache'),
+      'restoring cached wasmer cache'
+    )
+    // restore_cwd_cache "wapm_packages" "wapm packages"
+    await moveCache(
+      path.join(cacheDir, '.wasmer/cache'),
+      path.join(cwd, '.wasmer/cache'),
+      'restoring cached wasmer cache'
+    )
+
+    try {
+      await execa('wapm', ['install'])
+    } catch (err) {
+      console.log('Error during Wapm install')
+      console.log(err)
+      process.exit(1)
+    }
+    console.log('Wapm packages installed')
+  }
+}
 
 /*
 # WAPM version
