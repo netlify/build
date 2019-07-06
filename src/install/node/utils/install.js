e6a61ff4b2feb96538966979da76a7d06d63c02d
DavidWells <hello@davidwells.io>
2019-07-05 17:25:12 -0700

    add Todo for `npm ci`

--- src/install/node/utils/install.js
+++ src/install/node/utils/install.js
@@ -9,6 +9,9 @@ module.exports = async function installWithYarnOrNpm(name, cwd) {
 
   const [use, cmd] = (hasYarnLock) ? ['yarn', 'add'] : ['npm', 'install']
 
+  // TODO check for package-lock. for `npm ci`
+  // https://hackernoon.com/how-to-speed-up-continuous-integration-build-with-new-npm-ci-and-package-lock-json-7647f91751a
+
   try {
     await execa(use, [cmd, name])
   } catch (err) {
