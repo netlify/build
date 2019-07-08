3d7d9fa2252e58a59d54a28a1804203b43e72ce9
DavidWells <hello@davidwells.io>
2019-07-08 14:43:34 -0700

    add heuristics checker

---
+++ src/heuristics/utils/has-deps.js
@@ -0,0 +1,10 @@
+
+function hasDependency(name, pkg) {
+  const { dependencies, devDependencies } = pkg
+  const hasItInDeps = dependencies && dependencies[name]
+  const hasItInDevDeps = devDependencies && devDependencies[name]
+  if (!hasItInDeps && !hasItInDevDeps) {
+    return false
+  }
+  return true
+}
