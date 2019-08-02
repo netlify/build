09f056eeeeab4bb8e352d6761d6aef4e3dc59bc6
DavidWells <hello@davidwells.io>
2019-08-02 13:22:44 -0700

    feat: add working sitemap plugin

--- packages/netlify-plugin-sitemap/index.js
+++ packages/netlify-plugin-sitemap/index.js
@@ -1,9 +1,64 @@
+/**
+ * Generates sitemap
+ */
+const fs = require('fs')
+const path = require('path')
+const sm = require('sitemap')
+const globby = require('globby')
 
-function netlifySitemapPlugin(conf) {
+async function makeSitemap(opts = {}) {
+  const { distPath, fileName, homepage } = opts
+  const htmlFiles = `${distPath}/**/**.html`
+  const paths = await globby([ htmlFiles ])
+  const urls = paths.map((file) => {
+    const regex = new RegExp(`^${distPath}`)
+    const urlPath = file.replace(regex, '')
+    return {
+      url: urlPath,
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmodrealtime: true,
+      lastmodfile: file
+    }
+  })
+  console.log('homepage', homepage)
+  const options = {
+    hostname: `${homepage.replace(/\/$/, '')}/`,
+    cacheTime: 600000, // 600 sec cache period
+    urls
+  }
+  // Creates a sitemap object given the input configuration with URLs
+  const sitemap = sm.createSitemap(options)
+  // Generates XML with a callback function
+  sitemap.toXML((error) => {
+    if (error) {
+      throw error
+    }
+  })
+  // Gives you a string containing the XML data
+  const xml = sitemap.toString()
+  // write sitemap to file
+  const sitemapFileName = fileName || 'sitemap.xml'
+  const sitemapFile = path.join(distPath, sitemapFileName)
+  fs.writeFileSync(sitemapFile, xml, 'utf-8')
+  console.log('Sitemap Built!', sitemapFile)
+}
+
+function netlifySitemapPlugin(conf = {}) {
   return {
     // Hook into lifecycle
-    postbuild: () => {
-      console.log('Build finished. Create sitemap from files')
+    postbuild: async () => {
+      if (!conf.homepage) {
+        throw new Error('Sitemap plugin missing homepage value')
+      }
+      if (!conf.distPath) {
+        throw new Error('Sitemap plugin missing build directory')
+      }
+      console.log('Creating sitemap from files...')
+      await makeSitemap({
+        homepage: conf.homepage,
+        distPath: conf.distPath
+      })
     }
   }
 }
