2dc7229955fe33561f0f61a879c12cdad1af3e9d
DavidWells <hello@davidwells.io>
2019-08-02 13:53:36 -0700

    feat: add working lighthouse plugin

--- packages/netlify-plugin-lighthouse/index.js
+++ packages/netlify-plugin-lighthouse/index.js
@@ -1,10 +1,23 @@
+const execa = require('execa')
+const path = require('path')
 
 function netlifyLighthousePlugin(conf) {
   return {
     // Hook into lifecycle
-    postdeploy: () => {
-      console.log('Deploy finished.')
-      console.log('Score site with lighthouse and save results')
+    postdeploy: async () => {
+      const lighthouseCI = path.join(__dirname, 'node_modules', '.bin', 'lighthouse-ci')
+      let resp
+      try {
+        const subprocess = execa(lighthouseCI, ['https://netlify.com'], {
+          shell: true
+        })
+        subprocess.stderr.pipe(process.stderr)
+        subprocess.stdout.pipe(process.stdout)
+        resp = await subprocess
+      } catch (err) {
+        console.log(err)
+      }
+      // console.log(resp.stdout)
     }
   }
 }
