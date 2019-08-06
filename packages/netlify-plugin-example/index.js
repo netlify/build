72ee2be129c5c855232aeea5fe9b044624ee9e10
DavidWells <hello@davidwells.io>
2019-08-06 14:11:50 -0700

    refactor: remove store ref

--- packages/netlify-plugin-example/index.js
+++ packages/netlify-plugin-example/index.js
@@ -1,38 +1,12 @@
 
-function netlifySitemapPlugin(conf, createStore) {
-  // you can create a simple KV store for yourself if you need one 
-  // API: https://github.com/sindresorhus/conf#usage
-  const store = createStore({
-    // totally optional options.
-    // you may want to set defaults, schema and other options
-    // https://github.com/sindresorhus/conf#options
-    defaults: {
-      foo: {
-        num: 1234,
-        str: '1234',
-        bool: false
-      }
-    }
-  })
-
+function netlifySitemapPlugin(conf) {
   return {
     // Hook into lifecycle
     init: () => {
-      console.log('Getting str from store: ', store.get('foo.str'))
-      console.log('Getting num from store: ', store.get('foo.num'))
-      const rand = Math.floor(Math.random() * 100)
-      console.log('Setting random number: ', rand)
-      store.set('randTimes100', rand * 100)
-      store.set('foo.num', rand)
-      console.log('Getting str from store: ', store.get('foo.str'))
-      console.log('Getting num from store: ', store.get('foo.num'))
+      console.log('init')
     },
     finally: () => {
-      // get nested key and value
-      console.log('Getting str from store: ', store.get('foo.str'))
-      console.log('Getting randTimes100 from store: ', store.get('randTimes100'))
-      console.log('Getting num from store: ', store.get('foo.num'))
-      console.log('Build finished. Create sitemap from files')
+      console.log('finally')
     }
   }
 }
