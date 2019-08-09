b4c1c2ab45296349f1bb2646e693180a6b6f2861
DavidWells <hello@davidwells.io>
2019-08-09 15:52:44 -0700

    feat: automatically resolve config path

--- packages/build-core/src/exec-build.js
+++ packages/build-core/src/exec-build.js
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
-
+const chalk = require('chalk')
 const build = require('./build')
+const hasConfig = require('./utils/hasConfig')
 
-build().catch((e) => {
+async function execBuild() {
+  console.log(chalk.greenBright.bold(`Starting Netlify Build`))
+  console.log()
+  // Automatically resolve the config path
+  const configPath = await hasConfig(process.cwd())
+
+  console.log(chalk.cyanBright.bold(`Using config file:`))
+  console.log(configPath)
+  console.log()
+  // Then run build lifecycle
+  build(configPath).catch((e) => {
+    console.log(e)
+  })
+}
+
+execBuild().catch((e) => {
   console.log(e)
 })
