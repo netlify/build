1bc375f85f531b25ce03c731450fd051b20c196a
DavidWells <hello@davidwells.io>
2019-08-10 16:17:54 -0700

    refactor: update build plugin

--- packages/cli-command/src/commands/build.js
+++ packages/cli-command/src/commands/build.js
@@ -1,21 +1,26 @@
 const Command = require('@netlify/cli-utils')
 const { flags } = require('@oclif/command')
-// const build = require('@netlify/build')
+const build = require('@netlify/build')
+
+function getConfigPath() {}
 
 class BuildCommand extends Command {
   async run() {
-    let { flags } = this.parse(BuildCommand);
-    const { api, site, config } = this.netlify;
+    let { flags } = this.parse(BuildCommand)
 
-    await build()
+    const pathToConfig = getConfigPath()
+    await build(pathToConfig, flags).catch((e) => {
+      console.log('error', e)
+    })
   }
 }
 
-BuildCommand.description = `Run netlify build`;
+BuildCommand.description = `Run netlify build`
 
-DeployCommand.flags = {
+BuildCommand.flags = {
   dry: flags.boolean({
     description: 'Dry run build and dont execute commands'
   }),
 }
-module.exports = BuildCommand;
+
+module.exports = BuildCommand
