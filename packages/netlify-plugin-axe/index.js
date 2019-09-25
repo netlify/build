4c4c66e5234421473c0b2f0a965b3e8f1060a352
ehmicky <ehmicky@gmail.com>
2019-09-25 18:27:25 +0200

    Fix axe plugin use of Execa

--- packages/netlify-plugin-axe/index.js
+++ packages/netlify-plugin-axe/index.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 const execa = require('execa')
 const mkdirp = require('mkdirp')
 
@@ -13,28 +11,17 @@ function netlifyAxePlugin(conf /* createStore */) {
     /* Run axe on post deploy */
     postdeploy: async () => {
       const site = conf.site || process.env.SITE
-      /* TODO fetch previous scores from cache */
-      const axeCLI = path.join(__dirname, 'node_modules', '.bin', 'axe')
-      let resp
-      try {
-        const subprocess = execa(axeCLI, [site, axeFlags, `--save .axe-results/result.json`], {
-          shell: true
-        })
-        subprocess.stderr.pipe(process.stderr)
-        subprocess.stdout.pipe(process.stdout)
-        resp = await subprocess
-      } catch (err) {
-        console.log(err)
-      }
-      // console.log({resp})
 
-      if (resp && resp.exitCodeName === 'SUCCESS') {
+      await execa(`axe ${site} ${axeFlags} --save .axe-results/result.json`, {
+        stdio: 'inherit',
+        preferLocal: true
+      })
+
         let results = require(`${process.cwd()}/.axe-results/result.json`)
         if (results && results[0]) {
           results = results[0].violations
         }
         console.log({ violations: results })
-      }
     }
   }
 }
