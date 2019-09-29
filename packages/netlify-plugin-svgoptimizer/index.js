df2ef58906e2fd092a3a13cce8617d498f72f8c0
Sarah Drasner <sarah.drasner@gmail.com>
2019-09-29 12:20:13 -0600

    add in the plugin

---
+++ packages/netlify-plugin-svgoptimizer/index.js
@@ -0,0 +1,140 @@
+const fs = require('fs')
+const path = require('path')
+const pkg = require('./package.json')
+const svgo = require('svgo')
+
+const svgoinit = new svgo({
+  plugins: [
+    {
+      cleanupAttrs: true
+    },
+    {
+      removeDoctype: true
+    },
+    {
+      removeXMLProcInst: true
+    },
+    {
+      removeComments: true
+    },
+    {
+      removeMetadata: true
+    },
+    {
+      removeTitle: true
+    },
+    {
+      removeDesc: true
+    },
+    {
+      removeUselessDefs: true
+    },
+    {
+      removeEditorsNSData: true
+    },
+    {
+      removeEmptyAttrs: true
+    },
+    {
+      removeHiddenElems: true
+    },
+    {
+      removeEmptyText: true
+    },
+    {
+      removeEmptyContainers: true
+    },
+    {
+      removeViewBox: false
+    },
+    {
+      cleanupEnableBackground: true
+    },
+    {
+      convertStyleToAttrs: true
+    },
+    {
+      convertColors: true
+    },
+    {
+      convertPathData: true
+    },
+    {
+      convertTransform: true
+    },
+    {
+      removeUnknownsAndDefaults: true
+    },
+    {
+      removeNonInheritableGroupAttrs: true
+    },
+    {
+      removeUselessStrokeAndFill: true
+    },
+    {
+      removeUnusedNS: true
+    },
+    {
+      cleanupIDs: true
+    },
+    {
+      cleanupNumericValues: true
+    },
+    {
+      moveElemsAttrsToGroup: true
+    },
+    {
+      moveGroupAttrsToElems: true
+    },
+    {
+      collapseGroups: true
+    },
+    {
+      removeRasterImages: false
+    },
+    {
+      mergePaths: false
+    },
+    {
+      convertShapeToPath: true
+    },
+    {
+      sortAttrs: true
+    },
+    {
+      removeDimensions: true
+    }
+  ]
+})
+
+function netlifyPlugin(config) {
+  return {
+    init: config => {
+      const srcDirectory = config.pluginConfig.src.directory
+      if (!srcDirectory) return console.log(`No src found in ${pkg.name} plugin config`)
+      const directoryPath = path.join(__dirname, `../../${srcDirectory}`)
+
+      //scanning the directory and then reading each file
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) console.log(`Unable to scan directory: ${err}`)
+
+        files.forEach(function(file) {
+          const filePath = path.join(directoryPath, file)
+          fs.readFile(filePath, 'utf8', function(err, data) {
+            if (err) console.log(err)
+
+            svgoinit.optimize(data, { path: filePath }).then(result => {
+              //take the file and rewrite it with the new optimized SVG
+              fs.writeFile(filePath, result.data, err => {
+                if (err) throw err
+                console.log('SVG optimized correctly!')
+              })
+            })
+          })
+        })
+      })
+    }
+  }
+}
+
+module.exports = netlifyPlugin
