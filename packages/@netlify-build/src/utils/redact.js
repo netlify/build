ae20ae0a357cccaaf704ba93f148fc2fbee5624f
ehmicky <ehmicky@gmail.com>
2019-09-30 19:13:01 +0200

    Improve secrets redaction

--- packages/@netlify-build/src/utils/redact.js
+++ packages/@netlify-build/src/utils/redact.js
@@ -1,36 +1,16 @@
 const stream = require('stream')
 
 const redactEnv = require('redact-env')
-const isPlainObject = require('is-plain-obj')
-
-function isObject(value) {
-  var type = typeof value
-  return value != null && (type === 'object' || type === 'function')
-}
+// TODO: npm uninstall es6-week-map after
+// https://github.com/mcmath/deep-map/pull/15 is merged
+const deepMap = require('deep-map')
 
 function getSecrets(secretKeys) {
   return redactEnv.build(secretKeys)
 }
 
 function redactValues(target, secrets) {
-  // If it's not an object or string then it's a primitive. Nothing to redact.
-  if (!isObject(target) && typeof target !== 'string') {
-    return target
-  }
-  // Redact string values
-  if (typeof target === 'string') {
-    return redactEnv.redact(target, secrets)
-  }
-  // Redact Array values
-  if (Array.isArray(target)) {
-    return target.map(val => redactValues(val, secrets))
-  } else if (isPlainObject(target)) {
-    return Object.keys(target).reduce((newObj, key) => {
-      newObj[key] = redactValues(target[key], secrets)
-      return newObj
-    }, {})
-  }
-  return target
+  return deepMap(target, value => redactEnv.redact(value, secrets))
 }
 
 function redactStream(secrets) {
