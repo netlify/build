6d5a0b17908e5039d07751e467e49e728d8478c1
ehmicky <ehmicky@gmail.com>
2019-10-11 13:21:16 +0200

    feat (build): improve CLI

--- packages/build/src/core/bin.js
+++ packages/build/src/core/bin.js
@@ -1,17 +1,69 @@
 #!/usr/bin/env node
-const { argv } = require('process')
-
-const minimist = require('minimist')
-const omit = require('omit.js')
+const yargs = require('yargs')
+const filterObj = require('filter-obj')
 
 require('../utils/polyfills')
 
 const build = require('./main')
 
-async function runCli() {
-  const options = minimist(argv.slice(2))
-  const optionsA = omit(options, ['_'])
+// CLI entry point
+const runCli = async function() {
+  const options = parseArgs()
+  const optionsA = filterObj(options, isUserOption)
   await build(optionsA)
 }
+
+const parseArgs = function() {
+  return yargs
+    .options(OPTIONS)
+    .usage(USAGE)
+    .strict()
+    .parse()
+}
+
+const OPTIONS = {
+  config: {
+    string: true,
+    describe: `Path to the configuration file.
+Defaults to any netlify.yml, netlify.toml, netlify.json or netlify.js file in
+the current directory or any parent directory`
+  },
+  token: {
+    string: true,
+    describe: `Netlify API token for authentication.
+The NETLIFY_TOKEN environment variable can be used as well.`
+  },
+  context: {
+    string: true,
+    describe: `Build context.
+Default: 'production'`
+  },
+  dry: {
+    boolean: true,
+    describe: `Run in dry mode, i.e. printing commands without
+executing them.
+Default: false`
+  },
+  verbose: {
+    boolean: true,
+    describe: `Print messages and errors verbosely.
+Default: false`
+  }
+}
+
+const USAGE = `$0 [OPTIONS...]
+
+Run Netlify Build system locally.
+
+Options can also be specified as environment variables prefixed with
+NETLIFY_BUILD_. For example the environment variable NETLIFY_BUILD_DRY=true can
+be used instead of the CLI flag --dry.`
+
+// Remove `yargs`-specific options, shortcuts and dash-cased
+const isUserOption = function(key, value) {
+  return value !== undefined && !INTERNAL_KEYS.includes(key) && key.length !== 1 && !key.includes('-')
+}
+
+const INTERNAL_KEYS = ['help', 'version', '_', '$0']
 
 runCli()
