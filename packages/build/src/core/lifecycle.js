872bec5c4f1a153196dd4070c72950b72bb103d1
DavidWells <hello@davidwells.io>
2019-10-11 14:21:14 -0700

    docs: update lifecycle docs

--- packages/build/src/core/lifecycle.js
+++ packages/build/src/core/lifecycle.js
@@ -1,61 +1,63 @@
 
 const LIFECYCLE = [
   /**
-   *  init - Runs before anything else
+   * `init` - Runs before anything else
    */
   'init',
   /**
-   * getCache - Fetch previous build cache
+   * `getCache` - Fetch previous build cache
    */
   'preGetCache',
   'getCache',
   'postGetCache',
   /**
-   * install - Install project dependancies
+   * `install` - Install project dependancies
    */
   'preInstall',
   'install',
   'postInstall',
   /**
-   * preBuild - runs before functions & build commands run
+   * `preBuild` - runs before functions & build commands run
    */
   'preBuild',
   /**
-   * functionsBuild - build the serverless functions
+   * `functionsBuild` - build the serverless functions
    */
   'preFunctionsBuild',
   'functionsBuild',
   'postFunctionsBuild',
   /**
-   * build - build commands run
+   * `build` - build commands run
    */
   'build',
+  /**
+   * `postBuild` - Runs after site & functions have been built
+   */
   'postBuild',
   /**
-   * package - Package & optimize artifact
+   * `package` - Package & optimize artifact
    */
   'prePackage',
   'package',
   'postPackage',
   /**
-   * deploy - Deploy built artifact
+   * `preDeploy` - Deploy built artifact
    */
   'preDeploy',
-  'deploy',
-  'postDeploy',
+
+  // 'deploy', Not currently active
+  // 'postDeploy', Not currently active
   /**
-   * saveCache - Save cached assets *
+   * `saveCache` - Save cached assets
    */
   'preSaveCache',
   'saveCache',
   'postSaveCache',
   /**
-   * finally - Runs after anything else
+   * `finally` - Runs after anything else
    */
   'finally',
-  /**
-   * Todo onError is not part of lifecycle. Its a special handler
-   */
+  // Todo onError is not part of lifecycle. Its a special handler
   'onError'
 ]
 
