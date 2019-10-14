7721e5cc6f6024fe6f9d2821c113e5b36c7edca8
DavidWells <hello@davidwells.io>
2019-10-13 22:52:53 -0700

    refactor: add telemetry

---
+++ packages/build/src/utils/telemetry/index.js
@@ -0,0 +1,20 @@
+const Analytics = require('analytics').default
+
+const pkg = require('../../../package.json')
+
+const plugins = require('./plugins')
+
+/* If BUILD_TELEMETRY_DISBALED, disable api calls */
+const activePlugins = (process.env.BUILD_TELEMETRY_DISBALED) ? [] : plugins
+
+/* If DEBUG true, disable telemetry api calls */
+const DEBUG_ENABLED = true
+
+const telemetry = Analytics({
+  app: 'netlifyCI',
+  debug: DEBUG_ENABLED,
+  version: pkg.version,
+  plugins: activePlugins
+})
+
+module.exports = telemetry
---
+++ packages/build/src/utils/telemetry/plugins.js
@@ -0,0 +1,55 @@
+const pkg = require('../../../package.json')
+const isNetlifyCI = require('../is-netlify-ci')
+
+const sendData = require('./api')
+
+/* automatically prefix event names */
+const prefixEventNames = {
+  NAMESPACE: 'prefixer',
+  trackStart: ({ payload, instance }) => {
+    const { event } = payload
+    const prefix = `${instance.getState('context.app')}:`
+    const prefixedEvent = event.startsWith(prefix) ? event : `${prefix}${event}`
+    return {
+      ...payload,
+      ...{
+        event: prefixedEvent
+      }
+    }
+  }
+}
+
+/* enrich telemetry payload */
+const enrichPayload = {
+  NAMESPACE: 'enrich',
+  trackStart: ({ payload, instance }) => {
+    console.log('payload', payload)
+    return {
+      ...payload,
+      ...{
+        properties: Object.assign({}, payload.properties, {
+          isNetlifyCI: isNetlifyCI()
+        }),
+      }
+    }
+  }
+}
+
+/* Sent data to telemetry service, if telemetry enabled */
+const netlifyTelemetry = {
+  NAMESPACE: 'netlify-telemetry',
+  track: ({ payload, instance }) => {
+    if (instance.getState('context.debug')) {
+      console.log(`send`, payload)
+      return
+    }
+    sendData(payload, pkg.version)
+  }
+}
+
+/* Return analytic plugins */
+module.exports = [
+  prefixEventNames,
+  enrichPayload,
+  netlifyTelemetry
+]
