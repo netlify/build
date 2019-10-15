1208f8ce0a92eb21de7c168700e0ce8a9bb59303
DavidWells <hello@davidwells.io>
2019-10-14 19:22:12 -0700

    docs: add example one site with yml config

---
+++ examples/example-one/functions/one.js
@@ -0,0 +1,9 @@
+
+exports.handler = (event, context, callback) => {
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      hello: 'from function one'
+    })
+  })
+}
---
+++ examples/example-one/plugins/example/index.js
@@ -0,0 +1,36 @@
+
+// Export plugin
+module.exports = function netlifyPlugin(config) {
+  return {
+    init: () => {
+      console.log('Hi from init')
+    },
+    getCache: () => {
+      console.log('Hi from getCache')
+    },
+    install: () => {
+      console.log('Hi from install')
+    },
+    preBuild: () => {
+      console.log('Hi from preBuild')
+    },
+    build: () => {
+      console.log('Hi from build')
+    },
+    postBuild: () => {
+      console.log('Hi from postBuild')
+    },
+    package: () => {
+      console.log('Hi from package')
+    },
+    preDeploy: () => {
+      console.log('Hi from preDeploy')
+    },
+    saveCache: () => {
+      console.log('Hi from saveCache')
+    },
+    finally: () => {
+      console.log('Hi from finally')
+    },
+  }
+}
