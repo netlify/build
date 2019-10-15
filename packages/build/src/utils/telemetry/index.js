007b42d3c70ada074148f65084d1dab5ab09b131
ehmicky <ehmicky@gmail.com>
2019-10-15 12:49:49 +0200

    fix (build): fix typo in environment variable `DISBALED` -> `DISABLED`

--- packages/build/src/utils/telemetry/index.js
+++ packages/build/src/utils/telemetry/index.js
@@ -4,8 +4,8 @@ const pkg = require('../../../package.json')
 
 const plugins = require('./plugins')
 
-/* If BUILD_TELEMETRY_DISBALED, disable api calls */
-const activePlugins = process.env.BUILD_TELEMETRY_DISBALED ? [] : plugins
+/* If BUILD_TELEMETRY_DISABLED, disable api calls */
+const activePlugins = process.env.BUILD_TELEMETRY_DISABLED ? [] : plugins
 
 /* If DEBUG true, disable telemetry api calls */
 const DEBUG_ENABLED = false
