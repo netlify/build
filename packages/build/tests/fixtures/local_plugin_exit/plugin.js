2472de97356f0343d6cf604558020c0a0e896f2e
ehmicky <ehmicky@gmail.com>
2019-11-04 13:28:23 +0100

    Fix test

--- packages/build/tests/fixtures/local_plugin_exit/plugin.js
+++ packages/build/tests/fixtures/local_plugin_exit/plugin.js
@@ -1,7 +1,12 @@
+const { promisify } = require('util')
+
+const pSetTimeout = promisify(setTimeout)
+
 module.exports = {
   name: 'netlify-plugin-test',
-  init() {
+  async init() {
     process.kill(process.pid)
+    await pSetTimeout(1e4)
   },
   build() {
     console.log('test')
