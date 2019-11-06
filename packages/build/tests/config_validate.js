ac6193df481e44476b4d04d9f756780d4f9a11cc
ehmicky <ehmicky@gmail.com>
2019-11-06 15:30:13 +0100

    Add test when user specify the old plugins syntax

--- packages/build/tests/config_validate.js
+++ packages/build/tests/config_validate.js
@@ -5,3 +5,7 @@ const { runFixture } = require('./helpers/main')
 test('build.command + build.lifecycle', async t => {
   await runFixture(t, 'config_validate_old_command')
 })
+
+test('Old config.plugins syntax', async t => {
+  await runFixture(t, 'config_validate_old_plugins')
+})
