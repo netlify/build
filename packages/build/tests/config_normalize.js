93dfb84030c15a3a7b47fe72e35ed23e8d4e26ba
ehmicky <ehmicky@gmail.com>
2019-11-06 15:24:08 +0100

    Add more tests about config normalization

--- packages/build/tests/config_normalize.js
+++ packages/build/tests/config_normalize.js
@@ -2,6 +2,18 @@ const test = require('ava')
 
 const { runFixture } = require('./helpers/main')
 
-test('Allow prebuild instead of preBuild', async t => {
+test('prebuild instead of preBuild', async t => {
   await runFixture(t, 'config_normalize_case')
+})
+
+test('Multiline commands', async t => {
+  await runFixture(t, 'config_normalize_multiline')
+})
+
+test('Array commands', async t => {
+  await runFixture(t, 'config_normalize_array')
+})
+
+test('build.command', async t => {
+  await runFixture(t, 'config_normalize_command')
 })
