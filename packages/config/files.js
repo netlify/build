a5bc2de1a48e5d22e9578f9118171feaa5f0be59
ehmicky <ehmicky@gmail.com>
2019-11-06 12:17:13 +0100

    Refactor configuration paths

--- packages/config/files.js
+++ packages/config/files.js
@@ -1,23 +1,35 @@
 const { resolve } = require('path')
 
-const { get, set } = require('dot-prop')
+const { get, set, delete: deleteProp } = require('dot-prop')
 
 const handleFiles = function(config, baseDir) {
-  return PATHS.reduce(normalizePath.bind(null, baseDir), config)
+  return FILES.reduce(handleFile.bind(null, baseDir), config)
+}
+
+const handleFile = function(baseDir, config, { path }) {
+  const value = get(config, path)
+
+  const valueA = normalizePath(value, baseDir)
+
+  if (valueA === undefined) {
+    deleteProp(config, path)
+  } else {
+    set(config, path, valueA)
+  }
+
+  return config
 }
 
 // Resolve paths relatively to the config file.
 // Also normalize paths to OS-specific path delimiters.
-const normalizePath = function(baseDir, config, path) {
-  const value = get(config, path)
-
-  if (typeof value !== 'string') {
-    return config
+const normalizePath = function(value, baseDir) {
+  if (value === undefined) {
+    return value
   }
 
-  return set(config, path, resolve(baseDir, value))
+  return resolve(baseDir, value)
 }
 
-const PATHS = ['build.publish', 'build.functions']
+const FILES = [{ path: 'build.publish' }, { path: 'build.functions' }]
 
 module.exports = { handleFiles }
