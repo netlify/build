08c1d92570f26e4384314ed9250bbea68f49a160
ehmicky <ehmicky@gmail.com>
2019-11-08 20:17:58 +0100

    Add configuration validation

--- packages/build/tests/config/validate/tests.js
+++ packages/build/tests/config/validate/tests.js
@@ -2,10 +2,74 @@ const test = require('ava')
 
 const { runFixture } = require('../../helpers/main')
 
-test('build.command + build.lifecycle', async t => {
-  await runFixture(t, 'old_command')
+test('plugins: not array', async t => {
+  await runFixture(t, 'plugins_not_array')
 })
 
-test('Old config.plugins syntax', async t => {
-  await runFixture(t, 'old_plugins')
+test('plugins: not array of objects', async t => {
+  await runFixture(t, 'plugins_not_objects')
+})
+
+test('plugins.*: unknown property', async t => {
+  await runFixture(t, 'plugins_unknown')
+})
+
+test('plugins.*.id: string', async t => {
+  await runFixture(t, 'plugins_id_string')
+})
+
+test('plugins.*.type: required', async t => {
+  await runFixture(t, 'plugins_type_required')
+})
+
+test('plugins.*.type: string', async t => {
+  await runFixture(t, 'plugins_type_string')
+})
+
+test('plugins.*.enabled: boolean', async t => {
+  await runFixture(t, 'plugins_enabled_boolean')
+})
+
+test('plugins.*.config: object', async t => {
+  await runFixture(t, 'plugins_config_object')
+})
+
+test('build: object', async t => {
+  await runFixture(t, 'build_object')
+})
+
+test('build.publish: string', async t => {
+  await runFixture(t, 'build_publish_string')
+})
+
+test('build.functions: string', async t => {
+  await runFixture(t, 'build_functions_string')
+})
+
+test('build.command: string', async t => {
+  await runFixture(t, 'build_command_string')
+})
+
+test('build.command: array of strings', async t => {
+  await runFixture(t, 'build_command_array')
+})
+
+test('build.command: not with lifecycle', async t => {
+  await runFixture(t, 'build_command_no_lifecycle')
+})
+
+test('build.lifecycle: object', async t => {
+  await runFixture(t, 'build_lifecycle_object')
+})
+
+test('build.lifecycle: hook names', async t => {
+  await runFixture(t, 'build_lifecycle_hooks')
+})
+
+test('build.lifecycle: string', async t => {
+  await runFixture(t, 'build_lifecycle_hooks_string')
+})
+
+test('build.lifecycle: array', async t => {
+  await runFixture(t, 'build_lifecycle_hooks_array')
 })
--- packages/config/src/validate.js
+++ packages/config/src/validate.js
@@ -1,34 +1,106 @@
-// Validate the configuration object
+const indentString = require('indent-string')
+const isPlainObj = require('is-plain-obj')
+const { dump, JSON_SCHEMA } = require('js-yaml')
+
+const { VALIDATIONS } = require('./validations')
+
+// Validate the configuration file.
+// Performed before normalization.
 const validateConfig = function(config) {
-  validateOldCommand(config)
-  warnPluginArray(config)
-}
-
-const validateOldCommand = function({ build: { command, lifecycle } = {} }) {
-  if (command !== undefined && lifecycle !== undefined) {
-    throw new Error(
-      `'build.command' and 'build.lifecycle' are both defined in the configuration. Please rename 'build.command' to 'build.lifecycle.build'`,
-    )
+  try {
+    VALIDATIONS.forEach(({ property, ...validation }) => {
+      const propertyA = property.split('.')
+      validateProperty(config, { ...validation, property: propertyA })
+    })
+  } catch (error) {
+    error.cleanStack = true
+    throw error
   }
 }
 
-// TODO: remove after beta testing is over
-const warnPluginArray = function({ plugins }) {
-  if (plugins !== undefined && !Array.isArray(plugins)) {
-    throw new Error(`Attention beta testers!
-Plugins have been changed from an object to an array.
-Please update your plugins property accordingly.
+// Validate a single property in the configuration file.
+const validateProperty = function(
+  parent,
+  { property: [propName, nextPropName, ...property], propPath = propName, required, check, message, example },
+) {
+  const value = parent[propName]
 
-plugins:
-  - type: npm-module-path
-    config:
-      fizz: pop
-  - type: ./plugin/path
-    config:
-      foo: bar
-
-See more information in the docs http://bit.ly/31z46mF`)
+  if (nextPropName !== undefined) {
+    return validateChild({ value, nextPropName, property, propPath, required, check, message, example })
   }
+
+  if (value === undefined) {
+    return checkRequired({ value, required, propPath, example })
+  }
+
+  if (check !== undefined && check(value, parent)) {
+    return
+  }
+
+  throw new Error(`Configuration property '${propPath}' ${message}
+${getExample(value, example)}`)
+}
+
+// Recurse over children (each part of the `property` array).
+// Can use * to recurse over array|object elements.
+const validateChild = function({ value, nextPropName, property, propPath, example, ...rest }) {
+  if (value === undefined) {
+    return
+  }
+
+  if (nextPropName !== '*') {
+    return validateProperty(value, {
+      property: [nextPropName, ...property],
+      propPath: `${propPath}.${nextPropName}`,
+      example,
+      ...rest,
+    })
+  }
+
+  if (!Array.isArray(value) && !isPlainObj(value)) {
+    throw new Error(`Configuration property '${propPath}' must be an array or a plain object.
+${getExample(value, example)}`)
+  }
+
+  return Object.keys(value).forEach(childProp => {
+    const propPathA = Array.isArray(value) ? `${propPath}[${childProp}]` : `${propPath}.${childProp}`
+    const childPropA = Array.isArray(value) ? Number(childProp) : childProp
+    validateProperty(value, {
+      property: [childPropA, ...property],
+      propPath: propPathA,
+      example,
+      ...rest,
+    })
+  })
+}
+
+// When `required` is `true`, property must be defined, unless its parent is
+// `undefined`. To make parent required, set its `required` to `true` as well.
+const checkRequired = function({ value, required, propPath, example }) {
+  if (!required) {
+    return
+  }
+
+  throw new Error(`Configuration property '${propPath}' is required.
+${getExample(value, example)}`)
+}
+
+const getExample = function(value, example) {
+  return `
+Invalid value:
+
+${indentString(serializeValue(value), 2)}
+
+Example 'netlify.yml':
+${indentString(example, 2)}`
+}
+
+const serializeValue = function(value) {
+  if (value === undefined) {
+    return String(value)
+  }
+
+  return dump(value, { schema: JSON_SCHEMA, noRefs: true }).trim()
 }
 
 module.exports = { validateConfig }
