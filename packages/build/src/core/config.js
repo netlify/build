507e2462c464f5a2a0c05a8a903377605f988e16
DavidWells <hello@davidwells.io>
2019-11-18 12:04:31 -0800

    refactor: set default opts

--- packages/build/src/core/config.js
+++ packages/build/src/core/config.js
@@ -1,7 +1,5 @@
 const { dirname } = require('path')
-const {
-  env: { NETLIFY_TOKEN },
-} = require('process')
+const { NETLIFY_TOKEN, CONTEXT } = require('process').env
 
 const resolveConfig = require('@netlify/config')
 const { getConfigPath } = require('@netlify/config')
@@ -11,17 +9,24 @@ const { logOptions, logConfigPath } = require('../log/main')
 
 // Retrieve configuration object
 const loadConfig = async function(options) {
-  const { token = NETLIFY_TOKEN, config, cwd, context } = options
-  const opts = omit(options, ['token', 'dry', 'cwd'])
+  const optsWithDefaults = Object.assign({}, {
+    ...(CONTEXT && { context: CONTEXT }),
+    ...(NETLIFY_TOKEN && { token: NETLIFY_TOKEN })
+  }, options)
+  const opts = omit(optsWithDefaults, ['token'])
   logOptions(opts)
 
-  const configPath = await getConfigPath(config, cwd)
+  const configPath = await getConfigPath(options.config, options.cwd)
   logConfigPath(configPath)
   const baseDir = dirname(configPath)
 
   try {
-    const config = await resolveConfig(configPath, { cwd, context })
-    return { config, configPath, token, baseDir }
+    const config = await resolveConfig(configPath, opts)
+    return {
+      config,
+      configPath,
+      token: optsWithDefaults.token,
+      baseDir }
   } catch (error) {
     error.message = `Netlify configuration error:\n${error.message}`
     throw error
