1cedd670c5d76ca6effc3da6806c2ab55cf56c64
ehmicky <ehmicky@gmail.com>
2019-11-22 12:07:38 +0100

    Improve path logging

--- packages/build/src/plugins/child/constants.js
+++ packages/build/src/plugins/child/constants.js
@@ -1,4 +1,4 @@
-const { resolve } = require('path')
+const { relative } = require('path')
 const { tmpdir } = require('os')
 const {
   env: { DEPLOY_ID },
@@ -14,17 +14,21 @@ const getConstants = function({
     build: { publish, functions },
   },
 }) {
-  const cacheDir = getCacheDir(baseDir)
-  const functionsDist = getFunctionsDist(baseDir)
+  const configPathA = normalizePath(configPath, baseDir)
+  const publishA = normalizePath(publish, baseDir)
+  const cacheDir = getCacheDir()
+  const functionsSrc = normalizePath(functions, baseDir)
+  const functionsDist = getFunctionsDist()
+
   return {
     /**
      * Path to the netlify configuration file
      */
-    CONFIG_PATH: configPath,
+    CONFIG_PATH: configPathA,
     /**
      * The build directory of the site
      */
-    BUILD_DIR: publish,
+    BUILD_DIR: publishA,
     /**
      * The directory files can be cached in between builds
      */
@@ -32,7 +36,7 @@ const getConstants = function({
     /**
      * The directory where function source code lives
      */
-    FUNCTIONS_SRC: functions,
+    FUNCTIONS_SRC: functionsSrc,
     /**
      * The directory where built serverless functions are placed before deployment
      */
@@ -40,23 +44,34 @@ const getConstants = function({
   }
 }
 
-const getCacheDir = function(baseDir) {
+// The current directory is `baseDir`. Most constants are inside this `baseDir`.
+// Instead of passing absolute paths, we pass paths relative to `baseDir`, so
+// that logs are less verbose.
+const normalizePath = function(path, baseDir) {
+  if (path === undefined) {
+    return
+  }
+
+  return relative(baseDir, path)
+}
+
+const getCacheDir = function() {
   if (isNetlifyCI()) {
     return CI_CACHE_DIR
   }
 
-  return resolve(baseDir, LOCAL_CACHE_DIR)
+  return LOCAL_CACHE_DIR
 }
 
 const CI_CACHE_DIR = '/opt/build/cache/'
 const LOCAL_CACHE_DIR = '.netlify/cache/'
 
-const getFunctionsDist = function(baseDir) {
+const getFunctionsDist = function() {
   if (isNetlifyCI()) {
     return `${tmpdir()}/zisi-${DEPLOY_ID}`
   }
 
-  return resolve(baseDir, LOCAL_FUNCTIONS_DIST)
+  return LOCAL_FUNCTIONS_DIST
 }
 
 const LOCAL_FUNCTIONS_DIST = '.netlify/functions/'
