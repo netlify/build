249c414fcb0b2a6b7dd5d141fd60b5689584ff48
ehmicky <ehmicky@gmail.com>
2019-11-25 13:55:37 +0100

    Fix TOML serialization

--- packages/build/src/core/toml.js
+++ packages/build/src/core/toml.js
@@ -1,7 +1,7 @@
 const { writeFile } = require('fs')
 const { promisify } = require('util')
 
-const { formatUtils } = require('@netlify/config')
+const { stringify } = require('@iarna/toml')
 
 const { logTomlWrite } = require('../log/main')
 
@@ -9,7 +9,7 @@ const pWriteFile = promisify(writeFile)
 
 // Write TOML file to support current buildbot
 const tomlWrite = async function(config, baseDir) {
-  const toml = formatUtils.toml.dump(config)
+  const toml = stringify(config)
   const tomlPath = `${baseDir}/netlify.toml`
   await pWriteFile(tomlPath, toml)
 
