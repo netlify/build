d9ee743fce87fa787cc3dcd0b655e282d6e58ab9
ehmicky <ehmicky@gmail.com>
2019-11-27 14:25:56 +0100

    Add API call test

---
+++ packages/build/tests/plugins/api/fixtures/api_call/plugin.js
@@ -0,0 +1,16 @@
+const nock = require('nock')
+
+module.exports = {
+  name: 'netlify-plugin-test',
+  scopes: ['listSites'],
+  async init({ api, api: { scheme, host, pathPrefix } }) {
+    const scope = nock(`${scheme}://${host}`)
+    scope.get(`${pathPrefix}/sites`).reply(200, [{ id: 'test' }])
+
+    const sites = await api.listSites('https://example.com')
+    console.log(sites)
+
+    scope.done()
+    nock.restore()
+  },
+}
