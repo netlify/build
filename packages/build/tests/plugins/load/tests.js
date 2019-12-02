35818981db56b9dd37c8fb6c9e691c9c6264a03f
ehmicky <ehmicky@gmail.com>
2019-12-02 15:48:42 +0100

    Rename tests

--- packages/build/tests/plugins/load/tests.js
+++ packages/build/tests/plugins/load/tests.js
@@ -2,15 +2,15 @@ const test = require('ava')
 
 const { runFixture } = require('../../helpers/main')
 
-test('Can use local plugins', async t => {
+test('Local plugins', async t => {
   await runFixture(t, 'local')
 })
 
-test('Can use Node module plugins', async t => {
+test('Node module plugins', async t => {
   await runFixture(t, 'module')
 })
 
-test('Reports missing plugins', async t => {
+test('Missing plugins', async t => {
   await runFixture(t, 'missing')
 })
 
@@ -18,19 +18,19 @@ test('Plugin.id is optional', async t => {
   await runFixture(t, 'optional_id')
 })
 
-test('Can override plugins', async t => {
+test('Override plugins', async t => {
   await runFixture(t, 'override')
 })
 
-test('Handles top-level errors', async t => {
+test('Top-level errors', async t => {
   await runFixture(t, 'error_top')
 })
 
-test('Handles top function errors', async t => {
+test('Top function errors', async t => {
   await runFixture(t, 'error_function')
 })
 
-test('Handles process warnings', async t => {
+test('Process warnings', async t => {
   await runFixture(t, 'error_warning')
 })
 
