f807f74b088dc56a029e682b0838c7faaee9d7a6
ehmicky <ehmicky@gmail.com>
2019-12-03 13:32:49 +0100

    Add global binary test

--- packages/build/tests/core/lifecycle/tests.js
+++ packages/build/tests/core/lifecycle/tests.js
@@ -2,6 +2,10 @@ const test = require('ava')
 
 const { runFixture } = require('../../helpers/main')
 
+test('Lifecycle commands can execute global binaries', async t => {
+  await runFixture(t, 'global_bin')
+})
+
 test('Lifecycle commands can execute local binaries', async t => {
   await runFixture(t, 'local_bin')
 })
