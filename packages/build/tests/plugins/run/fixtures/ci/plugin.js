940eefcb04edda65d765f63d800b71565eb947cf
ehmicky <ehmicky@gmail.com>
2019-12-04 17:55:55 +0100

    Improve fixture

--- packages/build/tests/plugins/run/fixtures/ci/plugin.js
+++ packages/build/tests/plugins/run/fixtures/ci/plugin.js
@@ -2,8 +2,10 @@ module.exports = {
   name: 'netlify-plugin-test',
   init() {
     console.log('a'.repeat(1e3))
+    console.error('b'.repeat(1e3))
   },
   build() {
-    console.log('b'.repeat(1e3))
+    console.log('c'.repeat(1e3))
+    console.error('d'.repeat(1e3))
   },
 }
