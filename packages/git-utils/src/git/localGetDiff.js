f75ed8bfbb6b1d0e06dc9c38a4c040b6b3618887
DavidWells <hello@davidwells.io>
2019-12-13 16:26:36 -0800

    refactor: add error handler

--- packages/git-utils/src/git/localGetDiff.js
+++ packages/git-utils/src/git/localGetDiff.js
@@ -8,6 +8,7 @@ const localGetDiff = (base, head) => {
   return new Promise(done => {
     const args = ['diff', `${base}...${head}`]
     let stdout = ''
+    let stdErr = ''
     const child = spawn('git', args, { env: process.env })
     d('> git', args.join(' '))
     child.stdout.on('data', chunk => {
@@ -15,11 +16,18 @@ const localGetDiff = (base, head) => {
     })
     child.stderr.on('data', data => {
       console.error(`Could not get diff from git between ${base} and ${head}`)
+      stdErr += data.toString()
       throw new Error(data.toString())
     })
     child.on('close', function(code) {
       if (code === 0) {
         done(stdout)
+      }
+    })
+    child.on('error', error => {
+      stdErr += error.toString()
+      if (stdErr) {
+        console.log('git diff error', stdErr)
       }
     })
   })
