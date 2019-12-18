c4684f0d48654d792bbae8d69c687cf2b5f505ca
ehmicky <ehmicky@gmail.com>
2019-12-18 12:24:30 +0100

    Fix test

--- packages/build/tests/plugins/utils-exec/fixtures/defined/plugin.js
+++ packages/build/tests/plugins/utils-exec/fixtures/defined/plugin.js
@@ -1,6 +1,11 @@
 module.exports = {
   name: 'netlify-plugin-test',
   onInit({ utils: { exec } }) {
-    console.log(typeof exec, Object.keys(exec).join(' '))
+    console.log(
+      typeof exec,
+      Object.keys(exec)
+        .sort()
+        .join(' '),
+    )
   },
 }
