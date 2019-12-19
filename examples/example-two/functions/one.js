1f367fda3ad9789eaf37917ddf9efd610707b79d
ehmicky <ehmicky@gmail.com>
2019-12-19 12:20:09 +0100

    Update HTML

--- examples/example-two/functions/one.js
+++ examples/example-two/functions/one.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
 module.exports.handler = async function(event, context) {
-  return { statusCode: 200, body: String(Date.now()) }
+  return { statusCode: 200, body: `console.log('Timestamp: ' + ${Date.now()})` }
 }
--- examples/example-two/gulpfile.js
+++ examples/example-two/gulpfile.js
@@ -2,7 +2,7 @@
 const { src, dest } = require('gulp')
 
 const build = function() {
-  return src('src').pipe(dest('build'))
+  return src('src/**').pipe(dest('build'))
 }
 
 module.exports = { build }
