100fe4cfb9461240bc785a00cf3cee78ad94e64f
ehmicky <ehmicky@gmail.com>
2019-12-19 12:12:12 +0100

    Add example directory

---
+++ examples/example-two/gulpfile.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line node/no-unpublished-require
+const { src, dest } = require('gulp')
+
+const build = function() {
+  return src('src').pipe(dest('build'))
+}
+
+module.exports = { build }
