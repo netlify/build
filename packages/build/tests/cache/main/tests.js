d7ed798a410eeb3a26326e59fbe56272bf7017e2
ehmicky <ehmicky@gmail.com>
2019-12-19 21:06:48 +0100

    Remove comments

--- packages/build/tests/cache/main/tests.js
+++ packages/build/tests/cache/main/tests.js
@@ -8,10 +8,6 @@ test('Cache local', async t => {
   await runFixture(t, 'local', { env: { TEST_CACHE_PATH: 'bower_components' } })
 })
 
-// This works on Windows locally but not inside GitHub actions
-// TODO: figure out why
-// if (platform !== 'win32') {
 test('Cache CI', async t => {
   await runFixture(t, 'ci', { env: { TEST_CACHE_PATH: 'bower_components', DEPLOY_PRIME_URL: 'test' } })
 })
-// }
