e9782c9d7a5159009bf5b2b4d7b087cc57211dee
ehmicky <ehmicky@gmail.com>
2019-12-20 16:14:17 +0100

    Simplify code

--- packages/build/src/plugins/child/logic.js
+++ packages/build/src/plugins/child/logic.js
@@ -1,8 +1,8 @@
 // Require the plugin file and fire its top-level function.
 // The returned object is the `logic` which includes all event handlers.
-const getLogic = function({ pluginPath, pluginConfig }, constants) {
+const getLogic = function({ pluginPath, pluginConfig }) {
   const logic = requireLogic(pluginPath)
-  const logicA = loadLogic({ logic, pluginConfig, constants })
+  const logicA = loadLogic({ logic, pluginConfig })
   return logicA
 }
 
@@ -15,13 +15,13 @@ const requireLogic = function(pluginPath) {
   }
 }
 
-const loadLogic = function({ logic, pluginConfig, constants }) {
+const loadLogic = function({ logic, pluginConfig }) {
   if (typeof logic !== 'function') {
     return logic
   }
 
   try {
-    return logic(pluginConfig, { constants })
+    return logic(pluginConfig)
   } catch (error) {
     error.message = `Error loading plugin:\n${error.message}`
     throw error
