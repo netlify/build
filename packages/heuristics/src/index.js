db0d096dcf619534a4592c4dfadcd275e4104ca3
Raees Iqbal <raees.bhatti@gmail.com>
2020-01-14 14:08:46 +0500

    Add Git repo detection

--- packages/heuristics/src/index.js
+++ packages/heuristics/src/index.js
@@ -1,7 +1,9 @@
 const detectProjectSettings = require('./detect-project')
 const detectFunctionsBuilder = require('./detect-functions-builder')
+const detectGitRepo = require('./detect-git-repo')
 
 module.exports = {
   detectProjectSettings,
   detectFunctionsBuilder,
+  detectGitRepo,
 }
