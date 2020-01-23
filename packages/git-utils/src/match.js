5ecb2f77643a7b8650fc9f6c469fd5081714ea58
ehmicky <ehmicky@gmail.com>
2020-01-23 12:16:53 +0100

    Fix fileMatch() and match()

--- packages/git-utils/src/match.js
+++ packages/git-utils/src/match.js
@@ -3,27 +3,14 @@ const mapObj = require('map-obj')
 
 // Return functions that return modified|created|deleted files filtered by a
 // globbing pattern
-const getMatchers = function({ modifiedFiles, createdFiles, deletedFiles }) {
+const fileMatch = function({ modifiedFiles, createdFiles, deletedFiles }, ...patterns) {
   const matchFiles = {
     modified: modifiedFiles,
     created: createdFiles,
     deleted: deletedFiles,
     edited: [...modifiedFiles, ...createdFiles],
   }
-  return {
-    fileMatch: fileMatch.bind(null, matchFiles),
-    match: match.bind(null, matchFiles),
-  }
+  return mapObj(matchFiles, (key, paths) => [key, micromatch(paths, patterns)])
 }
 
-// Return each list of files as a boolean (whether there is any match)
-const fileMatch = function(files, ...patterns) {
-  return mapObj(files, (key, paths) => [key, micromatch(paths, patterns).length !== 0])
-}
-
-// Return each list of files as an array of filepaths
-const match = function(files, ...patterns) {
-  return mapObj(files, (key, paths) => [key, micromatch(paths, patterns)])
-}
-
-module.exports = { getMatchers }
+module.exports = { fileMatch }
