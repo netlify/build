bf71e6ac8605f103adaba558265bc3fa3c08f004
Raees Iqbal <raees.bhatti@gmail.com>
2020-02-07 16:03:50 +0400

    heuristics: Make detectFunctionsBuilder async

--- packages/heuristics/src/detect-functions-builder.js
+++ packages/heuristics/src/detect-functions-builder.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const fs = require('fs')
+const util = require('util')
 
-module.exports = function detectFunctionsBuilder() {
-  const detectors = require('fs')
-    .readdirSync(path.join(__dirname, 'function-builder-detectors'))
+const readDirAsync = util.promisify(fs.readdir)
+
+module.exports = async function detectFunctionsBuilder() {
+  const detectors = (await readDirAsync(path.join(__dirname, 'function-builder-detectors')))
     .filter(x => x.endsWith('.js')) // only accept .js detector files
     .map(det => require(path.join(__dirname, `function-builder-detectors/${det}`)))
 
