195300f920b330c6d49ac6faa222b41baff6d673
ehmicky <ehmicky@gmail.com>
2020-02-13 21:00:05 +0100

    Fix git utility crashing when there is no git repository

--- packages/git-utils/src/main.js
+++ packages/git-utils/src/main.js
@@ -11,17 +11,21 @@ const getGitUtils = async function(
   // istanbul ignore next
   { base, cwd = getCwd() } = {},
 ) {
-  const properties = await addProperties(base, cwd)
-  const gitUtils = addMethods(properties)
-  return gitUtils
+  try {
+    const baseA = await getBase(base, cwd)
+    const properties = await getProperties(baseA, cwd)
+    const gitUtils = addMethods(properties)
+    return gitUtils
+  } catch (error) {
+    return getFakeGitUtils(error)
+  }
 }
 
-const addProperties = async function(base, cwd) {
-  const baseA = await getBase(base, cwd)
+const getProperties = async function(base, cwd) {
   const [{ modifiedFiles, createdFiles, deletedFiles }, commits, linesOfCode] = await Promise.all([
-    getDiffFiles(baseA, cwd),
-    getCommits(baseA, cwd),
-    getLinesOfCode(baseA, cwd),
+    getDiffFiles(base, cwd),
+    getCommits(base, cwd),
+    getLinesOfCode(base, cwd),
   ])
   return { modifiedFiles, createdFiles, deletedFiles, commits, linesOfCode }
 }
@@ -29,6 +33,26 @@ const addProperties = async function(base, cwd) {
 const addMethods = function(properties) {
   const fileMatchA = fileMatch.bind(null, properties)
   return { ...properties, fileMatch: fileMatchA }
+}
+
+// Initialization might fail for many reasons, including the repository not
+// having a `.git` directory or some `git` commands failing.
+// However initialization happens at the beginning of the build. At that stage,
+// we do not know whether the `git` utility will be used yet. In those cases,
+// we don't want to report any errors since the user might not use the utility.
+// However we still want to report errors when the user does use the utility.
+// We achieve this by using a Proxy.
+const getFakeGitUtils = function(error) {
+  return new Proxy(
+    // We define those so that `Object.keys()` and similar methods still work
+    { modifiedFiles: [], createdFiles: [], deletedFiles: [], commits: [], linesOfCode: 0, fileMatch() {} },
+    // Intercept any `git.*` referencing and throw the original initialization error instead.
+    {
+      get() {
+        throw error
+      },
+    },
+  )
 }
 
 module.exports = getGitUtils
