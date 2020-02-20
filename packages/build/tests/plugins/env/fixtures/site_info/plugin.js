d0e8163a74753c1c5f913d5979cf6abee9d4e8c7
ehmicky <ehmicky@gmail.com>
2020-02-20 20:33:10 +0100

    Fix test snapshots

--- packages/build/tests/plugins/env/fixtures/site_info/plugin.js
+++ packages/build/tests/plugins/env/fixtures/site_info/plugin.js
@@ -5,6 +5,6 @@ const {
 module.exports = {
   name: 'netlify-plugin-test',
   onInit({ constants: { SITE_ID } }) {
-    console.log({ SITE_ID, URL, REPOSITORY_URL })
+    console.log(JSON.stringify({ SITE_ID, URL, REPOSITORY_URL }, null, 2))
   },
 }
