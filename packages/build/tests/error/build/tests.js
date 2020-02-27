c15ad1bdf5ae7fc7140b8d2cdeaa6280486e6cc2
ehmicky <ehmicky@gmail.com>
2020-02-27 14:52:56 +0100

    Add tests

--- packages/build/tests/error/build/tests.js
+++ packages/build/tests/error/build/tests.js
@@ -6,8 +6,16 @@ test('build.fail()', async t => {
   await runFixture(t, 'fail')
 })
 
+test('build.fail() error option', async t => {
+  await runFixture(t, 'fail_error_option')
+})
+
 test('build.cancel()', async t => {
   await runFixture(t, 'cancel')
+})
+
+test('build.cancel() error option', async t => {
+  await runFixture(t, 'cancel_error_option')
 })
 
 test('exception', async t => {
