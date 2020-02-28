35b24434582dfb57ac6b6aa150b40fc950a36082
ehmicky <ehmicky@gmail.com>
2020-02-28 21:23:33 +0100

    Remove shareable configuration modules

--- packages/config/src/path.js
+++ packages/config/src/path.js
@@ -1,14 +1,10 @@
 const { resolve } = require('path')
-const { promisify } = require('util')
 const { cwd: getCwd } = require('process')
 
 const findUp = require('find-up')
-const resolvePath = require('resolve')
 const pathExists = require('path-exists')
 
 const { throwError } = require('./error')
-
-const pResolve = promisify(resolvePath)
 
 // Configuration location can be:
 //  - `undefined`, in which case `cwd` (default: current directory) is looked up
@@ -20,10 +16,6 @@ const getConfigPath = async function(configFile, cwd = getCwd()) {
     return getDefaultConfig(cwd)
   }
 
-  if (isNodeModule(configFile)) {
-    return getModuleConfig(configFile, cwd)
-  }
-
   return getLocalConfig(configFile, cwd)
 }
 
@@ -32,20 +24,6 @@ const getDefaultConfig = function(cwd) {
 }
 
 const FILENAMES = ['netlify.toml', 'netlify.yml', 'netlify.yaml', 'netlify.json']
-
-const isNodeModule = function(configFile) {
-  return configFile.startsWith('@') || !configFile.includes('/')
-}
-
-// We use `resolve` because `require()` should be relative to `baseDir` not to
-// this `__filename`
-const getModuleConfig = async function(configFile, cwd) {
-  try {
-    return await pResolve(configFile, { basedir: cwd })
-  } catch (error) {
-    throwError(`Configuration file does not exist: ${configFile}`, error)
-  }
-}
 
 const getLocalConfig = async function(configFile, cwd) {
   const configPath = resolve(cwd, configFile)
