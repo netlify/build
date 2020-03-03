e2c281ead9266e0f36fcbefcb7939406a9ebb1ae
ehmicky <ehmicky@users.noreply.github.com>
2020-03-03 23:37:33 +0100

    Fix `context` default value (#875)

--- packages/config/src/index.js
+++ packages/config/src/index.js
@@ -1,4 +1,7 @@
-const { cwd: getCwd } = require('process')
+const {
+  cwd: getCwd,
+  env: { CONTEXT },
+} = require('process')
 
 const { getConfigPath } = require('./path')
 const { getBaseDir } = require('./base_dir')
@@ -9,7 +12,7 @@ const { handleFiles } = require('./files')
 const { EVENTS, LEGACY_EVENTS } = require('./events')
 const { parseConfig } = require('./parse/main')
 
-const resolveConfig = async function(configFile, { cwd = getCwd() } = {}) {
+const resolveConfig = async function(configFile, { cwd = getCwd(), context = CONTEXT || 'production' } = {}) {
   const configPath = await getConfigPath(configFile, cwd)
 
   try {
@@ -23,7 +26,7 @@ const resolveConfig = async function(configFile, { cwd = getCwd() } = {}) {
 
     const configB = normalizeConfig(configA)
     const configC = await handleFiles(configB, baseDir)
-    return { configPath, baseDir, config: configC }
+    return { configPath, baseDir, config: configC, context }
   } catch (error) {
     const configMessage = configPath === undefined ? '' : ` file ${configPath}`
     error.message = `When resolving config${configMessage}:\n${error.message}`
