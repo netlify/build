9aa3269ff994ab52ce5c6ffd907085f838732377
ehmicky <ehmicky@gmail.com>
2020-03-03 18:57:48 +0100

    Validate that `base`, `publish` and `functions` are not outside the repository root directory

--- packages/config/src/validate/helpers.js
+++ packages/config/src/validate/helpers.js
@@ -1,3 +1,5 @@
+const { normalize } = require('path')
+
 const isString = function(value) {
   return typeof value === 'string'
 }
@@ -51,4 +53,22 @@ const identity = function(value) {
   return value
 }
 
-module.exports = { isString, isBoolean, validProperties, deprecatedProperties }
+// Ensure paths specified by users in the configuration file are not targetting
+// files outside the repository root directory.
+const isInsideRoot = function(path) {
+  return !normalize(path).startsWith('..')
+}
+
+const insideRootCheck = {
+  check: isInsideRoot,
+  message: 'must be inside the root directory.',
+}
+
+// Used in examples to show how to fix the above check
+const removeParentDots = function(path) {
+  return normalize(path).replace(PARENT_DOTS_REGEXP, '')
+}
+
+const PARENT_DOTS_REGEXP = /\.\.\//g
+
+module.exports = { isString, isBoolean, validProperties, deprecatedProperties, insideRootCheck, removeParentDots }
