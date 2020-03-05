bc9e0ed6ab8b15ccbd0e26e4c671c699418a2791
ehmicky <ehmicky@users.noreply.github.com>
2020-03-05 13:11:08 +0100

    Remove permissions checks on build directory (#879)
    * Remove permissions checks on `build.base`
    
    * Remove async/await

--- packages/config/src/base_dir.js
+++ packages/config/src/base_dir.js
@@ -1,37 +1,14 @@
 const { dirname } = require('path')
 const { cwd } = require('process')
-const {
-  access,
-  constants: { R_OK, W_OK },
-} = require('fs')
-const { promisify } = require('util')
-
-const { throwError } = require('./error')
-
-const pAccess = promisify(access)
 
 // Retrieve the base directory used to resolve most paths.
 // This is the configuration file's directory.
-const getBaseDir = async function(configPath) {
-  const baseDir = getBaseDirValue(configPath)
-  await validatePermissions(baseDir)
-  return baseDir
-}
-
-const getBaseDirValue = function(configPath) {
+const getBaseDir = function(configPath) {
   if (configPath === undefined) {
     return cwd()
   }
 
   return dirname(configPath)
-}
-
-const validatePermissions = async function(baseDir) {
-  try {
-    await pAccess(baseDir, R_OK | W_OK)
-  } catch (error) {
-    throwError(`Wrong permissions on the base directory ${baseDir}`)
-  }
 }
 
 module.exports = { getBaseDir }
