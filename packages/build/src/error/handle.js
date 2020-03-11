a2a9be3a95eb92801521cda5d7e15c959975b229
ehmicky <ehmicky@gmail.com>
2020-03-11 17:36:43 +0100

    Fix comment

--- packages/build/src/error/handle.js
+++ packages/build/src/error/handle.js
@@ -1,5 +1,5 @@
 // Handle top-level build errors.
-// Logging is done separtely.
+// Logging is done separately.
 const handleBuildError = async function() {}
 
 module.exports = { handleBuildError }
