22bc668bfec45a9f80c21506d3e3b57912d0dea1
ehmicky <ehmicky@gmail.com>
2020-03-24 23:14:17 +0100

    Improve tests of `git-utils`

--- packages/build/tests/utils/git/fixtures/defined/plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  onInit({ utils: { git } }) {
-    console.log(
-      Object.keys(git)
-        .sort()
-        .join(' '),
-    )
-  },
-}
--- packages/build/tests/utils/git/fixtures/lines/plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  onInit({
-    utils: {
-      git: { linesOfCode },
-    },
-  }) {
-    console.log({ linesOfCode })
-  },
-}
