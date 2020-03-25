8d0e99dec6eac8dc093e7ad3892e61f55b630bf3
ehmicky <ehmicky@users.noreply.github.com>
2020-03-25 15:52:45 +0100

    Improve tests of the `run` utility (#1045)

--- packages/build/tests/utils/run/fixtures/defined/plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  onInit({ utils: { run } }) {
-    console.log(
-      Object.keys(run)
-        .sort()
-        .join(' '),
-    )
-  },
-}
--- packages/build/tests/utils/run/fixtures/no_args/plugin.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  async onInit({ utils: { run } }) {
-    await run('echo')
-  },
-}
