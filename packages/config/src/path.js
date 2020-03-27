d8ff3f0e886c4be67ad81ffb9e869a178bed8d74
ehmicky <ehmicky@gmail.com>
2020-03-27 14:18:52 +0100

    Improve performance of configuration file load

--- packages/config/src/path.js
+++ packages/config/src/path.js
@@ -3,6 +3,7 @@ const { resolve, basename } = require('path')
 const findUp = require('find-up')
 const pathExists = require('path-exists')
 const pFilter = require('p-filter')
+const pLocate = require('p-locate')
 
 const { resolvePath } = require('./files')
 const { throwError } = require('./error')
@@ -13,27 +14,38 @@ const { throwError } = require('./error')
 //  - a `netlify.*` file in the `repositoryRoot`
 //  - a `netlify.*` file in the current directory or any parent
 const getConfigPath = async function({ configOpt, cwd, repositoryRoot, base }) {
-  if (configOpt !== undefined) {
-    return resolve(cwd, configOpt)
-  }
-
-  if (base !== undefined) {
-    const basePath = resolvePath(base, repositoryRoot)
-    const configPath = await searchConfigFile(basePath)
-    if (configPath !== undefined) {
-      return configPath
-    }
-  }
-
-  const configPathA = await searchConfigFile(repositoryRoot)
-  if (configPathA !== undefined) {
-    return configPathA
-  }
-
-  const configPathB = await findUp(FILENAMES, { cwd })
-  return configPathB
+  const configPath = await pLocate(
+    [
+      searchConfigOpt(cwd, configOpt),
+      searchBaseConfigFile(repositoryRoot, base),
+      searchConfigFile(repositoryRoot),
+      findUp(FILENAMES, { cwd }),
+    ],
+    Boolean,
+  )
+  return configPath
 }
 
+// --config CLI flag
+const searchConfigOpt = function(cwd, configOpt) {
+  if (configOpt === undefined) {
+    return
+  }
+
+  return resolve(cwd, configOpt)
+}
+
+// Look for `repositoryRoot/{base}/netlify.*`
+const searchBaseConfigFile = function(repositoryRoot, base) {
+  if (base === undefined) {
+    return
+  }
+
+  const basePath = resolvePath(repositoryRoot, base)
+  return searchConfigFile(basePath)
+}
+
+// Look for several file extensions for `netlify.*`
 const searchConfigFile = async function(cwd) {
   const paths = FILENAMES.map(filename => resolve(cwd, filename))
   const pathsA = await pFilter(paths, pathExists)
