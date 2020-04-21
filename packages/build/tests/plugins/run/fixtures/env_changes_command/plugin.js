40fff0db61d0df6fa10f1e90ca735bd5c1dbb5f2
ehmicky <ehmicky@gmail.com>
2020-04-21 21:26:36 +0200

    Propagate `process.env` between Build plugins and `build.command`

---
+++ packages/build/tests/plugins/run/fixtures/env_changes_command/plugin.js
@@ -0,0 +1,9 @@
+const { env } = require('process')
+
+module.exports = {
+  onPreBuild() {
+    env.TEST_ONE = 'one'
+    env.TEST_TWO = 'two'
+    delete env.LANGUAGE
+  },
+}
---
+++ packages/build/tests/plugins/run/fixtures/env_changes_plugin/pluginTwo.js
@@ -0,0 +1,13 @@
+const { env } = require('process')
+
+module.exports = {
+  onPreBuild() {
+    console.log(env.TEST_ONE, env.TEST_TWO, env.LANG)
+  },
+  onBuild() {
+    console.log(env.TEST_ONE, env.TEST_TWO, env.LANG)
+
+    delete env.TEST_ONE
+    env.TEST_TWO = 'twoChanged'
+  },
+}
