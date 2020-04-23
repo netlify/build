7532777372380c6f1c2a7685ee8b72d0edb3ba2c
ehmicky <ehmicky@gmail.com>
2020-04-23 18:46:43 +0200

    Load core plugin with `@netlify/build` Node.js version

--- packages/build/tests/core/flags/tests.js
+++ packages/build/tests/core/flags/tests.js
@@ -1,16 +1,17 @@
-const { execPath } = require('process')
-
 const test = require('ava')
+const getNode = require('get-node')
 
-const { removeDir } = require('../../helpers/dir')
 const { runFixture } = require('../../helpers/main')
-const { copyToTemp } = require('../../helpers/temp')
 
-test('--node-version success', async t => {
-  const { tempDir, tempFile } = await copyToTemp(execPath)
-  try {
-    await runFixture(t, 'node_version', { flags: `--node-path=${tempFile}`, env: { TEST_NODE_PATH: tempFile } })
-  } finally {
-    await removeDir(tempDir)
-  }
+const CHILD_NODE_VERSION = '8.3.0'
+const VERY_OLD_NODE_VERSION = '4.0.0'
+
+test('--node-path is used by build.command and Build plugins', async t => {
+  const { path } = await getNode(CHILD_NODE_VERSION)
+  await runFixture(t, 'node_version', { flags: `--node-path=${path}`, env: { TEST_NODE_PATH: path } })
+})
+
+test('--node-path is not used by core plugins', async t => {
+  const { path } = await getNode(VERY_OLD_NODE_VERSION)
+  await runFixture(t, 'core_plugin', { flags: `--node-path=${path}` })
 })
