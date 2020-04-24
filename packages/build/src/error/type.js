ef201b462acc4af354a69114c5e87f2ee165d783
ehmicky <ehmicky@gmail.com>
2020-04-24 20:09:01 +0200

    Add comments on error types

--- packages/build/src/error/type.js
+++ packages/build/src/error/type.js
@@ -14,20 +14,41 @@ const getTypeInfo = function(errorProps) {
 
 // List of error types, and their related properties
 const TYPES = {
-  resolveConfig: { header: 'Configuration error', stackType: 'none' },
-  dependencies: { header: 'Dependencies error', stackType: 'none' },
+  // User configuration error (`@netlify/config`)
+  resolveConfig: {
+    header: 'Configuration error',
+    stackType: 'none',
+  },
+
+  // User misconfigured a plugin
   pluginInput: {
     header: ({ location: { package, input } }) => `Invalid input "${input}" for plugin "${package}"`,
     stackType: 'none',
     getLocation: getBuildFailLocation,
   },
-  buildCommand: { header: 'Build failed', stackType: 'message', getLocation: getBuildCommandLocation },
-  failBuild: { header: 'Build failed', stackType: 'stack', getLocation: getBuildFailLocation },
+
+  // `build.command` non-0 exit code
+  buildCommand: {
+    header: 'Build failed',
+    stackType: 'message',
+    getLocation: getBuildCommandLocation,
+  },
+
+  // Plugin called `utils.build.failBuild()`
+  failBuild: {
+    header: 'Build failed',
+    stackType: 'stack',
+    getLocation: getBuildFailLocation,
+  },
+
+  // Plugin called `utils.build.failPlugin()`
   failPlugin: {
     header: ({ location: { package } }) => `Plugin "${package}" failed`,
     stackType: 'stack',
     getLocation: getBuildFailLocation,
   },
+
+  // Plugin called `utils.build.cancelBuild()`
   cancelBuild: {
     header: ({ location: { package } }) => `Build canceled by ${package}`,
     stackType: 'stack',
@@ -35,17 +56,15 @@ const TYPES = {
     isSuccess: true,
     shouldCancel: true,
   },
-  ipc: {
-    header: ({ location: { package } }) => `Plugin "${package}" internal error`,
-    stackType: 'none',
-    getLocation: getBuildFailLocation,
-  },
-  api: { header: 'API error', stackType: 'message', showErrorProps: true, getLocation: getApiLocation },
+
+  // Plugin has an invalid shape
   pluginValidation: {
     header: ({ location: { package } }) => `Plugin "${package}" internal error`,
     stackType: 'none',
     getLocation: getBuildFailLocation,
   },
+
+  // Plugin threw an uncaught exception
   pluginInternalError: {
     header: ({ location: { package } }) => `Plugin "${package}" internal error`,
     stackType: 'stack',
@@ -53,7 +72,35 @@ const TYPES = {
     rawStack: true,
     getLocation: getBuildFailLocation,
   },
-  internalError: { header: 'Core internal error', stackType: 'stack', showErrorProps: true, rawStack: true },
+
+  // Bug while orchestrating child processes
+  ipc: {
+    header: ({ location: { package } }) => `Plugin "${package}" internal error`,
+    stackType: 'none',
+    getLocation: getBuildFailLocation,
+  },
+
+  // Request error when `@netlify/build` was calling Netlify API
+  api: {
+    header: 'API error',
+    stackType: 'message',
+    showErrorProps: true,
+    getLocation: getApiLocation,
+  },
+
+  // Error while installing user packages (missing plugins, local plugins or functions dependencies)
+  dependencies: {
+    header: 'Dependencies error',
+    stackType: 'none',
+  },
+
+  // `@netlify/build` threw an uncaught exception
+  internalError: {
+    header: 'Core internal error',
+    stackType: 'stack',
+    showErrorProps: true,
+    rawStack: true,
+  },
 }
 // When no error type matches, it's an uncaught exception, i.e. a bug
 const DEFAULT_TYPE = 'internalError'
