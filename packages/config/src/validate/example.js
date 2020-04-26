9ee8911bacd1caa87a12634e928128d3f4456a51
ehmicky <ehmicky@users.noreply.github.com>
2020-04-26 05:53:34 -0700

    Improve `@netlify/config` validation messages (#1183)

--- packages/config/src/validate/example.js
+++ packages/config/src/validate/example.js
@@ -2,8 +2,8 @@ const indentString = require('indent-string')
 const tomlify = require('tomlify-j0.4')
 
 // Print invalid value and example netlify.toml
-const getExample = function({ value, parent, key, prevPath, example }) {
-  const exampleA = typeof example === 'function' ? example(value, key, parent, prevPath) : example
+const getExample = function({ value, key, prevPath, example }) {
+  const exampleA = typeof example === 'function' ? example(value, key, prevPath) : example
   return `
 Invalid syntax
 
