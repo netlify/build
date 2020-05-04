9ae94795aedebc3c81be7050fe0bcfb7ac7cc5b0
ehmicky <ehmicky@users.noreply.github.com>
2020-05-04 06:25:36 -0700

    Deprecate `netlify.yml` and `netlify.json` (#1171)

--- packages/config/src/parse/parsers.js
+++ packages/config/src/parse/parsers.js
@@ -1,5 +1,14 @@
+const { writeFile } = require('fs')
+const { basename, dirname, join } = require('path')
+const { promisify } = require('util')
+
+const { yellowBright } = require('chalk')
 const { load: loadYaml, JSON_SCHEMA } = require('js-yaml')
 const { parse: loadToml } = require('toml')
+
+const { serializeToml } = require('../utils/toml.js')
+
+const pWriteFile = promisify(writeFile)
 
 const parseYaml = function(configString) {
   return loadYaml(configString, { schema: JSON_SCHEMA, json: true })
@@ -14,6 +23,7 @@ const parseJson = function(configString) {
 }
 
 // List of parsers for each file extension
+// TODO: remove YAML and JSON after out of beta
 const PARSERS = {
   yaml: parseYaml,
   yml: parseYaml,
@@ -21,4 +31,39 @@ const PARSERS = {
   json: parseJson,
 }
 
-module.exports = { PARSERS }
+// YAML and JSON are deprecated.
+// We convert user's configuration file to `netlify.toml` so they don't have to
+// do it themselves.
+// TODO: remove after out of beta
+const fixBackwardCompat = async function(config, extension, configPath) {
+  if (extension === 'toml') {
+    return
+  }
+
+  console.warn(yellowBright(`netlify.${extension} is deprecated: please use netlify.toml instead.\n`))
+
+  const tomlConfig = getTomlConfig(config)
+  if (tomlConfig === undefined) {
+    return
+  }
+
+  const tomlConfigPath = getTomlConfigPath(configPath, extension)
+  await pWriteFile(tomlConfigPath, `${tomlConfig}\n`)
+}
+
+const getTomlConfig = function(config) {
+  try {
+    return serializeToml(config)
+    // Some YAML is not serializable to TOML. In that case, we silently give up
+    // trying to convert it
+  } catch (error) {
+    return
+  }
+}
+
+const getTomlConfigPath = function(configPath, extension) {
+  const filename = `${basename(configPath, `.${extension}`)}.toml`
+  return join(dirname(configPath), filename)
+}
+
+module.exports = { PARSERS, fixBackwardCompat }
--- packages/config/tests/parse/tests.js
+++ packages/config/tests/parse/tests.js
@@ -1,19 +1,49 @@
+const { readFile } = require('fs')
 const { platform, version } = require('process')
+const { promisify } = require('util')
 
 const test = require('ava')
+const del = require('del')
+const pathExists = require('path-exists')
 
 const { runFixture, FIXTURES_DIR } = require('../helpers/main')
 
+const pReadFile = promisify(readFile)
+
+// Test that we create a netlify.toml when using a netlify.yml|yaml|json locally
+const getNetlifyTomlPath = async function(fixture) {
+  const netlifyTomlPath = `${FIXTURES_DIR}/${fixture}/netlify.toml`
+
+  if (await pathExists(netlifyTomlPath)) {
+    await del(netlifyTomlPath, { force: true })
+  }
+
+  return netlifyTomlPath
+}
+
+const checkNetlifyToml = async function(t, netlifyTomlPath) {
+  t.true(await pathExists(netlifyTomlPath))
+  const netlifyTomlContent = await pReadFile(netlifyTomlPath, 'utf8')
+  t.true(netlifyTomlContent.includes('[build]'))
+  await del(netlifyTomlPath, { force: true })
+}
+
 test('Configuration file - netlify.yaml', async t => {
+  const netlifyTomlPath = await getNetlifyTomlPath('yaml')
   await runFixture(t, 'yaml')
+  await checkNetlifyToml(t, netlifyTomlPath)
 })
 
 test('Configuration file - netlify.yml', async t => {
+  const netlifyTomlPath = await getNetlifyTomlPath('yml')
   await runFixture(t, 'yml')
+  await checkNetlifyToml(t, netlifyTomlPath)
 })
 
 test('Configuration file - netlify.json', async t => {
+  const netlifyTomlPath = await getNetlifyTomlPath('json')
   await runFixture(t, 'json')
+  await checkNetlifyToml(t, netlifyTomlPath)
 })
 
 test('Configuration file - netlify.toml', async t => {
@@ -22,6 +52,12 @@ test('Configuration file - netlify.toml', async t => {
 
 test('Configuration file - invalid format', async t => {
   await runFixture(t, '', { flags: `--config=${FIXTURES_DIR}/invalid/netlify.invalid` })
+})
+
+test('Configuration file - netlify.yml that cannot be converted to TOML', async t => {
+  const netlifyTomlPath = await getNetlifyTomlPath('non_convertible_yaml')
+  await runFixture(t, 'non_convertible_yaml')
+  t.false(await pathExists(netlifyTomlPath))
 })
 
 test('Configuration file - advanced YAML', async t => {
