41208531d0a31e3ec13625dc4908d297eb7dc02a
ehmicky <ehmicky@users.noreply.github.com>
2020-05-26 16:37:12 +0200

    Allow capitalized configuration properties (#1368)

--- packages/config/src/normalize.js
+++ packages/config/src/normalize.js
@@ -3,15 +3,43 @@ const { removeFalsy } = require('./utils/remove_falsy')
 
 // Normalize configuration object
 const normalizeConfig = function(config) {
-  const { build, plugins, ...configA } = deepMerge(DEFAULT_CONFIG, config)
+  const configA = normalizePropertiesCase(config)
+  const configB = deepMerge(DEFAULT_CONFIG, configA)
+  const { build, plugins, ...configC } = configB
   const buildA = removeEmptyCommand(build)
   const pluginsA = plugins.map(normalizePlugin)
-  return { ...configA, build: buildA, plugins: pluginsA }
+  return { ...configC, build: buildA, plugins: pluginsA }
 }
 
 const DEFAULT_CONFIG = {
   build: { environment: {} },
   plugins: [],
+}
+
+// Some properties can be optionally capitalized. We normalize them to lowercase
+const normalizePropertiesCase = function({
+  Build = {},
+  build: {
+    Base,
+    base = Base,
+    Command,
+    command = Command,
+    Environment,
+    environment = Environment,
+    Functions,
+    functions = Functions,
+    Ignore,
+    ignore = Ignore,
+    Processing,
+    processing = Processing,
+    Publish,
+    publish = Publish,
+    ...build
+  } = Build,
+  ...config
+}) {
+  const buildA = removeFalsy({ ...build, base, command, environment, functions, ignore, processing, publish })
+  return { ...config, build: buildA }
 }
 
 const removeEmptyCommand = function({ command, ...build }) {
--- packages/config/tests/normalize/tests.js
+++ packages/config/tests/normalize/tests.js
@@ -5,3 +5,7 @@ const { runFixture } = require('../helpers/main')
 test('build.command empty', async t => {
   await runFixture(t, 'command_empty')
 })
+
+test('Some properties can be capitalized', async t => {
+  await runFixture(t, 'props_case')
+})
