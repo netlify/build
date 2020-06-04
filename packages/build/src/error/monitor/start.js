db10b6c3418784127be14c61dda1e370f6ef7163
ehmicky <ehmicky@users.noreply.github.com>
2020-06-04 17:03:08 +0200

    Improve Bugsnag start logic (#1418)

--- packages/build/src/error/monitor/start.js
+++ packages/build/src/error/monitor/start.js
@@ -1,6 +1,7 @@
 const { env } = require('process')
 
 const Bugsnag = require('@bugsnag/js')
+const memoizeOne = require('memoize-one')
 
 const { name, version } = require('../../../package.json')
 const { log } = require('../../log/logger.js')
@@ -16,7 +17,7 @@ const startErrorMonitor = function({ mode }) {
 
   const releaseStage = getReleaseStage(mode)
   try {
-    const errorMonitor = Bugsnag.start({
+    const errorMonitor = startBugsnag({
       apiKey,
       appVersion: `${name} ${version}`,
       appType: name,
@@ -35,6 +36,11 @@ const startErrorMonitor = function({ mode }) {
     return
   }
 }
+
+// Bugsnag.start() caches a global instance and warns on duplicate calls.
+// This ensures the warning message is not shown when calling the main function
+// several times.
+const startBugsnag = memoizeOne(Bugsnag.start.bind(Bugsnag), () => true)
 
 // Based the release stage on the `mode`
 const getReleaseStage = function(mode = DEFAULT_RELEASE_STAGE) {
