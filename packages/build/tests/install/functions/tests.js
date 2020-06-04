9cc3fa5876477702addb399540d7edafab8d1ee0
ehmicky <ehmicky@users.noreply.github.com>
2020-06-04 15:06:56 +0200

    Remove some unused tests related to installations (#1415)

--- packages/build/tests/install/functions/tests.js
+++ packages/build/tests/install/functions/tests.js
@@ -1,5 +1,4 @@
 const test = require('ava')
-const del = require('del')
 const pathExists = require('path-exists')
 
 const { removeDir } = require('../../helpers/dir')
@@ -18,67 +17,25 @@ test.serial('Functions: ignore package.json inside node_modules', async t => {
   await runFixture(t, 'node_modules')
 })
 
-test.serial('Functions: install dependencies with npm locally with lock file', async t => {
-  await removeDir([`${FIXTURES_DIR}/npm_lock/.netlify/functions/`, `${FIXTURES_DIR}/npm_lock/functions/node_modules/`])
-  await runFixture(t, 'npm_lock')
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_lock/functions/node_modules/`))
-  await removeDir([`${FIXTURES_DIR}/npm_lock/.netlify/functions/`, `${FIXTURES_DIR}/npm_lock/functions/node_modules/`])
+test.serial('Functions: install dependencies with npm', async t => {
+  await removeDir([`${FIXTURES_DIR}/npm/.netlify/functions/`, `${FIXTURES_DIR}/npm/functions/node_modules/`])
+  await runFixture(t, 'npm')
+  t.true(await pathExists(`${FIXTURES_DIR}/npm/functions/node_modules/`))
+  await removeDir([`${FIXTURES_DIR}/npm/.netlify/functions/`, `${FIXTURES_DIR}/npm/functions/node_modules/`])
 })
 
-test.serial('Functions: install dependencies with npm in CI with no lock file', async t => {
-  await removeDir([
-    `${FIXTURES_DIR}/npm_ci_no_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/npm_ci_no_lock/functions/node_modules/`,
-  ])
-
-  await runFixture(t, 'npm_ci_no_lock', { flags: '--mode=buildbot' })
-
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_no_lock/functions/node_modules/`))
-  await removeDir([
-    `${FIXTURES_DIR}/npm_ci_no_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/npm_ci_no_lock/functions/node_modules/`,
-  ])
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_no_lock/functions/package-lock.json`))
-  await del(`${FIXTURES_DIR}/npm_ci_no_lock/functions/package-lock.json`, { force: true })
+test.serial('Functions: install dependencies with Yarn locally', async t => {
+  await removeDir([`${FIXTURES_DIR}/yarn/.netlify/functions/`, `${FIXTURES_DIR}/yarn/functions/node_modules/`])
+  await runFixture(t, 'yarn')
+  t.true(await pathExists(`${FIXTURES_DIR}/yarn/functions/node_modules/`))
+  await removeDir([`${FIXTURES_DIR}/yarn/.netlify/functions/`, `${FIXTURES_DIR}/yarn/functions/node_modules/`])
 })
 
-test.serial('Functions: install dependencies with npm in CI with lock file', async t => {
-  await removeDir([
-    `${FIXTURES_DIR}/npm_ci_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/npm_ci_lock/functions/node_modules/`,
-  ])
-  await runFixture(t, 'npm_ci_lock', { flags: '--mode=buildbot' })
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_lock/functions/node_modules/`))
-  await removeDir([
-    `${FIXTURES_DIR}/npm_ci_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/npm_ci_lock/functions/node_modules/`,
-  ])
-})
-
-test.serial('Functions: install dependencies with Yarn locally with lock file', async t => {
-  await removeDir([
-    `${FIXTURES_DIR}/yarn_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/yarn_lock/functions/node_modules/`,
-  ])
-  await runFixture(t, 'yarn_lock')
-  t.true(await pathExists(`${FIXTURES_DIR}/yarn_lock/functions/node_modules/`))
-  await removeDir([
-    `${FIXTURES_DIR}/yarn_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/yarn_lock/functions/node_modules/`,
-  ])
-})
-
-test.serial('Functions: install dependencies with Yarn in CI with lock file', async t => {
-  await removeDir([
-    `${FIXTURES_DIR}/yarn_ci_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/yarn_ci_lock/functions/node_modules/`,
-  ])
-  await runFixture(t, 'yarn_ci_lock', { flags: '--mode=buildbot' })
-  t.true(await pathExists(`${FIXTURES_DIR}/yarn_ci_lock/functions/node_modules/`))
-  await removeDir([
-    `${FIXTURES_DIR}/yarn_ci_lock/.netlify/functions/`,
-    `${FIXTURES_DIR}/yarn_ci_lock/functions/node_modules/`,
-  ])
+test.serial('Functions: install dependencies with Yarn in CI', async t => {
+  await removeDir([`${FIXTURES_DIR}/yarn_ci/.netlify/functions/`, `${FIXTURES_DIR}/yarn_ci/functions/node_modules/`])
+  await runFixture(t, 'yarn_ci', { flags: '--mode=buildbot' })
+  t.true(await pathExists(`${FIXTURES_DIR}/yarn_ci/functions/node_modules/`))
+  await removeDir([`${FIXTURES_DIR}/yarn_ci/.netlify/functions/`, `${FIXTURES_DIR}/yarn_ci/functions/node_modules/`])
 })
 
 test.serial('Functions: does not install dependencies unless opting in', async t => {
--- packages/build/tests/install/local/tests.js
+++ packages/build/tests/install/local/tests.js
@@ -1,49 +1,29 @@
 const test = require('ava')
-const del = require('del')
 const pathExists = require('path-exists')
 
 const { removeDir } = require('../../helpers/dir')
 const { runFixture, FIXTURES_DIR } = require('../../helpers/main')
 
 // Need to run `npm install` and `yarn` serially to avoid network errors
-test.serial('Install local plugin dependencies: with npm locally with lock file', async t => {
-  await removeDir(`${FIXTURES_DIR}/npm_lock/plugin/node_modules`)
-  await runFixture(t, 'npm_lock')
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_lock/plugin/node_modules`))
-  await removeDir(`${FIXTURES_DIR}/npm_lock/plugin/node_modules`)
+test.serial('Install local plugin dependencies: with npm', async t => {
+  await removeDir(`${FIXTURES_DIR}/npm/plugin/node_modules`)
+  await runFixture(t, 'npm')
+  t.true(await pathExists(`${FIXTURES_DIR}/npm/plugin/node_modules`))
+  await removeDir(`${FIXTURES_DIR}/npm/plugin/node_modules`)
 })
 
-test.serial('Install local plugin dependencies: with npm in CI with lock file', async t => {
-  await removeDir(`${FIXTURES_DIR}/npm_ci_lock/plugin/node_modules`)
-  await runFixture(t, 'npm_ci_lock', { flags: '--mode=buildbot' })
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_lock/plugin/node_modules`))
-  await removeDir(`${FIXTURES_DIR}/npm_ci_lock/plugin/node_modules`)
+test.serial('Install local plugin dependencies: with yarn locally', async t => {
+  await removeDir(`${FIXTURES_DIR}/yarn/plugin/node_modules`)
+  await runFixture(t, 'yarn')
+  t.true(await pathExists(`${FIXTURES_DIR}/yarn/plugin/node_modules`))
+  await removeDir(`${FIXTURES_DIR}/yarn/plugin/node_modules`)
 })
 
-test.serial('Install local plugin dependencies: with npm in CI with no lock file', async t => {
-  await removeDir(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/node_modules`)
-  await del(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/package-lock.json`, { force: true })
-
-  await runFixture(t, 'npm_ci_no_lock', { flags: '--mode=buildbot' })
-
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/node_modules`))
-  await removeDir(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/node_modules`)
-  t.true(await pathExists(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/package-lock.json`))
-  await del(`${FIXTURES_DIR}/npm_ci_no_lock/plugin/package-lock.json`, { force: true })
-})
-
-test.serial('Install local plugin dependencies: with yarn locally with lock file', async t => {
-  await removeDir(`${FIXTURES_DIR}/yarn_lock/plugin/node_modules`)
-  await runFixture(t, 'yarn_lock')
-  t.true(await pathExists(`${FIXTURES_DIR}/yarn_lock/plugin/node_modules`))
-  await removeDir(`${FIXTURES_DIR}/yarn_lock/plugin/node_modules`)
-})
-
-test.serial('Install local plugin dependencies: with yarn in CI with lock file', async t => {
-  await removeDir(`${FIXTURES_DIR}/yarn_ci_lock/plugin/node_modules`)
-  await runFixture(t, 'yarn_ci_lock', { flags: '--mode=buildbot' })
-  t.true(await pathExists(`${FIXTURES_DIR}/yarn_ci_lock/plugin/node_modules`))
-  await removeDir(`${FIXTURES_DIR}/yarn_ci_lock/plugin/node_modules`)
+test.serial('Install local plugin dependencies: with yarn in CI', async t => {
+  await removeDir(`${FIXTURES_DIR}/yarn_ci/plugin/node_modules`)
+  await runFixture(t, 'yarn_ci', { flags: '--mode=buildbot' })
+  t.true(await pathExists(`${FIXTURES_DIR}/yarn_ci/plugin/node_modules`))
+  await removeDir(`${FIXTURES_DIR}/yarn_ci/plugin/node_modules`)
 })
 
 test.serial('Install local plugin dependencies: propagate errors', async t => {
