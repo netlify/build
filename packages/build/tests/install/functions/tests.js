1acdde7f576b4c2cdecef118110013b994d22704
ehmicky <ehmicky@users.noreply.github.com>
2020-06-04 15:50:38 +0200

    Improve speed of installation-related tests (#1416)

--- packages/build/tests/install/functions/tests.js
+++ packages/build/tests/install/functions/tests.js
@@ -4,48 +4,47 @@ const pathExists = require('path-exists')
 const { removeDir } = require('../../helpers/dir')
 const { runFixture, FIXTURES_DIR } = require('../../helpers/main')
 
-// Need to run `npm install` and `yarn` serially to avoid network errors
-test.serial('Functions: install dependencies nested', async t => {
+test('Functions: install dependencies nested', async t => {
   await removeDir([`${FIXTURES_DIR}/dir/.netlify/functions/`, `${FIXTURES_DIR}/dir/functions/function/node_modules/`])
   await runFixture(t, 'dir')
   t.true(await pathExists(`${FIXTURES_DIR}/dir/functions/function/node_modules/`))
   await removeDir([`${FIXTURES_DIR}/dir/.netlify/functions/`, `${FIXTURES_DIR}/dir/functions/function/node_modules/`])
 })
 
-test.serial('Functions: ignore package.json inside node_modules', async t => {
+test('Functions: ignore package.json inside node_modules', async t => {
   await removeDir(`${FIXTURES_DIR}/node_modules/.netlify/functions/`)
   await runFixture(t, 'node_modules')
 })
 
-test.serial('Functions: install dependencies with npm', async t => {
+test('Functions: install dependencies with npm', async t => {
   await removeDir([`${FIXTURES_DIR}/npm/.netlify/functions/`, `${FIXTURES_DIR}/npm/functions/node_modules/`])
   await runFixture(t, 'npm')
   t.true(await pathExists(`${FIXTURES_DIR}/npm/functions/node_modules/`))
   await removeDir([`${FIXTURES_DIR}/npm/.netlify/functions/`, `${FIXTURES_DIR}/npm/functions/node_modules/`])
 })
 
-test.serial('Functions: install dependencies with Yarn locally', async t => {
+test('Functions: install dependencies with Yarn locally', async t => {
   await removeDir([`${FIXTURES_DIR}/yarn/.netlify/functions/`, `${FIXTURES_DIR}/yarn/functions/node_modules/`])
   await runFixture(t, 'yarn')
   t.true(await pathExists(`${FIXTURES_DIR}/yarn/functions/node_modules/`))
   await removeDir([`${FIXTURES_DIR}/yarn/.netlify/functions/`, `${FIXTURES_DIR}/yarn/functions/node_modules/`])
 })
 
-test.serial('Functions: install dependencies with Yarn in CI', async t => {
+test('Functions: install dependencies with Yarn in CI', async t => {
   await removeDir([`${FIXTURES_DIR}/yarn_ci/.netlify/functions/`, `${FIXTURES_DIR}/yarn_ci/functions/node_modules/`])
   await runFixture(t, 'yarn_ci', { flags: '--mode=buildbot' })
   t.true(await pathExists(`${FIXTURES_DIR}/yarn_ci/functions/node_modules/`))
   await removeDir([`${FIXTURES_DIR}/yarn_ci/.netlify/functions/`, `${FIXTURES_DIR}/yarn_ci/functions/node_modules/`])
 })
 
-test.serial('Functions: does not install dependencies unless opting in', async t => {
+test('Functions: does not install dependencies unless opting in', async t => {
   await removeDir([`${FIXTURES_DIR}/optional/.netlify/functions/`, `${FIXTURES_DIR}/optional/functions/node_modules/`])
   await runFixture(t, 'optional')
   t.false(await pathExists(`${FIXTURES_DIR}/optional/functions/node_modules/`))
   await removeDir([`${FIXTURES_DIR}/optional/.netlify/functions/`, `${FIXTURES_DIR}/optional/functions/node_modules/`])
 })
 
-test.serial('Functions: does not print warnings when dependency was mispelled', async t => {
+test('Functions: does not print warnings when dependency was mispelled', async t => {
   await removeDir([
     `${FIXTURES_DIR}/mispelled_dep/.netlify/functions/`,
     `${FIXTURES_DIR}/mispelled_dep/functions/node_modules/`,
