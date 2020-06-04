541ae963d643331cb136008a3752425f9feb2d27
ehmicky <ehmicky@users.noreply.github.com>
2020-06-04 17:06:22 +0200

    Make `flags` object optional (#1420)

--- packages/build/src/core/main.js
+++ packages/build/src/core/main.js
@@ -35,7 +35,7 @@ const { doDryRun } = require('./dry')
  * @param  {string} [flags.context] - Build context
  * @param  {boolean} [flags.dry] - printing commands without executing them
  */
-const build = async function(flags) {
+const build = async function(flags = {}) {
   const buildTimer = startTimer()
 
   logBuildStart()
