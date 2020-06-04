0dcba270d559afd6f9aad6a1963dc3c49d583f4c
ehmicky <ehmicky@gmail.com>
2020-06-04 14:06:40 +0200

    Remove process warnings debugging

--- packages/build/tests/error/process/tests.js
+++
@@ -1,7 +0,0 @@
-const test = require('ava')
-
-const { runFixture } = require('../../helpers/main')
-
-test('NETLIFY_BUILD_DEBUG_PROCESS_ERRORS environment variable', async t => {
-  await runFixture(t, 'process', { env: { NETLIFY_BUILD_DEBUG_PROCESS_ERRORS: '1' } })
-})
