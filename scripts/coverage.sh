cb228a4acabf857d64f326961e5bac56e2c392df
ehmicky <ehmicky@users.noreply.github.com>
2020-06-08 14:04:19 +0200

    Fix codecov script (#1442)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,4 +8,4 @@ node="$2"
 node="node_${node//./_}"
 
 curl -s https://codecov.io/bash | \
-  bash -s -- -Z -y codecov.yml -f coverage/coverage-final.json -F "$os" -F "$node"
+  bash -s -- -Z -f coverage/coverage-final.json -F "$os" -F "$node"
