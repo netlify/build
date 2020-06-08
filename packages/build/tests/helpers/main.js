92605cd35363a5cf8ded683841aba82d878d4452
ehmicky <ehmicky@gmail.com>
2020-06-08 18:18:22 +0200

    Remove `path-key`

--- packages/build/tests/helpers/main.js
+++ packages/build/tests/helpers/main.js
@@ -1,13 +1,8 @@
-const { delimiter } = require('path')
-const { env } = require('process')
-
 const { getBinPath } = require('get-bin-path')
-const pathKey = require('path-key')
 
 const { runFixtureCommon, FIXTURES_DIR } = require('./common')
 
 const ROOT_DIR = `${__dirname}/../..`
-const BUILD_BIN_DIR = `${ROOT_DIR}/node_modules/.bin`
 
 const runFixture = async function(t, fixtureName, { env: envOption, flags = '', ...opts } = {}) {
   return runFixtureCommon(t, fixtureName, {
@@ -18,9 +13,6 @@ const runFixture = async function(t, fixtureName, { env: envOption, flags = '', 
       BUILD_TELEMETRY_DISABLED: '',
       // Ensure local tokens aren't used during development
       NETLIFY_AUTH_TOKEN: '',
-      // Allows executing any locally installed Node modules inside tests,
-      // regardless of the current directory
-      [pathKey()]: `${env[pathKey()]}${delimiter}${BUILD_BIN_DIR}`,
       ...envOption,
     },
   })
