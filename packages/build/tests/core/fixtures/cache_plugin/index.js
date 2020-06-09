81f71eb6a66ee7410a0df607d6baf2577d3e28a8
ehmicky <ehmicky@gmail.com>
2020-06-09 12:46:12 +0200

    Merge tests

---
+++ packages/build/tests/core/fixtures/cache_plugin/index.js
@@ -0,0 +1,33 @@
+const { writeFile, readFile } = require('fs')
+const { dirname } = require('path')
+const {
+  env: { TEST_CACHE_PATH },
+} = require('process')
+const { promisify } = require('util')
+
+// eslint-disable-next-line node/no-extraneous-require
+const del = require('del')
+// eslint-disable-next-line node/no-extraneous-require
+const makeDir = require('make-dir')
+
+const cachePath = `${TEST_CACHE_PATH}/test/test`
+
+const pWriteFile = promisify(writeFile)
+const pReadFile = promisify(readFile)
+
+const DUMMY_VALUE = String(Math.random())
+
+module.exports = {
+  async onPreBuild({ utils: { cache } }) {
+    await cache.remove(TEST_CACHE_PATH)
+    await makeDir(dirname(cachePath))
+    await pWriteFile(cachePath, DUMMY_VALUE)
+  },
+  async onPostBuild({ utils: { cache } }) {
+    await del(TEST_CACHE_PATH, { force: true })
+    await cache.restore(TEST_CACHE_PATH)
+    const value = await pReadFile(cachePath, 'utf8')
+    console.log(value === DUMMY_VALUE)
+    await del(TEST_CACHE_PATH, { force: true })
+  },
+}
