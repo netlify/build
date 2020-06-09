4a9d422a76dbcf7d380fe58ab399bc5ba6c2e765
ehmicky <ehmicky@gmail.com>
2020-06-09 12:47:03 +0200

    Merge tests

--- packages/build/tests/plugins/fixtures/interleave/plugin.js
+++ packages/build/tests/plugins/fixtures/interleave/plugin.js
@@ -5,9 +5,9 @@ const pSetTimeout = promisify(setTimeout)
 module.exports = {
   async onPreBuild() {
     console.log('one')
-    await pSetTimeout(1e3)
+    await pSetTimeout(1e2)
     console.error('two')
-    await pSetTimeout(1e3)
+    await pSetTimeout(1e2)
     console.log('three')
   },
 }
