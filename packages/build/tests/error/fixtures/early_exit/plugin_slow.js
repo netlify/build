c4153253871b733bc0933f83654f4aef48be645f
ehmicky <ehmicky@users.noreply.github.com>
2020-06-09 16:32:35 +0200

    Fix test failing on Node 8 (#1486)

--- packages/build/tests/error/fixtures/early_exit/plugin_slow.js
+++ packages/build/tests/error/fixtures/early_exit/plugin_slow.js
@@ -1,5 +1,10 @@
+const { promisify } = require('util')
+
+const pSetTimeout = promisify(setTimeout)
+
 module.exports = {
-  onBuild() {
+  async onBuild() {
     process.kill(process.env.TEST_PID)
+    await pSetTimeout(0)
   },
 }
