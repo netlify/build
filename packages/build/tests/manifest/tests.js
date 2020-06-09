40a6d2ad8070e502fcccb481a1c5edfc885ab343
ehmicky <ehmicky@gmail.com>
2020-06-09 12:46:55 +0200

    Merge tests

--- packages/build/tests/manifest/tests.js
+++ packages/build/tests/manifest/tests.js
@@ -1,13 +1,49 @@
 const test = require('ava')
 
-const { runFixture } = require('../../helpers/main')
+const { runFixture } = require('../helpers/main')
+
+test('manifest.yml check required inputs', async t => {
+  await runFixture(t, 'required')
+})
+
+test('manifest.yml check unknown property', async t => {
+  await runFixture(t, 'unknown')
+})
+
+test('manifest.yml check default value', async t => {
+  await runFixture(t, 'default')
+})
+
+test('manifest.yml same directory', async t => {
+  await runFixture(t, 'same_directory')
+})
+
+test('manifest.yml root directory', async t => {
+  await runFixture(t, 'root_directory')
+})
+
+test('manifest.yml not root directory', async t => {
+  await runFixture(t, 'not_root_directory')
+})
+
+test('manifest.yml missing', async t => {
+  await runFixture(t, 'missing')
+})
+
+test('manifest.yml parse error', async t => {
+  await runFixture(t, 'parse_error')
+})
+
+test('manifest.yml advanced YAML', async t => {
+  await runFixture(t, 'advanced_yaml')
+})
 
 test('manifest.yml plain object', async t => {
   await runFixture(t, 'plain_object')
 })
 
 test('manifest.yml unknown properties', async t => {
-  await runFixture(t, 'unknown')
+  await runFixture(t, 'manifest_unknown')
 })
 
 test('manifest.yml name undefined', async t => {
