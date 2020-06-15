8690ccfbb6a83afc205178f88b0d343001c09ee3
ehmicky <ehmicky@users.noreply.github.com>
2020-06-15 12:47:37 +0200

    Simplify colors logic (#1516)

--- packages/build/src/log/colors.js
+++ packages/build/src/log/colors.js
@@ -1,34 +1,36 @@
-const { env } = require('process')
 const {
   inspect: { defaultOptions },
 } = require('util')
 
-// Ensure colors are used in the buildbot.
-// For `chalk`, underlying `supports-color` and `console.log()`.
-const setColorLevel = function() {
-  // We cannot use the --mode CLI flag since this must be loaded before `chalk`.
-  if (!env.NETLIFY && env.PARENT_HAS_COLOR !== '1') {
-    return
-  }
-
-  env.FORCE_COLOR = '1'
-  defaultOptions.colors = true
-}
+const supportsColor = require('supports-color')
 
 // Plugin child processes use `stdio: 'pipe'` so they are always
 // non-interactive even if the parent is an interactive TTY. This means they
 // would normally lose colors. If the parent has colors, we pass an environment
 // variable to the child process to force colors.
 const getParentColorEnv = function() {
-  // This must be required after `env.FORCE_COLOR` has been set because it
-  // checks it at require-time.
-  const supportsColor = require('supports-color')
-
-  if (supportsColor.stdout === false) {
+  if (!hasColors()) {
     return {}
   }
 
-  return { PARENT_HAS_COLOR: '1' }
+  return { FORCE_COLOR: '1' }
 }
 
-module.exports = { setColorLevel, getParentColorEnv }
+// Child processes and the buildbot relies on `FORCE_COLOR=1` to set colors.
+// However `utils.inspect()` (used by `console.log()`) uses
+// `process.stdout.hasColors` which is always `undefined` when the TTY is
+// non-interactive. So we need to set `util.inspect.defaultOptions.colors`
+// manually both in plugin processes.
+const setInspectColors = function() {
+  if (!hasColors()) {
+    return
+  }
+
+  defaultOptions.colors = true
+}
+
+const hasColors = function() {
+  return supportsColor.stdout !== false
+}
+
+module.exports = { getParentColorEnv, setInspectColors }
