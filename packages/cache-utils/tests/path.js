05866a9c0f500dbac859c11569be10a0997f58ad
ehmicky <ehmicky@users.noreply.github.com>
2020-06-22 14:17:46 +0200

    Fix invalid `cwd` in `cache` utility (#1542)

--- packages/cache-utils/tests/path.js
+++ packages/cache-utils/tests/path.js
@@ -26,3 +26,35 @@ test('Should not allow caching the current directory', async t => {
 test('Should not allow caching a direct parent directory', async t => {
   await t.throwsAsync(cacheUtils.save('..'))
 })
+
+// Windows does not allow deleting directory uses as current directory
+if (process.platform !== 'win32') {
+  // Need to use `test.serial()` due to `process.chdir()`
+  test.serial('Should not allow invalid cwd with relative paths', async t => {
+    const tmpDir = await createTmpDir()
+    const oldCwd = process.cwd()
+    process.chdir(tmpDir)
+    await removeFiles(tmpDir)
+    try {
+      await t.throwsAsync(cacheUtils.save('test'))
+    } finally {
+      process.chdir(oldCwd)
+    }
+  })
+
+  test.serial('Should allow invalid cwd with absolute paths', async t => {
+    const [tmpDir, [srcFile, srcDir]] = await Promise.all([createTmpDir(), createTmpFile()])
+    const oldCwd = process.cwd()
+    process.chdir(tmpDir)
+    await removeFiles(tmpDir)
+    try {
+      await t.notThrowsAsync(cacheUtils.save(srcFile))
+      await removeFiles(srcFile)
+      await t.notThrowsAsync(cacheUtils.restore(srcFile))
+      t.true(await pathExists(srcFile))
+    } finally {
+      process.chdir(oldCwd)
+      await removeFiles(srcDir)
+    }
+  })
+}
