c1e951d33918689b2ca584676188df13f028aced
ehmicky <ehmicky@gmail.com>
2020-06-23 12:17:35 +0200

    Improve debug mode

---
+++ packages/build/src/log/cleanup.js
@@ -0,0 +1,70 @@
+const filterObj = require('filter-obj')
+
+const { removeFalsy } = require('../utils/remove_falsy')
+
+// Make sure we are not printing secret values. Use an allow list.
+const cleanupConfig = function({
+  build: { base, command, environment = {}, functions, ignore, processing, publish },
+  headers,
+  plugins,
+  redirects,
+}) {
+  const environmentA = cleanupEnvironment(environment)
+  const build = { base, command, environment: environmentA, functions, ignore, processing, publish }
+  const pluginsA = plugins.map(cleanupPlugin)
+  const netlifyConfig = simplifyConfig({ build, plugins: pluginsA, headers, redirects })
+  return netlifyConfig
+}
+
+const cleanupEnvironment = function(environment) {
+  return Object.keys(environment).filter(key => !BUILDBOT_ENVIRONMENT.includes(key))
+}
+
+// Added by the buildbot. We only want to print environment variables specified
+// by the user.
+const BUILDBOT_ENVIRONMENT = [
+  'BRANCH',
+  'CONTEXT',
+  'DEPLOY_PRIME_URL',
+  'DEPLOY_URL',
+  'GO_VERSION',
+  'NETLIFY_IMAGES_CDN_DOMAIN',
+  'SITE_ID',
+  'SITE_NAME',
+  'URL',
+]
+
+const cleanupPlugin = function({ package, origin, inputs }) {
+  const inputsA = filterObj(inputs, isPublicInput)
+  return { package, origin, inputs: inputsA }
+}
+
+const isPublicInput = function(key, input) {
+  return typeof input === 'boolean'
+}
+
+// The resolved configuration gets assigned some default values (empty objects and arrays)
+// to make it more convenient to use without checking for `undefined`.
+// However those empty values are not useful to users, so we don't log them.
+const simplifyConfig = function({ build: { environment, ...build }, plugins, ...netlifyConfig }) {
+  const buildA = {
+    ...build,
+    ...removeEmptyArray(environment, 'environment'),
+  }
+  return removeFalsy({
+    ...netlifyConfig,
+    ...removeEmptyObject(buildA, 'build'),
+    ...removeEmptyArray(plugins, 'plugins'),
+  })
+}
+
+const removeEmptyObject = function(object, propName) {
+  const objectA = removeFalsy(object)
+  return Object.keys(objectA).length === 0 ? {} : { [propName]: objectA }
+}
+
+const removeEmptyArray = function(array, propName) {
+  return array.length === 0 ? {} : { [propName]: array }
+}
+
+module.exports = { cleanupConfig }
