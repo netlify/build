b8d05fc929932cf7db52da5bfb75311f559f1b56
ehmicky <ehmicky@users.noreply.github.com>
2020-06-23 19:50:19 +0200

    Print `@netlify/config` result in debugging (#1561)

--- packages/config/tests/log/tests.js
+++ packages/config/tests/log/tests.js
@@ -3,10 +3,10 @@ const test = require('ava')
 const { runFixture } = require('../helpers/main')
 
 test('Prints some information in debug mode', async t => {
-  await runFixture(t, 'empty', { flags: { debug: true }, useBinary: true })
+  await runFixture(t, 'simple', { flags: { debug: true }, useBinary: true })
 })
 
 test('Does not print confidential information in debug mode', async t => {
   const defaultConfig = JSON.stringify({ build: { environment: { SECRET: 'true' } } })
-  await runFixture(t, 'empty', { flags: { debug: true, defaultConfig, env: { SECRET: 'true' } }, useBinary: true })
+  await runFixture(t, 'simple', { flags: { debug: true, defaultConfig, env: { SECRET: 'true' } }, useBinary: true })
 })
