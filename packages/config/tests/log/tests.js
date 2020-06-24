132883eb79ea64270620d3a5bb5ff92a3fd686a3
ehmicky <ehmicky@users.noreply.github.com>
2020-06-24 12:59:47 +0200

    Allow debug mode to be abled from the UI settings (#1569)

--- packages/config/tests/log/tests.js
+++ packages/config/tests/log/tests.js
@@ -10,3 +10,12 @@ test('Does not print confidential information in debug mode', async t => {
   const defaultConfig = JSON.stringify({ build: { environment: { SECRET: 'true' } } })
   await runFixture(t, 'simple', { flags: { debug: true, defaultConfig, env: { SECRET: 'true' } }, useBinary: true })
 })
+
+test('Debug mode can be enabled using the NETLIFY_BUILD_DEBUG environment variable', async t => {
+  await runFixture(t, 'simple', { flags: { env: { NETLIFY_BUILD_DEBUG: 'true' } }, useBinary: true })
+})
+
+test('Debug mode can be enabled using the NETLIFY_BUILD_DEBUG environment UI setting', async t => {
+  const defaultConfig = JSON.stringify({ build: { environment: { NETLIFY_BUILD_DEBUG: 'true' } } })
+  await runFixture(t, 'simple', { flags: { defaultConfig }, useBinary: true })
+})
