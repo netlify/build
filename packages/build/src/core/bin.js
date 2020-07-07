342e9e8da9a2665dcd27e3795eef2ae7e25d4be0
Benaiah Mischenko <benaiah@mischenko.com>
2020-07-06 17:17:35 -0700

    Add triggerDeployWithBuildbotServer feature flag

--- packages/build/src/core/bin.js
+++ packages/build/src/core/bin.js
@@ -115,6 +115,15 @@ Default: automatically guessed`,
 Default: none`,
     hidden: true,
   },
+  triggerDeployWithBuildbotServer: {
+    boolean: true,
+    describe: `Feature flag.
+When enabled, triggers a deploy by connecting to the buildbot deploy server and
+passing it a "deploySite" command. After the deploy is finished, it triggers the events
+"onDeploySuccess", "onDeployError", and "onPostDeploy" as appropriate.
+Default: false`,
+    hidden: true,
+  },
   buildbotServerSocket: {
     string: true,
     describe: `Path to the buildbot server socket. This is used to connect to the buildbot to trigger deploys.`,
