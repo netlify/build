bdb55ba9dd5125eb7fdb204e8fc34be1991a1084
ehmicky <ehmicky@gmail.com>
2020-08-06 14:01:58 +0200

    Do not log `timersFile` in production

--- packages/build/src/log/main.js
+++ packages/build/src/log/main.js
@@ -39,7 +39,7 @@ const SECURE_FLAGS = ['token', 'bugsnagKey', 'env', 'cachedConfig', 'defaultConf
 // Hidden because those are used in tests
 const TEST_FLAGS = ['buffer', 'telemetry']
 // Hidden because those are only used internally
-const INTERNAL_FLAGS = ['nodePath', 'functionsDistDir', 'buildImagePluginsDir', 'sendStatus']
+const INTERNAL_FLAGS = ['nodePath', 'functionsDistDir', 'buildImagePluginsDir', 'sendStatus', 'timersFile']
 const HIDDEN_FLAGS = [...SECURE_FLAGS, ...TEST_FLAGS, ...INTERNAL_FLAGS]
 const HIDDEN_DEBUG_FLAGS = [...SECURE_FLAGS, ...TEST_FLAGS]
 
