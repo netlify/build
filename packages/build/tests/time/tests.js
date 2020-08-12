5e108539192522a13865a5465fae9029e2c94c0d
ehmicky <ehmicky@gmail.com>
2020-08-12 21:36:09 +0200

    Add --framework CLI flag

--- packages/build/tests/time/tests.js
+++ packages/build/tests/time/tests.js
@@ -3,32 +3,52 @@ const test = require('ava')
 const { runFixture } = require('../helpers/main')
 const { startUdpServer } = require('../helpers/udp_server')
 
-const FIXTURES = ['simple', 'plugin']
-
-FIXTURES.forEach(fixtureName => {
-  test(`Sends timings to --statsd.host|port | ${fixtureName}`, async t => {
-    const { host, port, requests, stopServer } = await startUdpServer()
-    try {
-      await runFixture(t, fixtureName, { flags: { statsd: { host, port } }, snapshot: false })
-      const timerRequests = getTimerRequests(requests)
-      t.snapshot(timerRequests)
-    } finally {
-      await stopServer()
-    }
-  })
+test('Sends timings to --statsd.host|port', async t => {
+  t.snapshot(await getTimerRequestsString(t, 'simple'))
 })
 
-// Retrieve statsd packets sent to --statsd.host|port, and snapshot them
-const getTimerRequests = function(requests) {
-  return requests
-    .flatMap(flattenRequest)
-    .map(normalizeRequest)
-    .sort()
-    .join('\n')
+test('Sends timings of plugins', async t => {
+  t.snapshot(await getTimerRequestsString(t, 'plugin'))
+})
+
+test('Allow passing --framework CLI flag', async t => {
+  const timerRequests = await getAllTimerRequests(t, 'simple', { framework: 'test' })
+  t.true(timerRequests.every(timerRequest => timerRequest.includes('framework:test')))
+})
+
+test('Default --framework CLI flag to nothing', async t => {
+  const timerRequests = await getAllTimerRequests(t, 'simple')
+  t.true(timerRequests.every(timerRequest => !timerRequest.includes('framework:')))
+})
+
+// Retrieve statsd packets sent to --statsd.host|port, and get their snapshot
+const getTimerRequestsString = async function(t, fixtureName, flags) {
+  const timerRequests = await getAllTimerRequests(t, fixtureName, flags)
+  const timerRequestsString = serializeTimerRequests(timerRequests)
+  return timerRequestsString
+}
+
+const getAllTimerRequests = async function(t, fixtureName, flags = {}) {
+  const { host, port, requests, stopServer } = await startUdpServer()
+  try {
+    await runFixture(t, fixtureName, { flags: { statsd: { host, port }, ...flags }, snapshot: false })
+  } finally {
+    await stopServer()
+  }
+
+  const timerRequests = requests.flatMap(flattenRequest)
+  return timerRequests
 }
 
 const flattenRequest = function(request) {
   return request.trim().split('\n')
+}
+
+const serializeTimerRequests = function(timerRequests) {
+  return timerRequests
+    .map(normalizeRequest)
+    .sort()
+    .join('\n')
 }
 
 const normalizeRequest = function(request) {
