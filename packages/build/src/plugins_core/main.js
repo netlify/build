dc6a68db6d7e64c3b78609827fc5379c32d187d2
ehmicky <ehmicky@gmail.com>
2020-08-14 17:36:26 +0200

    Refactor core plugins logic

--- packages/build/src/plugins_core/main.js
+++ packages/build/src/plugins_core/main.js
@@ -1,25 +1,43 @@
+const { LOCAL_INSTALL_PLUGIN_NAME } = require('../install/local')
+
 const FUNCTIONS_INSTALL_PLUGIN = `${__dirname}/functions_install/plugin.js`
 const FUNCTIONS_PLUGIN = `${__dirname}/functions/plugin.js`
 
-const { LOCAL_INSTALL_NAME } = require('../install/local')
+// List of core plugin names
+const FUNCTIONS_PLUGIN_NAME = '@netlify/plugin-functions-core'
+const FUNCTIONS_INSTALL_PLUGIN_NAME = '@netlify/plugin-functions-install-core'
+const CORE_PLUGINS = [FUNCTIONS_PLUGIN_NAME, FUNCTIONS_INSTALL_PLUGIN_NAME, LOCAL_INSTALL_PLUGIN_NAME]
 
 // Plugins that are installed and enabled by default
-const getCorePlugins = FUNCTIONS_SRC => [
-  // When no "Functions directory" is defined, it means users does not use
-  // Netlify Functions.
-  // However when it is defined but points to a non-existing directory, this
-  // might mean the directory is created later one, so we cannot do that check
-  // yet.
-  ...(FUNCTIONS_SRC === undefined
-    ? []
-    : [{ package: '@netlify/plugin-functions-install-core', pluginPath: FUNCTIONS_INSTALL_PLUGIN, optional: true }]),
-  ...(FUNCTIONS_SRC === undefined ? [] : [{ package: '@netlify/plugin-functions-core', pluginPath: FUNCTIONS_PLUGIN }]),
-]
+const getCorePlugins = function({ constants: { FUNCTIONS_SRC } }) {
+  const functionsPlugin = getFunctionsPlugin(FUNCTIONS_SRC)
+  const functionsInstallPlugin = getFunctionsInstallPlugin(FUNCTIONS_SRC)
+  return [functionsPlugin, functionsInstallPlugin].filter(Boolean)
+}
 
-const CORE_PLUGINS = ['@netlify/plugin-functions-install-core', '@netlify/plugin-functions-core']
+// When no "Functions directory" is defined, it means users does not use
+// Netlify Functions.
+// However when it is defined but points to a non-existing directory, this
+// might mean the directory is created later one, so we cannot do that check
+// yet.
+const getFunctionsPlugin = function(FUNCTIONS_SRC) {
+  if (FUNCTIONS_SRC === undefined) {
+    return
+  }
 
-// Those core plugins cannot be handled like regular plugins because they must
-// be run before plugin child processes start
-const EARLY_CORE_PLUGINS = [LOCAL_INSTALL_NAME]
+  return { package: FUNCTIONS_PLUGIN_NAME, pluginPath: FUNCTIONS_PLUGIN }
+}
 
-module.exports = { getCorePlugins, CORE_PLUGINS, EARLY_CORE_PLUGINS }
+const getFunctionsInstallPlugin = function(FUNCTIONS_SRC) {
+  if (FUNCTIONS_SRC === undefined) {
+    return
+  }
+
+  return { package: FUNCTIONS_INSTALL_PLUGIN_NAME, pluginPath: FUNCTIONS_INSTALL_PLUGIN, optional: true }
+}
+
+const isCorePlugin = function(package) {
+  return CORE_PLUGINS.includes(package)
+}
+
+module.exports = { getCorePlugins, isCorePlugin }
