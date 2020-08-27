1ecce1f98245234e62a54f66f455f81564f98208
ehmicky <ehmicky@users.noreply.github.com>
2020-08-27 17:34:40 +0200

    Remove `create-react-app` warning (#1775)

--- packages/build/src/commands/build_command.js
+++ packages/build/src/commands/build_command.js
@@ -1,13 +1,11 @@
 const { platform } = require('process')
 
 const execa = require('execa')
-const isPlainObj = require('is-plain-obj')
 
 const { setEnvChanges } = require('../env/changes.js')
 const { addErrorInfo } = require('../error/info')
-const { logBuildCommandStart, logCiReactWarning } = require('../log/main')
+const { logBuildCommandStart } = require('../log/main')
 const { getBuildCommandStdio, handleBuildCommandOutput } = require('../log/stream')
-const { getPackageJson } = require('../utils/package')
 
 // Fire `build.command`
 const fireBuildCommand = async function({
@@ -40,51 +38,12 @@ const fireBuildCommand = async function({
     return {}
   } catch (newError) {
     handleBuildCommandOutput(newError, logs)
-    await handleBuildCommandError({
-      error: newError,
-      buildCommand,
-      buildCommandOrigin,
-      configPath,
-      buildDir,
-      env,
-      logs,
-    })
+    addErrorInfo(newError, { type: 'buildCommand', location: { buildCommand, buildCommandOrigin, configPath } })
     return { newError }
   }
 }
 
 // We use Bash on Unix and `cmd.exe` on Windows
 const SHELL = platform === 'win32' ? true : 'bash'
-
-// When `build.command` fails
-const handleBuildCommandError = async function({
-  error,
-  buildCommand,
-  buildCommandOrigin,
-  configPath,
-  buildDir,
-  env,
-  logs,
-}) {
-  addErrorInfo(error, { type: 'buildCommand', location: { buildCommand, buildCommandOrigin, configPath } })
-
-  if (await isCiReactError({ error, env, buildDir })) {
-    logCiReactWarning(logs)
-  }
-}
-
-const isCiReactError = async function({ error, env: { CI }, buildDir }) {
-  return (
-    error.exitCode === 1 && typeof CI === 'string' && CI.toLowerCase() !== 'false' && (await isCreateReactApp(buildDir))
-  )
-}
-
-const isCreateReactApp = async function(buildDir) {
-  const { packageJson: sitePackageJson } = await getPackageJson(buildDir, { normalize: false })
-  const { scripts } = sitePackageJson
-  return isPlainObj(scripts) && typeof scripts.build === 'string' && scripts.build.startsWith(CREATE_REACT_APP_BUILD)
-}
-
-const CREATE_REACT_APP_BUILD = 'react-scripts build'
 
 module.exports = { fireBuildCommand }
