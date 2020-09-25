8130d5c79d46294f28dea04dc094653d548fbebf
ehmicky <ehmicky@gmail.com>
2020-09-25 22:03:13 +0200

    Improve error handling of the buildbot socket

---
+++ packages/build/src/utils/errors.js
@@ -0,0 +1,14 @@
+// Wrap an async function so it prepends an error message on exceptions.
+// This helps locate errors.
+const addAsyncErrorMessage = function(asyncFunc, message) {
+  return async (...args) => {
+    try {
+      return await asyncFunc(...args)
+    } catch (error) {
+      error.stack = `${message}: ${error.stack}`
+      throw error
+    }
+  }
+}
+
+module.exports = { addAsyncErrorMessage }
