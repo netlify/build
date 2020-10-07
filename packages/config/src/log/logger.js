f5525999c09d1a423be040faeb49558de7703e14
ehmicky <ehmicky@gmail.com>
2020-10-07 21:42:20 +0200

    Use consistent case in \u escapes

--- packages/config/src/log/logger.js
+++ packages/config/src/log/logger.js
@@ -31,7 +31,7 @@ const log = function(logs, string) {
 // We need to add a zero width space character in empty lines. Otherwise the
 // buildbot removes those due to a bug: https://github.com/netlify/buildbot/issues/595
 const EMPTY_LINES_REGEXP = /^\s*$/gm
-const EMPTY_LINE = '\u{200b}'
+const EMPTY_LINE = '\u{200B}'
 
 const logObject = function(logs, object) {
   const string = serializeObject(object)
