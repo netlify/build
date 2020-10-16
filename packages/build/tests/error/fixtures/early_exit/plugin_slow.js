80b253918390adac19a174bdd808c3c070c3cb43
ehmicky <ehmicky@gmail.com>
2020-10-16 19:31:52 +0200

    Add `no-await-in-loop` ESLint rule

--- packages/build/tests/error/fixtures/early_exit/plugin_slow.js
+++ packages/build/tests/error/fixtures/early_exit/plugin_slow.js
@@ -11,8 +11,9 @@ module.exports = {
 
     // Signals are async, so we need to wait for the child process to exit
     // The while loop is required due to `await`
-    // eslint-disable-next-line fp/no-loops
+    // eslint-disable-next-line fp/no-loops, no-await-in-loop
     while (await processExists(env.TEST_PID)) {
+      // eslint-disable-next-line no-await-in-loop
       await pSetTimeout(1e2)
     }
   },
