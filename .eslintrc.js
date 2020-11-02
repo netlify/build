1adde73ba022d05157b0974968d0a1d2713b98d9
ehmicky <ehmicky@gmail.com>
2020-11-02 15:52:04 +0100

    Use new `eslint-config-node`

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,61 +3,16 @@
 const { overrides } = require('@netlify/eslint-config-node')
 
 module.exports = {
-  extends: [
-    '@netlify/eslint-config-node',
-    'plugin:import/recommended',
-    'plugin:promise/recommended',
-    'plugin:fp/recommended',
-  ],
-  parserOptions: {
-    sourceType: 'script',
-  },
+  extends: '@netlify/eslint-config-node',
   rules: {
     strict: 2,
 
     // eslint-plugin-ava needs to know where test files are located
     'ava/no-ignored-test-files': [2, { files: ['tests/**/*.js', '!tests/{helpers,fixtures}/**/*.{js,json}'] }],
     'ava/no-import-test-files': [2, { files: ['tests/**/*.js', '!tests/{helpers,fixtures}/**/*.{js,json}'] }],
-
-    'promise/no-callback-in-promise': 2,
-    'promise/no-nesting': 2,
-    'promise/no-promise-in-callback': 2,
-    'promise/no-return-in-finally': 2,
-    'promise/prefer-await-to-callbacks': 2,
-    'promise/prefer-await-to-then': 2,
-    'promise/valid-params': 2,
-
-    // Those rules are too strict
-    'fp/no-rest-parameters': 0,
-    'fp/no-unused-expression': 0,
-    'fp/no-nil': 0,
-    'fp/no-throw': 0,
-
-    'import/extensions': [2, 'always', { ignorePackages: true }],
-    'import/newline-after-import': 2,
-    'import/no-amd': 2,
-    'import/no-anonymous-default-export': 2,
-    'import/no-cycle': [2, { commonjs: true }],
-    'import/no-deprecated': 2,
-    'import/no-dynamic-require': 2,
-    'import/no-extraneous-dependencies': 2,
-    'import/no-mutable-exports': 2,
-    'import/no-named-default': 2,
-    'import/no-namespace': 2,
-    'import/no-self-import': 2,
-    'import/no-unassigned-import': [2, { allow: ['*polyfill*', '**/*polyfill*', 'log-process-errors/**'] }],
-    'import/no-unresolved': [2, { commonjs: true }],
-    'import/no-useless-path-segments': [2, { commonjs: true }],
   },
   overrides: [
     ...overrides,
-    {
-      files: ['**/*.md'],
-      rules: {
-        strict: 0,
-        'import/no-unresolved': 0,
-      },
-    },
     {
       files: ['**/fixtures/**/*.js'],
       rules: {
