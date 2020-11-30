827efa4ab7ae8104422205962371fa59fdf409a2
ehmicky <ehmicky@gmail.com>
2020-11-30 20:06:51 +0100

    Improve error message when plugin package is wrong

--- packages/build/src/plugins/manifest/path.js
+++ packages/build/src/plugins/manifest/path.js
@@ -5,20 +5,20 @@ const locatePath = require('locate-path')
 const { addErrorInfo } = require('../../error/info')
 
 // Retrieve "manifest.yml" path for a specific plugin
-const getManifestPath = async function (pluginDir, packageDir) {
+const getManifestPath = async function ({ pluginDir, packageDir, packageName }) {
   const dirs = [pluginDir, packageDir].filter(Boolean).map((dir) => `${dir}/${MANIFEST_FILENAME}`)
   const manifestPath = await locatePath(dirs)
-  validateManifestExists(manifestPath)
+  validateManifestExists(manifestPath, packageName)
   return manifestPath
 }
 
-const validateManifestExists = function (manifestPath) {
+const validateManifestExists = function (manifestPath, packageName) {
   if (manifestPath !== undefined) {
     return
   }
 
   const error = new Error(
-    `This plugin is missing a "manifest.yml".
+    `The plugin "${packageName}" is missing a "manifest.yml".
 This might mean:
   - The plugin "package" name is misspelled
   - The plugin "package" points to a Node module that is not a Netlify Build plugin
