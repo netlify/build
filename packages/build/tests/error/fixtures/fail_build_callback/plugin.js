3fc79ebf01e14bf1c9aefe18038d0a741b77ed03
ehmicky <ehmicky@users.noreply.github.com>
2021-01-20 20:55:34 +0100

    Upgrade to Node 15 (#2012)

--- packages/build/tests/error/fixtures/fail_build_callback/plugin.js
+++ packages/build/tests/error/fixtures/fail_build_callback/plugin.js
@@ -3,6 +3,7 @@
 const { nextTick } = require('process')
 const { promisify } = require('util')
 
+// TODO: replace with `timers/promises` after dropping Node < 15.0.0
 const pSetTimeout = promisify(setTimeout)
 
 module.exports = {
