c3bc9a9a4d4018424becf25a11517408ea86b3ae
ehmicky <ehmicky@gmail.com>
2021-02-03 17:31:06 +0100

    feat: remove deploy feature flag

--- packages/build/src/core/feature_flags.js
+++ packages/build/src/core/feature_flags.js
@@ -15,11 +15,6 @@ const getFeatureFlag = function (name) {
 }
 
 const DEFAULT_FEATURE_FLAGS = {
-  // When `true`, triggers a deploy by connecting to the buildbot deploy server
-  // and passing it a "deploySite" command. Netlify Build then waits for the
-  // buildbot to finish its deploy before running the "onSuccess" and
-  // "onEnd" hooks.
-  service_buildbot_enable_deploy_server: false,
   // When `true`, the Deploy preview commenting core plugin is enabled
   dpc: false,
 }
