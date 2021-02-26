9beea68ad168a454b214a149a466d71fe403b74a
ehmicky <ehmicky@users.noreply.github.com>
2021-02-26 19:47:29 +0100

    feat: print warnings when using `compatibility` versions (#2319)

--- packages/build/src/plugins/compatibility.js
+++ packages/build/src/plugins/compatibility.js
@@ -8,25 +8,40 @@ const getExpectedVersion = function ({ latestVersion, compatibility, nodeVersion
   const matchingCondition = compatibility.find(({ conditions }) => matchesCompatField({ conditions, nodeVersion }))
 
   if (matchingCondition === undefined) {
-    return latestVersion
+    return { expectedVersion: latestVersion }
   }
 
-  return matchingCondition.version
+  const { version: expectedVersion, conditions } = matchingCondition
+  const compatWarning = getCompatWarning(conditions)
+  return { expectedVersion, compatWarning }
 }
 
 const matchesCompatField = function ({ conditions, nodeVersion }) {
-  return conditions.every(({ type, condition }) => CONDITIONS[type](condition, { nodeVersion }))
+  return conditions.every(({ type, condition }) => CONDITIONS[type].test(condition, { nodeVersion }))
+}
+
+// Retrieve warning message shown when using an older version with `compatibility`
+const getCompatWarning = function (conditions) {
+  return conditions.map(getConditionWarning).join(', ')
+}
+
+const getConditionWarning = function ({ type, condition }) {
+  return CONDITIONS[type].warning(condition)
 }
 
 // Plugins can use `compatibility.{version}.nodeVersion: 'allowedNodeVersion'`
 // to deliver different plugin versions based on the Node.js version
-const nodeVersionCondition = function (allowedNodeVersion, { nodeVersion }) {
+const nodeVersionTest = function (allowedNodeVersion, { nodeVersion }) {
   return satisfies(nodeVersion, allowedNodeVersion)
+}
+
+const nodeVersionWarning = function (allowedNodeVersion) {
+  return `Node.js ${allowedNodeVersion}`
 }
 
 // TODO: add some condition types: `dependencies`
 const CONDITIONS = {
-  nodeVersion: nodeVersionCondition,
+  nodeVersion: { test: nodeVersionTest, warning: nodeVersionWarning },
 }
 
 module.exports = { getExpectedVersion }
