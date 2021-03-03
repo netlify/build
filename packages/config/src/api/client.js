6de33fa726e2cc6f56fe34165b64e3f7aca10e20
ehmicky <ehmicky@gmail.com>
2021-03-03 20:39:16 +0100

    chore: debug

--- packages/config/src/api/client.js
+++ packages/config/src/api/client.js
@@ -5,11 +5,13 @@ const NetlifyAPI = require('netlify')
 const { removeFalsy } = require('../utils/remove_falsy')
 
 // Retrieve Netlify API client, if an access token was passed
+// eslint-disable-next-line complexity
 const getApiClient = function ({ token, offline, testOpts = {}, host, scheme, pathPrefix }) {
   if (!token || offline) {
     return
   }
 
+  console.log({ testOpts, host, scheme, pathPrefix }, testOpts.host || host)
   // TODO: find less intrusive way to mock HTTP requests
   const parameters = removeFalsy({ scheme: testOpts.scheme || scheme, host: testOpts.host || host, pathPrefix })
   const api = new NetlifyAPI(token, parameters)
