822a8bdca8b2b6e993809b0a0aa495479d94b268
Eduardo Bouças <mail@eduardoboucas.com>
2021-03-18 15:30:14 +0000

    chore: fix test

--- packages/build/tests/core/fixtures/functions_config_3/functions/function_one.js
+++ packages/build/tests/core/fixtures/functions_config_3/functions/function_one.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const module1 = require('@netlify/imaginary-module-one')
-const module3 = require('@netlify/imaginary-module-three')
 const module2 = require('@netlify/imaginary-module-two')
 
-module.exports = () => [module1, module2, module3]
+module.exports = () => [module1, module2]
