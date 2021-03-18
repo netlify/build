68ea13648dee061d1db9b7bbeb53d1ac7ad99f99
Eduardo Bouças <mail@eduardoboucas.com>
2021-03-18 14:49:11 +0000

    chore: simplify config normalisation logic

--- packages/build/src/plugins_core/functions/index.js
+++ packages/build/src/plugins_core/functions/index.js
@@ -17,39 +17,25 @@ const {
 
 const { getZipError } = require('./error')
 
-const normalizeFunctionConfig = (functionConfig) => {
-  // The function configuration keys returned by @netlify/config are not an
-  // exact match to the properties that @netlify/zip-it-and-ship-it expects.
-  // We do that translation according to this map.
-  const zisiConfigMap = {
-    external_node_modules: 'externalNodeModules',
-    ignored_node_modules: 'ignoredNodeModules',
-    node_bundler: 'nodeBundler',
-  }
-  const normalizedConfig = mapObject(functionConfig, (key, value) => {
-    const normalizedKey = zisiConfigMap[key] || key
+// The function configuration keys returned by @netlify/config are not an exact
+// match to the properties that @netlify/zip-it-and-ship-it expects. We do that
+// translation here.
+const normalizeFunctionConfig = (functionConfig = {}) => ({
+  externalNodeModules: functionConfig.external_node_modules,
+  ignoredNodeModules: functionConfig.ignored_node_modules,
 
-    // When the user selects esbuild as the Node bundler, we still want to use
-    // the legacy ZISI bundler as a fallback. Rather than asking the user to
-    // make this decision, we abstract that complexity away by injecting the
-    // fallback behavior ourselves. We do this by transforming the value
-    // `esbuild` into `esbuild_zisi`, which zip-it-and-ship-it understands.
-    if (normalizedKey === 'nodeBundler' && value === 'esbuild') {
-      return [normalizedKey, 'esbuild_zisi']
-    }
-
-    return [normalizedKey, value]
-  })
-
-  return normalizedConfig
-}
+  // When the user selects esbuild as the Node bundler, we still want to use
+  // the legacy ZISI bundler as a fallback. Rather than asking the user to
+  // make this decision, we abstract that complexity away by injecting the
+  // fallback behavior ourselves. We do this by transforming the value
+  // `esbuild` into `esbuild_zisi`, which zip-it-and-ship-it understands.
+  nodeBundler: functionConfig.node_bundler === 'esbuild' ? 'esbuild_zisi' : functionConfig.node_bundler,
+})
 
 const getZisiParameters = ({ functionsConfig }) => {
-  const parameters = {
-    config: mapObject(functionsConfig, (expression, object) => [expression, normalizeFunctionConfig(object)]),
-  }
+  const config = mapObject(functionsConfig, (expression, object) => [expression, normalizeFunctionConfig(object)])
 
-  return parameters
+  return { config }
 }
 
 const zipFunctionsAndLogResults = async ({ functionsConfig, functionsDist, functionsSrc, logs }) => {
