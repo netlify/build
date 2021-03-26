e9a8b95dd818b79474ee2068a341a5266d9a3e6b
ehmicky <ehmicky@gmail.com>
2021-03-26 21:03:15 +0100

    feat: improve how lingering processes are filtered out

--- packages/build/src/core/lingering.js
+++ packages/build/src/core/lingering.js
@@ -11,24 +11,48 @@ const warnOnLingeringProcesses = async function ({ mode, logs, testOpts: { silen
     return
   }
 
-  const {
-    stdout,
-  } = await execa(
-    'ps axho command | grep -v ps | grep -v grep | grep -v bash | grep -v "/opt/build-bin/buildbot" | grep -v defunct | grep -v "\\[build\\]" | grep -v "@netlify/build" | grep -v "buildbot.*\\[node\\]" | grep -v "gatsby-telemetry" | grep -v "jest-worker" | grep -v "broccoli-babel-transpiler"',
-    { shell: 'bash', reject: false },
-  )
+  const { stdout } = await execa('ps', ['axho', 'command'])
 
-  const processes = stdout.trim().split('\n').filter(isNotEmptyLine)
+  const commands = stdout.trim().split('\n').filter(isNotEmptyLine).filter(isNotIgnoredCommand)
 
-  if (processes.length === 0) {
+  if (commands.length === 0) {
     return
   }
 
-  logLingeringProcesses(logs, processes)
+  logLingeringProcesses(logs, commands)
 }
 
 const isNotEmptyLine = function (line) {
   return line.trim() !== ''
 }
+
+// We ignore any command known to be internal to the buildbot.
+// We also ignore commands known not to complete properly in builds if they are
+// widely used.
+const isNotIgnoredCommand = function (command) {
+  return !IGNORED_COMMANDS.some((ignoredCommand) => matchesIgnoredCommand(command, ignoredCommand))
+}
+
+const matchesIgnoredCommand = function (command, ignoredCommand) {
+  if (typeof ignoredCommand === 'string') {
+    return command.includes(ignoredCommand)
+  }
+
+  return ignoredCommand.test(command)
+}
+
+const IGNORED_COMMANDS = [
+  'ps',
+  'grep',
+  'bash',
+  '/opt/build-bin/buildbot',
+  'defunct',
+  '[build]',
+  '@netlify/build',
+  /buildbot.*\[node]/,
+  'gatsby-telemetry',
+  'jest-worker',
+  'broccoli-babel-transpiler',
+]
 
 module.exports = { warnOnLingeringProcesses }
