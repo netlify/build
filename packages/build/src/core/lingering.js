d805361b505d130acd7b00b3ab3bb70ba29a0ccd
ehmicky <ehmicky@users.noreply.github.com>
2021-03-26 16:36:37 +0100

    feat: improve lingering processes list (#2475)

--- packages/build/src/core/lingering.js
+++ packages/build/src/core/lingering.js
@@ -14,24 +14,15 @@ const warnOnLingeringProcesses = async function ({ mode, logs, testOpts: { silen
   const {
     stdout: processList,
   } = await execa(
-    'ps aux | grep -v ps | grep -v grep | grep -v bash | grep -v "/opt/build-bin/buildbot" | grep -v defunct | grep -v "\\[build\\]" | grep -v "@netlify/build" | grep -v "buildbot.*\\[node\\]" | grep -v "gatsby-telemetry" | grep -v "jest-worker" | grep -v "broccoli-babel-transpiler"',
+    'ps axho command | grep -v ps | grep -v grep | grep -v bash | grep -v "/opt/build-bin/buildbot" | grep -v defunct | grep -v "\\[build\\]" | grep -v "@netlify/build" | grep -v "buildbot.*\\[node\\]" | grep -v "gatsby-telemetry" | grep -v "jest-worker" | grep -v "broccoli-babel-transpiler"',
     { shell: 'bash' },
   )
 
-  if (!hasLingeringProcesses(processList)) {
+  if (processList.trim() === '') {
     return
   }
 
   logLingeringProcesses(logs, processList)
-}
-
-// Note that `ps aux` has a header line which is always printed.
-const hasLingeringProcesses = function (processList) {
-  return processList.split('\n').filter(isNotEmptyLine).length > 1
-}
-
-const isNotEmptyLine = function (line) {
-  return line.trim() !== ''
 }
 
 module.exports = { warnOnLingeringProcesses }
