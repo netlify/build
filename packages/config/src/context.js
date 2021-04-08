a642a9d36f24dc5c93e43304858007c524035b71
ehmicky <ehmicky@users.noreply.github.com>
2021-04-08 14:23:23 +0200

    feat: improve how context-specific config are merged (#2555)

--- packages/config/src/context.js
+++ packages/config/src/context.js
@@ -7,32 +7,7 @@ const { CONFIG_ORIGIN } = require('./origin')
 const { deepMerge } = require('./utils/merge')
 const { validatePreContextConfig } = require('./validate/main')
 
-// Takes a config object and adds each key to a namespace. This namespace is
-// usually `build`, since `context.*.{key}` is merged to `build.{key}`. The
-// only exception to that is for the `functions` key if the value is a plain
-// object, so that `context.*.functions.{key}` is merged to `functions.{key}`.
-const addNamespace = (context) => {
-  const namespacedObject = Object.entries(context).reduce((result, [key, value]) => {
-    if (key === 'functions' && isPlainObj(value)) {
-      return {
-        ...result,
-        functions: value,
-      }
-    }
-
-    return {
-      ...result,
-      build: {
-        ...result.build,
-        [key]: value,
-      },
-    }
-  }, {})
-
-  return namespacedObject
-}
-
-// Merge `config.context.{CONTEXT|BRANCH}.*` to `config.build.*`
+// Merge `config.context.{CONTEXT|BRANCH}.*` to `config.build.*` or `config.*`
 // CONTEXT is the `--context` CLI flag.
 // BRANCH is the `--branch` CLI flag.
 const mergeContext = function (config, context, branch) {
@@ -50,6 +25,39 @@ const mergeContext = function (config, context, branch) {
     .map((contextConfig) => normalizeBeforeConfigMerge(contextConfig, CONFIG_ORIGIN))
 
   return deepMerge(configA, ...allContextProps)
+}
+
+// `config.context.{context}.*` properties are merged either to `config.*` or
+// to `config.build.*`. We distinguish between both by checking the property
+// name.
+const addNamespace = (contextProps) => Object.entries(contextProps).reduce(addNamespacedProperty, {})
+
+const addNamespacedProperty = function (contextProps, [key, value]) {
+  return isBuildProperty(key, value)
+    ? { ...contextProps, build: { ...contextProps.build, [key]: value } }
+    : { ...contextProps, [key]: value }
+}
+
+const isBuildProperty = function (key, value) {
+  return BUILD_PROPERTIES.has(key) && !isFunctionsConfig(key, value)
+}
+
+// All properties in `config.build.*`
+const BUILD_PROPERTIES = new Set([
+  'base',
+  'command',
+  'edge_handlers',
+  'environment',
+  'functions',
+  'ignore',
+  'processing',
+  'publish',
+])
+
+// `config.functions` is a plain object while `config.build.functions` is a
+// string.
+const isFunctionsConfig = function (key, value) {
+  return key === 'functions' && isPlainObj(value)
 }
 
 module.exports = { mergeContext }
