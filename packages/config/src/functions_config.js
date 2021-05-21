01a857c7efe203b522ba28a8b4c9261b495bb6c5
ehmicky <ehmicky@users.noreply.github.com>
2021-05-21 16:06:39 +0200

    chore: add default `config.functions.*` (#2826)

--- packages/config/src/functions_config.js
+++ packages/config/src/functions_config.js
@@ -3,35 +3,16 @@
 const isPlainObj = require('is-plain-obj')
 
 const bundlers = ['esbuild', 'zisi']
-const configProperties = new Set([
-  'directory',
-  'external_node_modules',
-  'ignored_node_modules',
-  'included_files',
-  'node_bundler',
-])
-
 const WILDCARD_ALL = '*'
 
-const isConfigLeaf = (obj) => isPlainObj(obj) && Object.keys(obj).every(isConfigProperty)
-
-const isConfigProperty = (prop) => configProperties.has(prop)
-
-// Takes a functions configuration object and looks for the functions directory
-// definition, returning it under the `directory` key. The rest of the config
-// object is returned under the `functions` key.
-const extractFunctionsDirectory = (functions = {}) => {
-  const { [WILDCARD_ALL]: topLevelConfig = {}, ...otherFunctions } = functions
-  const { directory, ...topLevelConfigKeys } = topLevelConfig
-  const newTopLevelConfig = Object.keys(topLevelConfigKeys).length === 0 ? {} : { [WILDCARD_ALL]: topLevelConfigKeys }
-
-  return {
-    directory,
-    functions: {
-      ...otherFunctions,
-      ...newTopLevelConfig,
-    },
-  }
+// Removing the legacy `functions` from the `build` block.
+// Looking for a default directory in the `functions` block, separating it
+// from the rest of the configuration if it exists.
+const normalizeFunctionsProps = function ({ functions: v1FunctionsDirectory, ...build }, functions) {
+  const functionsA = Object.entries(functions).reduce(normalizeFunctionsProp, {})
+  const { directory: functionsDirectory, functions: functionsB } = extractFunctionsDirectory(functionsA)
+  const functionsDirectoryProps = getFunctionsDirectoryProps({ functionsDirectory, v1FunctionsDirectory })
+  return { build, functions: functionsB, functionsDirectoryProps }
 }
 
 // Normalizes a functions configuration object, so that the first level of keys
@@ -48,30 +29,53 @@ const extractFunctionsDirectory = (functions = {}) => {
 //   "*": { "external_node_modules": ["one"] },
 //   "my-function": { "external_node_modules": ["two"] }
 // }
-const normalize = (functions) => {
-  const normalizedFunctions = Object.keys(functions).reduce((result, prop) => {
-    // If `prop` is one of `configProperties``, we might be dealing with a
-    // top-level property (i.e. one that targets all functions). We consider
-    // that to be the case if the value is an object where all keys are also
-    // config properties. If not, it's simply a function with the same name
-    // as one of the config properties.
-    if (isConfigProperty(prop) && !isConfigLeaf(functions[prop])) {
-      return {
-        ...result,
-        [WILDCARD_ALL]: {
-          ...result[WILDCARD_ALL],
-          [prop]: functions[prop],
-        },
-      }
-    }
+// If `prop` is one of `configProperties``, we might be dealing with a
+// top-level property (i.e. one that targets all functions). We consider
+// that to be the case if the value is an object where all keys are also
+// config properties. If not, it's simply a function with the same name
+// as one of the config properties.
+const normalizeFunctionsProp = (functions, [propName, propValue]) =>
+  isConfigProperty(propName) && !isConfigLeaf(propValue)
+    ? { ...functions, [WILDCARD_ALL]: { ...functions[WILDCARD_ALL], [propName]: propValue } }
+    : { ...functions, [propName]: propValue }
 
+const isConfigLeaf = (functionConfig) =>
+  isPlainObj(functionConfig) && Object.keys(functionConfig).every(isConfigProperty)
+
+const isConfigProperty = (propName) => configProperties.has(propName)
+
+const configProperties = new Set([
+  'directory',
+  'external_node_modules',
+  'ignored_node_modules',
+  'included_files',
+  'node_bundler',
+])
+
+// Takes a functions configuration object and looks for the functions directory
+// definition, returning it under the `directory` key. The rest of the config
+// object is returned under the `functions` key.
+const extractFunctionsDirectory = ({ [WILDCARD_ALL]: { directory, ...wildcardFunctionsConfig }, ...functions }) => ({
+  directory,
+  functions: { ...functions, [WILDCARD_ALL]: wildcardFunctionsConfig },
+})
+
+const getFunctionsDirectoryProps = ({ functionsDirectory, v1FunctionsDirectory }) => {
+  if (functionsDirectory) {
     return {
-      ...result,
-      [prop]: functions[prop],
+      functionsDirectory,
+      functionsDirectoryOrigin: 'config',
     }
-  }, {})
+  }
 
-  return normalizedFunctions
+  if (v1FunctionsDirectory) {
+    return {
+      functionsDirectory: v1FunctionsDirectory,
+      functionsDirectoryOrigin: 'config-v1',
+    }
+  }
+
+  return {}
 }
 
-module.exports = { bundlers, extractFunctionsDirectory, normalize, WILDCARD_ALL }
+module.exports = { normalizeFunctionsProps, bundlers, WILDCARD_ALL }
--- packages/config/src/normalize.js
+++ packages/config/src/normalize.js
@@ -1,47 +1,20 @@
 'use strict'
 
-const { extractFunctionsDirectory, normalize: normalizeFunctionsConfig } = require('./functions_config')
+const { normalizeFunctionsProps, WILDCARD_ALL } = require('./functions_config')
 const { mergeConfigs } = require('./utils/merge')
 const { removeFalsy } = require('./utils/remove_falsy')
-
-const addFunctionsDirectory = ({ functionsDirectory, v1FunctionsDirectory }) => {
-  if (functionsDirectory) {
-    return {
-      functionsDirectory,
-      functionsDirectoryOrigin: 'config',
-    }
-  }
-
-  if (v1FunctionsDirectory) {
-    return {
-      functionsDirectory: v1FunctionsDirectory,
-      functionsDirectoryOrigin: 'config-v1',
-    }
-  }
-
-  return {}
-}
 
 // Normalize configuration object
 const normalizeConfig = function (config) {
   const { build, functions, plugins, ...configA } = mergeConfigs([DEFAULT_CONFIG, config])
-
-  // Removing the legacy `functions` from the `build` block.
-  const { functions: v1FunctionsDirectory, ...buildB } = build
-  const functionsConfig = normalizeFunctionsConfig(functions)
-
-  // Looking for a default directory in the `functions` block, separating it
-  // from the rest of the configuration if it exists.
-  const { directory: functionsDirectory, functions: functionsConfigA } = extractFunctionsDirectory(functionsConfig)
-  const functionsProperties = addFunctionsDirectory({ functionsDirectory, v1FunctionsDirectory })
+  const { build: buildA, functions: functionsA, functionsDirectoryProps } = normalizeFunctionsProps(build, functions)
   const pluginsA = plugins.map(normalizePlugin)
-
-  return { ...configA, build: buildB, functions: functionsConfigA, plugins: pluginsA, ...functionsProperties }
+  return { ...configA, build: buildA, functions: functionsA, plugins: pluginsA, ...functionsDirectoryProps }
 }
 
 const DEFAULT_CONFIG = {
   build: { environment: {} },
-  functions: {},
+  functions: { [WILDCARD_ALL]: {} },
   plugins: [],
 }
 
