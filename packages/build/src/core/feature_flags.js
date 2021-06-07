5a2e2f2dc96112f32a31ae1dba3c46fd9f23de82
João Antunes <joao@netlify.com>
2021-06-07 18:23:53 +0100

    feat(metrics): report distribution metrics under a feature flag (#2933)

--- packages/build/src/core/feature_flags.js
+++ packages/build/src/core/feature_flags.js
@@ -19,6 +19,7 @@ const getFeatureFlag = function (name) {
 // Default values for feature flags
 const DEFAULT_FEATURE_FLAGS = {
   zisiHandlerV2: env.NETLIFY_EXPERIMENTAL_FUNCTION_HANDLER_V2 === 'true',
+  buildbot_build_distribution_metrics: false,
 }
 
 module.exports = { normalizeFeatureFlags, DEFAULT_FEATURE_FLAGS }
