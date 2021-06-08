0b4b58bbdd1e5a9aa82b185404920f3c353b54f5
ehmicky <ehmicky@users.noreply.github.com>
2021-06-08 18:43:44 +0200

    feat: allow mutating `functions.*` top-level properties (#2944)

--- packages/build/tests/plugins/fixtures/config_mutate_functions_any/plugin.js
+++ packages/build/tests/plugins/fixtures/config_mutate_functions_any/plugin.js
@@ -2,8 +2,22 @@
 
 module.exports = {
   /* eslint-disable no-param-reassign */
+  // eslint-disable-next-line max-statements
   onPreBuild({ netlifyConfig }) {
+    netlifyConfig.functions.node_bundler = 'zisi'
+    console.log(netlifyConfig.functions['*'].node_bundler)
+    netlifyConfig.functions.external_node_modules = ['test']
+    console.log(netlifyConfig.functions['*'].external_node_modules)
+    netlifyConfig.functions.ignored_node_modules = ['test']
+    console.log(netlifyConfig.functions['*'].ignored_node_modules)
+    netlifyConfig.functions.included_files = ['test']
+    console.log(netlifyConfig.functions['*'].included_files)
+
     netlifyConfig.functions['*'].node_bundler = 'zisi'
+    netlifyConfig.functions['*'].external_node_modules = []
+    netlifyConfig.functions['*'].ignored_node_modules = []
+    netlifyConfig.functions['*'].included_files = []
+
     netlifyConfig.functions.test.node_bundler = 'esbuild'
     netlifyConfig.functions.test.external_node_modules = []
     netlifyConfig.functions.test.ignored_node_modules = []
--- packages/build/tests/plugins/fixtures/config_mutate_functions_directory_nested/plugin.js
+++ packages/build/tests/plugins/fixtures/config_mutate_functions_directory_nested/plugin.js
@@ -5,5 +5,6 @@ module.exports = {
     // eslint-disable-next-line no-param-reassign
     netlifyConfig.functions.directory = 'test_functions'
     console.log(netlifyConfig.functionsDirectory)
+    console.log(netlifyConfig.functions['*'].directory)
   },
 }
