150d1b2572fae85b816aea07aeac42e040aff17e
JGAntunes <joao@netlify.com>
2021-06-23 17:38:43 +0100

    chore(tests): sets feature flag by default and add extra test scenarios

---
+++ packages/build/tests/core/fixtures/local_node_path_unused/plugin.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const {
+  execPath,
+  env: { TEST_NODE_PATH },
+} = require('process')
+
+module.exports = {
+  onPreBuild() {
+    console.log(`expect execPath to NOT equal TEST_NODE_PATH. Got '${execPath !== TEST_NODE_PATH}'`)
+  },
+}
