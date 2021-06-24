6773d58b9eece26a0b56811ea92b0a6bc59847ba
ehmicky <ehmicky@users.noreply.github.com>
2021-06-24 18:59:32 +0200

    feat: move proxy handler logic (#3101)

--- packages/build/tests/plugins/fixtures/config_mutate_functions_directory_build/plugin.js
+++ packages/build/tests/plugins/fixtures/config_mutate_functions_directory_build/plugin.js
@@ -4,6 +4,8 @@ module.exports = {
   onPreBuild({ netlifyConfig }) {
     // eslint-disable-next-line no-param-reassign
     netlifyConfig.build.functions = 'test_functions'
+  },
+  onBuild({ netlifyConfig }) {
     console.log(netlifyConfig.functionsDirectory)
   },
 }
--- packages/build/tests/plugins/fixtures/config_mutate_functions_directory_star/plugin.js
+++ packages/build/tests/plugins/fixtures/config_mutate_functions_directory_star/plugin.js
@@ -4,6 +4,8 @@ module.exports = {
   onPreBuild({ netlifyConfig }) {
     // eslint-disable-next-line no-param-reassign
     netlifyConfig.functions['*'].directory = 'test_functions'
+  },
+  onBuild({ netlifyConfig }) {
     console.log(netlifyConfig.functionsDirectory)
   },
 }
