c6b41db0e0b55de220cf9d3b4c761f881e9d6e36
ehmicky <ehmicky@gmail.com>
2021-07-07 20:23:44 +0200

    feat: do not allow modifying `netlifyConfig` parent properties

--- packages/build/tests/plugins/fixtures/config_mutate_processing_prop/plugin.js
+++ packages/build/tests/plugins/fixtures/config_mutate_processing_prop/plugin.js
@@ -3,14 +3,10 @@
 module.exports = {
   /* eslint-disable no-param-reassign */
   onPreBuild({ netlifyConfig }) {
-    netlifyConfig.build.processing.css = { bundle: true, minify: true }
     netlifyConfig.build.processing.css.bundle = true
     netlifyConfig.build.processing.css.minify = true
-    netlifyConfig.build.processing.html = { pretty_urls: true }
     netlifyConfig.build.processing.html.pretty_urls = true
-    netlifyConfig.build.processing.images = { compress: true }
     netlifyConfig.build.processing.images.compress = true
-    netlifyConfig.build.processing.js = { bundle: true, minify: true }
     netlifyConfig.build.processing.js.bundle = true
     netlifyConfig.build.processing.js.minify = true
     netlifyConfig.build.processing.skip_processing = true
--- packages/build/tests/plugins/tests.js
+++ packages/build/tests/plugins/tests.js
@@ -181,10 +181,6 @@ if (!version.startsWith('v8.')) {
     }
   })
 
-  test('netlifyConfig.processing can be assigned all at once', async (t) => {
-    await runFixture(t, 'config_mutate_processing_all')
-  })
-
   test('netlifyConfig.processing can be assigned individually', async (t) => {
     await runFixture(t, 'config_mutate_processing_prop')
   })
@@ -216,10 +212,6 @@ test('netlifyConfig.build.edge_handlers mutations are used by constants.EDGE_HAN
 
 test('netlifyConfig.edge_handlers can be assigned all at once', async (t) => {
   await runFixture(t, 'config_mutate_edge_handlers_all')
-})
-
-test('netlifyConfig.services can be assigned all at once', async (t) => {
-  await runFixture(t, 'config_mutate_services_all')
 })
 
 test('netlifyConfig.services can be assigned individually', async (t) => {
