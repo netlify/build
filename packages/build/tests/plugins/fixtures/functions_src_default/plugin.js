aea65f5add4e37f8b7c87f2895854c5c993585cf
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-14 15:29:05 +0100

    chore: adjust tests

--- packages/build/tests/plugins/fixtures/functions_src_default/plugin.js
+++ packages/build/tests/plugins/fixtures/functions_src_default/plugin.js
@@ -1,7 +1,8 @@
 'use strict'
 
 module.exports = {
-  onPreBuild({ constants: { FUNCTIONS_SRC } }) {
+  onPreBuild({ constants: { FUNCTIONS_SRC, INTERNAL_FUNCTIONS_SRC } }) {
     console.log(FUNCTIONS_SRC)
+    console.log(INTERNAL_FUNCTIONS_SRC)
   },
 }
