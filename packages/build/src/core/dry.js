b91f838305caf45d82abcbea276f6b5a5069b507
ehmicky <ehmicky@users.noreply.github.com>
2021-07-27 20:37:23 +0200

    chore: small code simplification (#3350)

--- packages/build/src/core/dry.js
+++ packages/build/src/core/dry.js
@@ -1,16 +1,15 @@
 'use strict'
 
+const pFilter = require('p-filter')
+
 const { logDryRunStart, logDryRunCommand, logDryRunEnd } = require('../log/messages/dry')
 const { runsOnlyOnBuildFailure } = require('../plugins/events')
 
 // If the `dry` flag is specified, do a dry run
 const doDryRun = async function ({ buildDir, commands, netlifyConfig, constants, buildbotServerSocket, logs }) {
-  const evaluatedConditions = await Promise.all(
-    commands.map(({ event, condition }) =>
-      shouldIncludeCommand({ buildDir, event, condition, netlifyConfig, constants, buildbotServerSocket }),
-    ),
+  const successCommands = await pFilter(commands, ({ event, condition }) =>
+    shouldIncludeCommand({ buildDir, event, condition, netlifyConfig, constants, buildbotServerSocket }),
   )
-  const successCommands = commands.filter((_, index) => evaluatedConditions[index] === true)
   const eventWidth = Math.max(...successCommands.map(getEventLength))
   const commandsCount = successCommands.length
 
