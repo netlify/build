2aa96413cdd3daf8fa73a9ac26ee2f6c85fc89b7
João Antunes <joao@netlify.com>
2021-08-18 11:56:37 +0100

    fix(cache-utils): don't cache junk files nor list them as cached (#3516)
    * fix(cache-utils): don't cache junk files nor list them as cached
    
    * chore(cache-utils): utility test helper code reuse

--- packages/cache-utils/tests/has.js
+++ packages/cache-utils/tests/has.js
@@ -4,7 +4,7 @@ const test = require('ava')
 
 const cacheUtils = require('..')
 
-const { createTmpDir, createTmpFile, removeFiles } = require('./helpers/main')
+const { createTmpDir, createTmpFile, createTmpFiles, removeFiles } = require('./helpers/main')
 
 test('Should allow checking if one file is cached', async (t) => {
   const [cacheDir, [srcFile, srcDir]] = await Promise.all([createTmpDir(), createTmpFile()])
@@ -29,6 +29,42 @@ test('Should allow checking if several files are cached', async (t) => {
     t.true(await cacheUtils.has([srcFile, otherSrcFile], { cacheDir }))
   } finally {
     await removeFiles([cacheDir, srcDir, otherSrcDir])
+  }
+})
+
+test('Should not list junk files as cached nor cache them', async (t) => {
+  const [cacheDir, [[junkFile, srcFile, otherSrcFile], srcFilesDir]] = await Promise.all([
+    createTmpDir(),
+    // Create 3 files under the same temporary dir, including a .DS_Store junk file
+    createTmpFiles([{ name: '.DS_Store' }, {}, {}]),
+  ])
+  try {
+    t.false(await cacheUtils.has(junkFile, { cacheDir }))
+    t.true(await cacheUtils.save(srcFilesDir, { cacheDir }))
+    t.false(await cacheUtils.has(junkFile, { cacheDir }))
+    t.true(await cacheUtils.has(srcFile, { cacheDir }))
+    t.true(await cacheUtils.has(otherSrcFile, { cacheDir }))
+  } finally {
+    await removeFiles([cacheDir, srcFilesDir])
+  }
+})
+
+test('Should not list empty directories as cached nor cache them', async (t) => {
+  const [cacheDir, [junkFile, junkFileDir], emptyDir] = await Promise.all([
+    createTmpDir(),
+    // Create a directory with a junk .DS_Store file
+    createTmpFile({ name: '.DS_Store' }),
+    // Create an empty directory
+    createTmpDir(),
+  ])
+  try {
+    t.false(await cacheUtils.has(junkFile, { cacheDir }))
+    t.false(await cacheUtils.save(junkFileDir, { cacheDir }))
+    t.false(await cacheUtils.has(junkFile, { cacheDir }))
+    t.false(await cacheUtils.save(emptyDir, { cacheDir }))
+    t.false(await cacheUtils.has(emptyDir, { cacheDir }))
+  } finally {
+    await removeFiles([cacheDir, junkFileDir, emptyDir])
   }
 })
 
