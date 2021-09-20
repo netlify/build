0ce7d3208aa706634fd6bbb939e36601766cc393
Eduardo Bouças <mail@eduardoboucas.com>
2021-09-20 10:22:29 +0100

    feat: make functions-utils use internal functions directory

--- packages/functions-utils/src/main.js
+++ packages/functions-utils/src/main.js
@@ -26,13 +26,8 @@ const add = async function (src, dist, { fail = defaultFail } = {}) {
   }
 
   const srcBasename = basename(src)
-  const functionsDist = `${dist}/${srcBasename}`
-  if (await pathExists(functionsDist)) {
-    return fail(`Function file or directory already exists at "${functionsDist}"`)
-  }
-
   const srcGlob = await getSrcGlob(src, srcBasename)
-  await cpy(srcGlob, dist, { cwd: dirname(src), parents: true, overwrite: false })
+  await cpy(srcGlob, dist, { cwd: dirname(src), parents: true, overwrite: true })
 }
 
 const getSrcGlob = async function (src, srcBasename) {
@@ -45,8 +40,10 @@ const getSrcGlob = async function (src, srcBasename) {
   return srcBasename
 }
 
+const isEmptyArray = (input) => Array.isArray(input) && input.length === 0
+
 const list = async function (functionsSrc, { fail = defaultFail } = {}) {
-  if (functionsSrc === undefined) {
+  if (functionsSrc === undefined || isEmptyArray(functionsSrc)) {
     return fail('No function directory was specified')
   }
 
@@ -58,7 +55,7 @@ const list = async function (functionsSrc, { fail = defaultFail } = {}) {
 }
 
 const listAll = async function (functionsSrc, { fail = defaultFail } = {}) {
-  if (functionsSrc === undefined) {
+  if (functionsSrc === undefined || isEmptyArray(functionsSrc)) {
     return fail('No function directory was specified')
   }
 
