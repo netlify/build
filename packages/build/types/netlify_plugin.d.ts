b27e7c930f9280ccf7ec602e962760115d46ee90
Ben Grynhaus <ben@brandtotal.com>
2021-10-18 15:54:13 +0300

    fix(build): allow specifying plugin options from NetlifyPlugin definition (#3745)
    Co-authored-by: ehmicky <ehmicky@users.noreply.github.com>

--- packages/build/types/netlify_plugin.d.ts
+++ packages/build/types/netlify_plugin.d.ts
@@ -1,29 +1,30 @@
 import { NetlifyEvent } from './netlify_event'
 import { NetlifyPluginOptions } from './netlify_plugin_options'
+import { JSONValue } from './utils/json_value'
 
-export interface NetlifyPlugin {
+export interface NetlifyPlugin<TInputs extends Record<string, JSONValue> = Partial<Record<string, JSONValue>>> {
   /**
    * Runs before the build command is executed.
    */
-  onPreBuild?: NetlifyEvent
+  onPreBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
   /**
    * runs directly after the build command is executed and before Functions? bundling and Edge Handlers bundling.
    */
-  onBuild?: NetlifyEvent
+  onBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
   /**
    * runs after the build command completes; after onBuild? tasks, Functions? bundling, and Edge Handlers bundling are executed; and before the deploy stage. Can be used to prevent a build from being deployed.
    */
-  onPostBuild?: NetlifyEvent
+  onPostBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
   /**
    * runs when an error occurs in the build or deploy stage, failing the build. Can’t be used to prevent a build from being deployed.
    */
-  onError?: NetlifyEvent<NetlifyPluginOptions & { error: Error }>
+  onError?: NetlifyEvent<NetlifyPluginOptions<TInputs> & { error: Error }>
   /**
    * runs when the deploy succeeds. Can’t be used to prevent a build from being deployed.
    */
-  onSuccess?: NetlifyEvent
+  onSuccess?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
   /**
    * runs after completion of the deploy stage, regardless of build error or success; is useful for resources cleanup. Can’t be used to prevent a build from being deployed.
    */
-  onEnd?: NetlifyEvent<NetlifyPluginOptions & { error?: Error }>
+  onEnd?: NetlifyEvent<NetlifyPluginOptions<TInputs> & { error?: Error }>
 }
