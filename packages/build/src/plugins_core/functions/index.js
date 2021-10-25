c179a1812be7b07e38d2b191aba1b7e908ce76fa
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-10-25 15:46:55 +0200

    chore: order parameters

--- packages/build/src/plugins_core/functions/index.js
+++ packages/build/src/plugins_core/functions/index.js
@@ -22,7 +22,7 @@ const isUsingEsbuild = (functionsConfig = {}) =>
 // The function configuration keys returned by @netlify/config are not an exact
 // match to the properties that @netlify/zip-it-and-ship-it expects. We do that
 // translation here.
-const normalizeFunctionConfig = ({ buildDir, functionConfig = {}, isRunningLocally, featureFlags }) => ({
+const normalizeFunctionConfig = ({ buildDir, featureFlags, functionConfig = {}, isRunningLocally }) => ({
   externalNodeModules: functionConfig.external_node_modules,
   includedFiles: functionConfig.included_files,
   includedFilesBasePath: buildDir,
