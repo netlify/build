236da59af2fce1536b355c1fed920d139923caac
Eduardo Bouças <mail@eduardoboucas.com>
2021-11-04 18:26:06 +0000

    feat: pass `nftTranspile` feature flag to ZISI

--- packages/build/src/plugins_core/functions/feature_flags.js
+++ packages/build/src/plugins_core/functions/feature_flags.js
@@ -3,6 +3,7 @@
 const getZisiFeatureFlags = (featureFlags) => ({
   buildGoSource: featureFlags.buildbot_build_go_functions,
   defaultEsModulesToEsbuild: featureFlags.buildbot_es_modules_esbuild,
+  nftTranspile: featureFlags.buildbot_zisi_trace_nft,
   parseWithEsbuild: featureFlags.buildbot_zisi_esbuild_parser,
   traceWithNft: featureFlags.buildbot_zisi_trace_nft,
 })
