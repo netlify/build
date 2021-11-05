c08258517d9e93d386ac57cc147c24480a24f903
Matt Kane <m@mk.gg>
2021-11-05 11:20:26 +0000

    feat: export more build plugin types

--- packages/build/types/netlify_event_handler.d.ts
+++ packages/build/types/netlify_event_handler.d.ts
@@ -1,5 +1,5 @@
 import { NetlifyPluginOptions } from './netlify_plugin_options'
 
-export interface NetlifyEvent<PluginOptions extends NetlifyPluginOptions = NetlifyPluginOptions> {
+export interface NetlifyEventHandler<PluginOptions extends NetlifyPluginOptions = NetlifyPluginOptions> {
   (options: PluginOptions): void | Promise<void>
 }
--- packages/build/types/netlify_plugin.d.ts
+++ packages/build/types/netlify_plugin.d.ts
@@ -1,30 +1,30 @@
 import { PluginInputs } from './config/inputs'
-import { NetlifyEvent } from './netlify_event'
+import { NetlifyEventHandler } from './netlify_event_handler'
 import { NetlifyPluginOptions } from './netlify_plugin_options'
 
 export interface NetlifyPlugin<TInputs extends PluginInputs = PluginInputs> {
   /**
    * Runs before the build command is executed.
    */
-  onPreBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
+  onPreBuild?: NetlifyEventHandler<NetlifyPluginOptions<TInputs>>
   /**
    * runs directly after the build command is executed and before Functions? bundling and Edge Handlers bundling.
    */
-  onBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
+  onBuild?: NetlifyEventHandler<NetlifyPluginOptions<TInputs>>
   /**
    * runs after the build command completes; after onBuild? tasks, Functions? bundling, and Edge Handlers bundling are executed; and before the deploy stage. Can be used to prevent a build from being deployed.
    */
-  onPostBuild?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
+  onPostBuild?: NetlifyEventHandler<NetlifyPluginOptions<TInputs>>
   /**
    * runs when an error occurs in the build or deploy stage, failing the build. Can’t be used to prevent a build from being deployed.
    */
-  onError?: NetlifyEvent<NetlifyPluginOptions<TInputs> & { error: Error }>
+  onError?: NetlifyEventHandler<NetlifyPluginOptions<TInputs> & { error: Error }>
   /**
    * runs when the deploy succeeds. Can’t be used to prevent a build from being deployed.
    */
-  onSuccess?: NetlifyEvent<NetlifyPluginOptions<TInputs>>
+  onSuccess?: NetlifyEventHandler<NetlifyPluginOptions<TInputs>>
   /**
    * runs after completion of the deploy stage, regardless of build error or success; is useful for resources cleanup. Can’t be used to prevent a build from being deployed.
    */
-  onEnd?: NetlifyEvent<NetlifyPluginOptions<TInputs> & { error?: Error }>
+  onEnd?: NetlifyEventHandler<NetlifyPluginOptions<TInputs> & { error?: Error }>
 }
