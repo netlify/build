51b34f12fbe32fdfa5c84456017af0f5e1284993
Matt Kane <matt.kane@netlify.com>
2021-11-05 21:26:33 +0000

    fix: export more build plugin types (#3816)
    * feat: export more build plugin types
    
    * chore: export named event handlers
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/build/types/netlify_event.d.ts
+++
@@ -1,5 +0,0 @@
-import { NetlifyPluginOptions } from './netlify_plugin_options'
-
-export interface NetlifyEvent<PluginOptions extends NetlifyPluginOptions = NetlifyPluginOptions> {
-  (options: PluginOptions): void | Promise<void>
-}
