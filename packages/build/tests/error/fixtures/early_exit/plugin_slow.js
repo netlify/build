63b55d00f74595e05aa1cc8385117f2a827eb224
Erez Rokah <erezrokah@users.noreply.github.com>
2022-01-05 11:00:52 +0200

    Update plugin_slow.js

--- packages/build/tests/error/fixtures/early_exit/plugin_slow.js
+++ packages/build/tests/error/fixtures/early_exit/plugin_slow.js
@@ -15,9 +15,8 @@ export default {
 
     // Signals are async, so we need to wait for the child process to exit
     // The while loop is required due to `await`
-    // eslint-disable-next-line fp/no-loops, no-await-in-loop
+    // eslint-disable-next-line fp/no-loops
     while (await processExists(env.TEST_PID)) {
-      // eslint-disable-next-line no-await-in-loop
       await pSetTimeout(PROCESS_TIMEOUT)
     }
   },
