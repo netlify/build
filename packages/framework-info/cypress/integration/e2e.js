b7369b5016c8e0a5e93b48e8d547d27af72ef127
ehmicky <ehmicky@users.noreply.github.com>
2022-01-19 19:39:15 +0100

    chore: remove dead code (#677)

--- packages/framework-info/cypress/integration/e2e.js
+++ packages/framework-info/cypress/integration/e2e.js
@@ -6,9 +6,4 @@ describe('Detects frameworks', () => {
     cy.contains('React Site').click()
     cy.contains('"name":"Next.js"')
   })
-
-  // it('Next.js site framework detected on load - vanilla JS site', () => {
-  //   cy.contains('Vanilla JS Site').click()
-  //   cy.contains('"name":"Next.js"')
-  // })
 })
