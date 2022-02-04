b49c4475cec1328fc13bd3cf32ab4ef757162c92
Eduardo Bouças <mail@eduardoboucas.com>
2022-02-04 15:30:15 +0000

    refactor: make `serve` return `getManifest`

--- packages/edge-bundler/src/server.ts
+++ packages/edge-bundler/src/server.ts
@@ -1,21 +1,38 @@
 import { tmpName } from 'tmp-promise'
 
-import { DenoBridge } from './bridge.js'
+import { DenoBridge, LifecycleHook } from './bridge.js'
 import { preBundle } from './bundler.js'
 import type { Declaration } from './declaration.js'
 import { generateManifest } from './manifest.js'
 
-const serve = async (port: number, sourceDirectories: string[], declarations: Declaration[]) => {
-  const deno = new DenoBridge()
+interface ServeOptions {
+  onAfterDownload?: LifecycleHook
+  onBeforeDownload?: LifecycleHook
+}
+
+const serve = async (
+  port: number,
+  sourceDirectories: string[],
+  { onAfterDownload, onBeforeDownload }: ServeOptions = {},
+) => {
+  const deno = new DenoBridge({
+    onAfterDownload,
+    onBeforeDownload,
+  })
   const distDirectory = await tmpName()
   const { handlers, preBundlePath } = await preBundle(sourceDirectories, distDirectory)
-  const manifest = generateManifest(preBundlePath, handlers, declarations)
+  const getManifest = (declarations: Declaration[]) => generateManifest(preBundlePath, handlers, declarations)
+
+  // Wait for the binary to be downloaded if needed.
+  await deno.getBinaryPath()
 
   // TODO: Add `--no-clear-screen` when https://github.com/denoland/deno/pull/13454 is released.
-  deno.run(['run', '-A', '--unstable', '--watch', preBundlePath, port.toString()], { wait: false })
+  const flags = ['-A', '--unstable', '--watch']
+
+  deno.run(['run', ...flags, preBundlePath, port.toString()], { wait: false })
 
   return {
-    manifest,
+    getManifest,
   }
 }
 
