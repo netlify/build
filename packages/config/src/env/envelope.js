67e9b787e5340a18be3086424929d9a8dbcbc823
Jason Barry <jb@netlify.com>
2022-06-24 08:45:09 -0700

    fix: addressing pull request feedback

--- packages/config/src/env/envelope.js
+++ packages/config/src/env/envelope.js
@@ -8,17 +8,19 @@ export const getEnvelope = async function ({ api, accountId, siteId }) {
   try {
     const environmentVariables = await api.getEnvVars({ accountId, siteId })
     // eslint-disable-next-line fp/no-mutating-methods
-    const envVars = environmentVariables
+    const sortedEnvVarsFromDevContext = environmentVariables
       .sort((left, right) => (left.key.toLowerCase() < right.key.toLowerCase() ? -1 : 1))
       .reduce((acc, cur) => {
         const envVar = cur.values.find((val) => ['dev', 'all'].includes(val.context))
         if (envVar && envVar.value) {
-          // eslint-disable-next-line fp/no-mutation, no-param-reassign
-          acc[cur.key] = envVar.value
+          return {
+            ...acc,
+            [cur.key]: envVar.value,
+          }
         }
         return acc
       }, {})
-    return envVars
+    return sortedEnvVarsFromDevContext
   } catch (error) {
     throwUserError(`Failed retrieving envelope for site ${siteId}: ${error.message}. ${ERROR_CALL_TO_ACTION}`)
   }
