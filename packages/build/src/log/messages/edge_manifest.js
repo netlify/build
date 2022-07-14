74bd7be3f5f444de7fef7d7fa3cc04bd57a2ba4c
Ewan Valentine <ewan.valentine89@gmail.com>
2022-07-14 14:48:16 +0100

    fix: improve formatting

--- packages/build/src/log/messages/edge_manifest.js
+++ packages/build/src/log/messages/edge_manifest.js
@@ -2,6 +2,6 @@ import { logMessage, logSubHeader } from '../logger.js'
 
 export const logEdgeManifest = ({ logs, manifest }) => {
   logSubHeader(logs, 'Edge Functions Manifest')
-  logMessage(logs, JSON.stringify(manifest))
+  logMessage(logs, JSON.stringify(manifest, null, 2))
   logMessage(logs, '')
 }
