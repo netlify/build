3b695a50683dca43f9b54099c2c3a6e3b9f5fd62
Eduardo Bouças <mail@eduardoboucas.com>
2022-07-30 16:11:38 +0100

    chore: fix test

--- packages/build/tests/monitor/fixtures/edge_function_error/netlify/edge-functions/trouble.ts
+++ packages/build/tests/monitor/fixtures/edge_function_error/netlify/edge-functions/trouble.ts
@@ -1,6 +1,6 @@
 // @ts-expect-error Resolving specifier should error
-// eslint-disable-next-line import/no-unresolved, n/no-missing-import
-import { foo } from 'file://bar'
+// eslint-disable-next-line import/no-unresolved, n/no-missing-import, ava/no-import-test-files
+import { foo } from './file.ts'
 
 // eslint-disable-next-line import/no-anonymous-default-export, require-await
 export default async () => foo
