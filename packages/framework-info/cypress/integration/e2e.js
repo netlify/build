139b74ab7c105efd8bdd83df268db219ce902b19
Erica Pisani <pisani.erica@gmail.com>
2022-08-03 11:49:51 -0400

    refactor: remove site (#803)
    * refactor: remove site
    
    Was limited in terms of what it was doing, and wasn't being used
    
    * build: add webpack package back in
    
    works locally with only webpack-cli but is failing on netlify build

--- packages/framework-info/cypress/integration/e2e.js
+++
@@ -1,9 +0,0 @@
-describe('Detects frameworks', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-  it('Next.js site framework detected on load - react site', () => {
-    cy.contains('React Site').click()
-    cy.contains('"name":"Next.js"')
-  })
-})
--- packages/framework-info/cypress/plugins/index.mjs
+++
@@ -1,4 +0,0 @@
-// TODO: rename this file to `index.js` once Cypress supports it for pure
-// ES modules
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {}
