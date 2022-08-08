87b299e02c4a0f2c8e9841c4b0253da3ee923b0f
Eduardo Bouças <mail@eduardoboucas.com>
2022-08-08 12:09:25 +0100

    feat: improve serialization of system log input

--- packages/build/src/log/logger.js
+++ packages/build/src/log/logger.js
@@ -113,11 +113,21 @@ export const logWarningSubHeader = function (logs, string, opts) {
 const reduceLogLines = function (lines) {
   return lines
     .map((input) => {
-      if (typeof input === 'object') {
-        return JSON.stringify(input)
+      if (input instanceof Error) {
+        return `${input.message} ${input.stack}`
       }
 
-      return input.toString()
+      if (typeof input === 'object') {
+        try {
+          return JSON.stringify(input)
+        } catch {
+          // Value could not be serialized to JSON, so we return the string
+          // representation.
+          return String(input)
+        }
+      }
+
+      return String(input)
     })
     .join(' ')
 }
