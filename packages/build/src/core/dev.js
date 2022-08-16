6f38edd0ed0a4d7a85da6ee03e232de697cec859
Eduardo Bouças <mail@eduardoboucas.com>
2022-08-16 13:39:54 +0100

    refactor: simplify startDev entry point

--- packages/build/src/core/dev.js
+++ packages/build/src/core/dev.js
@@ -4,23 +4,18 @@ import { execBuild, startBuild } from './build.js'
 import { getSeverity } from './severity.js'
 
 export const startDev = async (devCommand, flags = {}) => {
-  const { errorMonitor, mode, logs, debug, systemLogFile, testOpts, dry, buildId, deployId, ...flagsA } =
-    startBuild(flags)
+  const { errorMonitor, mode, logs, debug, testOpts, ...normalizedFlags } = startBuild(flags)
   const errorParams = { errorMonitor, mode, logs, debug, testOpts }
 
   try {
     const { netlifyConfig: netlifyConfigA, configMutations } = await execBuild({
-      ...flagsA,
-      buildId,
-      systemLogFile,
-      deployId,
-      dry,
+      ...normalizedFlags,
       errorMonitor,
+      errorParams,
       mode,
       logs,
       debug,
       testOpts,
-      errorParams,
       timeline: 'dev',
       devCommand,
     })
