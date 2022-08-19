747b693ae458bc1b66a52d0b492deec3deefffe7
Ed Stephinson <edward.stephinson@gmail.com>
2022-08-19 15:34:50 +0100

    chore: add whitespace

--- packages/build/src/log/messages/compatibility.js
+++ packages/build/src/log/messages/compatibility.js
@@ -22,6 +22,7 @@ export const logLoadingPlugins = function (logs, pluginsOptions, debug) {
     // We don't want to show runtimes as plugins
     .filter((plugin) => !isRuntime(plugin))
     .map((pluginOptions) => getPluginDescription(pluginOptions, debug))
+
   if (loadingPlugins.length === 0) {
     return
   }
