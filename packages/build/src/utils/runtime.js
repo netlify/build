aa281cd75685f30523d2e17a29d36a9107d2e3b2
Erica Pisani <pisani.erica@gmail.com>
2022-09-07 15:12:43 -0400

    style: lint

--- packages/build/src/utils/runtime.js
+++ packages/build/src/utils/runtime.js
@@ -1,5 +1,5 @@
 export const isRuntime = function (pluginOption) {
   // Make this a bit more robust in the future
-  const {packageName} = pluginOption
+  const { packageName } = pluginOption
   return ['@netlify/next-runtime', '@netlify/plugin-nextjs'].includes(packageName)
 }
