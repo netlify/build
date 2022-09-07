ae03068642dd33b71620fe300875b957ace3051b
Erica Pisani <pisani.erica@gmail.com>
2022-09-07 15:45:03 -0400

    fix: update check determining if runtime is being used

--- packages/build/src/utils/runtime.js
+++ packages/build/src/utils/runtime.js
@@ -1,4 +1,5 @@
-export const isRuntime = function (packageName) {
+export const isRuntime = function (pluginOption) {
+  const { packageName } = pluginOption
   // Make this a bit more robust in the future
   return ['@netlify/next-runtime', '@netlify/plugin-nextjs'].includes(packageName)
 }
