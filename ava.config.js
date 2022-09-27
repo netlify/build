ea1bea78b9dd129f89c50faab0ae6cf99a753f82
jobala <japhethobalak@gmail.com>
2022-09-27 13:22:22 +0100

    chore: restore base ava config file

--- ava.config.js
+++ ava.config.js
@@ -1,25 +1,25 @@
-// import fs from 'fs'
-// import path from 'path'
-// import process from 'process'
+import fs from 'fs'
+import path from 'path'
+import process from 'process'
 
 // eslint-disable-next-line n/no-unpublished-import
-// import { isCI } from 'ci-info'
+import { isCI } from 'ci-info'
 
 // `tests-metadata.json` is created by running `npm run test:measure`
 
-// const testData = JSON.parse(fs.readFileSync('tests-metadata.json'))
+const testData = JSON.parse(fs.readFileSync('../../tests-metadata.json'))
 
-// const getOrder = (file) => {
-//   const fileRelative = path.relative(process.cwd(), file).replace(/\\/g, '/')
-//   if (testData[fileRelative]) {
-//     return testData[fileRelative].order
-//   }
+const getOrder = (file) => {
+  const fileRelative = path.relative(process.cwd(), file).replace(/\\/g, '/')
+  if (testData[fileRelative]) {
+    return testData[fileRelative].order
+  }
 
-//   console.warn(`Missing test metadata for ${fileRelative}`)
-//   return Number.MAX_SAFE_INTEGER
-// }
+  console.warn(`Missing test metadata for ${fileRelative}`)
+  return Number.MAX_SAFE_INTEGER
+}
 
-// const sortTestFiles = (file1, file2) => getOrder(file1) - getOrder(file2)
+const sortTestFiles = (file1, file2) => getOrder(file1) - getOrder(file2)
 
 const config = {
   files: ['packages/**/tests/*.{cjs,mjs,js}', 'packages/**/tests/**/tests.{cjs,mjs,js}'],
@@ -30,9 +30,7 @@ const config = {
     FORCE_COLOR: '1',
   },
   // we only sort in CI to split efficiently across machines
-  // ...(isCI && { sortTestFiles }),
+  ...(isCI && { sortTestFiles }),
 }
-console.log('Used')
-console.log(config)
-console.log('=============')
+
 export default config
