6f871ad4ad6b06f0378e847051aa0cf510ef1081
Lukas Holzer <lukas.holzer@netlify.com>
2022-09-27 11:42:05 +0200

    ci: fix running build after install (#4538)
    * ci: fix running build after install
    
    * chore: use npm 8 version for proper npm workspaces support
    
    * chore: add debug logging
    
    * chore: better debugability
    
    * chore: debug
    
    * chore: debug
    
    * chore: debug
    
    * chore: boostrap lerna
    
    * chore: fix typos 🤦🏻‍♂️
    
    * chore: try
    
    * chore: workaround npm 7 issue
    
    * chore: fix lockfile
    
    * chore: cleanup 🧹
    
    * chore: update

---
+++ packages/build/bin.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// This is a workaround for npm issue: https://github.com/npm/cli/issues/2632
+// eslint-disable-next-line import/no-unassigned-import
+import './lib/core/bin.js'
