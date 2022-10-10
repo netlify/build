bd6e04ea56374bfa2d7877e7da4440a3bb61650b
Lukas Holzer <lukas.holzer@netlify.com>
2022-10-10 17:14:25 +0200

    chore(run-utils): make tests execution with typescript

--- packages/run-utils/src/main.ts
+++ packages/run-utils/src/main.ts
@@ -1,9 +1,12 @@
 import process from 'process'
 
-import { execa, execaCommand } from 'execa'
+import { execa, ExecaChildProcess, execaCommand, Options } from 'execa'
 
-// Run a command, with arguments being an array
-export const run = function (file, args, options) {
+/** Allow running local binaries by default */
+const DEFAULT_OPTIONS: Partial<Options<string>> = { preferLocal: true }
+
+/** Run a command, with arguments being an array */
+export const run = (file: string, args?: string[] | object, options?: Record<string, unknown>) => {
   const [argsA, optionsA] = parseArgs(args, options)
   const optionsB = { ...DEFAULT_OPTIONS, ...optionsA }
   const childProcess = execa(file, argsA, optionsB)
@@ -11,15 +14,15 @@ export const run = function (file, args, options) {
   return childProcess
 }
 
-// Run a command, with file + arguments being a single string
-export const runCommand = function (command, options) {
+/** Run a command, with file + arguments being a single string */
+export const runCommand = (command: string, options: Options<string>) => {
   const optionsA = { ...DEFAULT_OPTIONS, ...options }
   const childProcess = execaCommand(command, optionsA)
   redirectOutput(childProcess, optionsA)
   return childProcess
 }
 
-// Both `args` and `options` are optional
+/** Both `args` and `options` are optional */
 const parseArgs = function (args, options) {
   if (Array.isArray(args)) {
     return [args, options]
@@ -32,12 +35,11 @@ const parseArgs = function (args, options) {
   return []
 }
 
-// Allow running local binaries by default
-const DEFAULT_OPTIONS = { preferLocal: true }
-
-// Redirect output by default, unless specified otherwise
-const redirectOutput = function (childProcess, { stdio, stdout, stderr }) {
-  if (stdio !== undefined || stdout !== undefined || stderr !== undefined) {
+/**
+ * Redirect output by default, unless specified otherwise
+ * */
+const redirectOutput = (childProcess: ExecaChildProcess<string>, options: Options<string>) => {
+  if (options.stdio !== undefined || options.stdout !== undefined || options.stderr !== undefined) {
     return
   }
 
