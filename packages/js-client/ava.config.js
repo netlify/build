3c4f2795e820ca67c4b1a60c0e5828d766d4a074
Japheth Obala <japhethobalak@gmail.com>
2022-10-12 19:52:52 +0100

    chore: migrate js-client tests to Typescript (#4608)
    * chore: migrate js-client tests to Typescript
    
    * chore: set ts-node to transpile only
    
    * chore: remove ts ignore checks
    
    * chore: remove remaining @ts-ignore check

--- packages/js-client/ava.config.js
+++ packages/js-client/ava.config.js
@@ -1,7 +1,11 @@
 const config = {
-  files: ['src/**/*.test.js'],
+  files: ['src/*.test.ts'],
   verbose: true,
   workerThreads: false,
+  extensions: {
+    ts: 'module',
+  },
+  nodeArguments: ['--loader=ts-node/esm'],
 }
 
 export default config
