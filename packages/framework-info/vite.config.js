3dc82e72c8ca9b15d36e2fcfde6141cb82194585
Erica Pisani <pisani.erica@gmail.com>
2022-10-18 10:21:48 -0400

    feat: migrate from webpack to vite (#819)

---
+++ packages/framework-info/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath } from 'url'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+
+const CORE_FILE = fileURLToPath(new URL('src/core.js', import.meta.url))
+
+/** @type {import('vite').UserConfig} */
+export default {
+  resolve: {
+    alias: {
+      path: 'rollup-plugin-node-polyfills/polyfills/path',
+    },
+  },
+  build: {
+    lib: {
+      entry: CORE_FILE,
+      name: 'frameworkInfo',
+      fileName: 'index',
+    },
+    sourcemap: true,
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
+}
