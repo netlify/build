e73360da4c9460ef34f671cb5afe322f5de30f55
Lukas Holzer <lukas.holzer@netlify.com>
2022-10-18 12:08:03 +0200

    test: migrate netlify-headers-parser to vitest

---
+++ packages/headers-parser/tests/all.test.ts
@@ -0,0 +1,54 @@
+import { expect, test } from 'vitest'
+
+import { parseHeaders } from './helpers/main.js'
+
+test.each([
+  ['empty', {}, []],
+  ['only_config', { netlifyConfigPath: 'success' }, [{ for: '/path', values: { test: 'one' } }]],
+  [
+    'only_files',
+    { headersFiles: ['success', 'success_two'] },
+    [
+      { for: '/path', values: { test: 'one' } },
+      { for: '/path', values: { test: 'two' } },
+    ],
+  ],
+  [
+    'both_config_files',
+    { headersFiles: ['success_two'], netlifyConfigPath: 'success' },
+    [
+      { for: '/path', values: { test: 'two' } },
+      { for: '/path', values: { test: 'one' } },
+    ],
+  ],
+  [
+    'config_headers',
+    { netlifyConfigPath: 'success', configHeaders: [{ for: '/path', values: { test: ' two ' } }] },
+    [
+      { for: '/path', values: { test: 'one' } },
+      { for: '/path', values: { test: 'two' } },
+    ],
+  ],
+  [
+    'duplicates',
+    { headersFiles: ['duplicate'], netlifyConfigPath: 'duplicate' },
+    [
+      { for: '/path', values: { test: 'three' } },
+      { for: '/path', values: { test: 'one' } },
+      { for: '/path', values: { test: 'two' } },
+    ],
+  ],
+  ['invalid_mixed', { headersFiles: ['success'], configHeaders: {} }, [{ for: '/path', values: { test: 'one' } }]],
+])(`Parses netlify.toml and _headers | %s`, async (_, input, output) => {
+  const { headers } = await parseHeaders(input)
+  expect(headers).toStrictEqual(output)
+})
+
+test.each([
+  ['invalid_config_headers_array', { configHeaders: {} }, /must be an array/],
+  ['invalid_mixed', { headersFiles: ['simple'], configHeaders: {} }, /must be an array/],
+])(`Validate syntax errors | %s`, async (_, input, errorMessage) => {
+  const { errors } = await parseHeaders(input)
+  expect(errors).not.toHaveLength(0)
+  expect(errors.some((error) => errorMessage.test(error.message))).toBeTruthy()
+})
