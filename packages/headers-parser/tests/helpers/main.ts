4bd20d87f44a2d841ec57c30076614d71c7c34d6
Lukas Holzer <lukas.holzer@netlify.com>
2022-10-18 15:54:32 +0200

    test: migrate netlify-headers-parser to vitest (#4633)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/headers-parser/tests/helpers/main.ts
+++ packages/headers-parser/tests/helpers/main.ts
@@ -4,20 +4,7 @@ import { parseAllHeaders } from '../../src/index.js'
 
 const FIXTURES_DIR = fileURLToPath(new URL('../fixtures', import.meta.url))
 
-// Pass an `input` to the main method and assert its output
-export const validateSuccess = async function (t, { input, output }) {
-  const { headers } = await parseHeaders(input)
-  t.deepEqual(headers, output)
-}
-
-// Pass an `input` to the main method and assert it fails with a specific error
-export const validateError = async function (t, { input, errorMessage }) {
-  const { errors } = await parseHeaders(input)
-  t.not(errors.length, 0)
-  t.true(errors.some((error) => errorMessage.test(error.message)))
-}
-
-const parseHeaders = async function ({ headersFiles, netlifyConfigPath, configHeaders, ...input }) {
+export const parseHeaders = async function ({ headersFiles, netlifyConfigPath, configHeaders, ...input }: any) {
   return await parseAllHeaders({
     ...(headersFiles && { headersFiles: headersFiles.map(addFileFixtureDir) }),
     ...(netlifyConfigPath && { netlifyConfigPath: addConfigFixtureDir(netlifyConfigPath) }),
---
+++ packages/headers-parser/tests/netlify-config-parser.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from 'vitest'
+
+import { parseHeaders } from './helpers/main.js'
+
+test.each([
+  ['empty', { netlifyConfigPath: 'empty' }, []],
+  ['non_existing', { netlifyConfigPath: 'non_existing' }, []],
+  ['success', { netlifyConfigPath: 'success' }, [{ for: '/path', values: { test: 'one' } }]],
+  ['trim_path', { netlifyConfigPath: 'trim_path' }, [{ for: '/path', values: { test: 'one' } }]],
+  ['trim_name', { netlifyConfigPath: 'trim_name' }, [{ for: '/path', values: { test: 'one' } }]],
+  ['trim_value', { netlifyConfigPath: 'trim_value' }, [{ for: '/path', values: { test: 'one' } }]],
+  ['trim_value_array', { netlifyConfigPath: 'trim_value_array' }, [{ for: '/path', values: { test: 'one, two' } }]],
+  ['multiple_values', { netlifyConfigPath: 'multiple_values' }, [{ for: '/path', values: { test: 'one, two' } }]],
+  ['values_undefined', { netlifyConfigPath: 'values_undefined' }, []],
+  ['value_array', { netlifyConfigPath: 'value_array' }, [{ for: '/path', values: { test: 'one, two' } }]],
+  ['for_path_no_slash', { netlifyConfigPath: 'for_path_no_slash' }, [{ for: 'path', values: { test: 'one' } }]],
+])(`Parses netlify.toml headers | %s`, async (_, input, output) => {
+  const { headers } = await parseHeaders(input)
+  expect(headers).toStrictEqual(output)
+})
+
+test.each([
+  ['invalid_toml', { netlifyConfigPath: 'invalid_toml' }, /parse configuration file/],
+  ['invalid_array', { netlifyConfigPath: 'invalid_array' }, /must be an array/],
+  ['invalid_object', { netlifyConfigPath: 'invalid_object' }, /must be an object/],
+  ['invalid_for_undefined', { netlifyConfigPath: 'invalid_for_undefined' }, /Missing "for"/],
+  ['invalid_for_string', { netlifyConfigPath: 'invalid_for_string' }, /must be a string/],
+  ['invalid_values_object', { netlifyConfigPath: 'invalid_values_object' }, /must be an object/],
+  ['invalid_value_name', { netlifyConfigPath: 'invalid_value_name' }, /Empty header name/],
+  ['invalid_value_string', { netlifyConfigPath: 'invalid_value_string' }, /must be a string/],
+  ['invalid_value_array', { netlifyConfigPath: 'invalid_value_array' }, /must be a string/],
+])(`Validate syntax errors | %s`, async (_, input, errorMessage) => {
+  const { errors } = await parseHeaders(input)
+  expect(errors).not.toHaveLength(0)
+  expect(errors.some((error) => errorMessage.test(error.message))).toBeTruthy()
+})
