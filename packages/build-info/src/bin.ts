5b9178749a71617b8d1a0acc3d13f5a80bf6f650
Lukas Holzer <lukas.holzer@netlify.com>
2022-10-19 17:30:54 +0200

    feat: add new print-workspaces command

--- packages/build-info/src/bin.ts
+++ packages/build-info/src/bin.ts
@@ -1,31 +1,49 @@
 #!/usr/bin/env node
+import { relative } from 'path'
 import { exit, argv } from 'process'
 
+import yargs, { Arguments } from 'yargs'
 import { hideBin } from 'yargs/helpers'
-import yargs from 'yargs/yargs'
 
+import { getContext } from './context.js'
 import { getBuildInfo } from './main.js'
+import { getWorkspaceInfo } from './workspaces.js'
 
-const { projectDir, rootDir } = yargs(hideBin(argv))
-  .command('* [projectDir]')
-  .options({
-    rootDir: {
-      string: true,
-      describe: `The root directory of the project if different from projectDir`,
+yargs(hideBin(argv))
+  .command(
+    'print-workspaces [rootDir]',
+    'This command is used inside buildbot to retrieve a space separated list of relative package paths',
+    () => {
+      // no build options
     },
-  })
-  .usage(
-    `$0 [OPTIONS...] [PROJECT_DIRECTORY]
-
-  Print relevant build information from a project.`,
+    async ({ projectDir }: Arguments<{ projectDir?: string }>) => {
+      const context = await getContext({ projectDir })
+      const workspaces = await getWorkspaceInfo(context)
+      if (!workspaces) {
+        return
+      }
+      console.log(workspaces.packages.map((p) => relative(context.projectDir, p)).join(' '))
+    },
+  )
+  .command(
+    '* [projectDir]',
+    '',
+    (builder) =>
+      builder.options({
+        rootDir: {
+          string: true,
+          describe: `The root directory of the project if different from projectDir`,
+        },
+      }),
+    async ({ rootDir, projectDir }: Arguments<{ projectDir?: string; rootDir?: string }>) => {
+      try {
+        const buildInfo = await getBuildInfo({ projectDir, rootDir })
+        console.log(JSON.stringify(buildInfo, null, 2))
+      } catch (error) {
+        console.error(error)
+        exit(1)
+      }
+    },
   )
   .strict()
   .parse()
-
-try {
-  const buildInfo = await getBuildInfo({ projectDir, rootDir })
-  console.log(JSON.stringify(buildInfo, null, 2))
-} catch (error) {
-  console.error(error)
-  exit(1)
-}
