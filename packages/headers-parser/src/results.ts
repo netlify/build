baf7c98c85e317dfa2705828a5b71a388855bfc0
JGAntunes <joao@netlify.com>
2022-10-31 18:23:08 +0000

    chore: add type guard and inline functions

--- packages/headers-parser/src/results.ts
+++ packages/headers-parser/src/results.ts
@@ -2,26 +2,18 @@ import type { Header } from './types.js'
 
 // If one header fails to parse, we still try to return the other ones
 export function splitResults<Type>(results: (Error | Type)[]) {
-  const headers = results.filter((result) => !isError(result)) as Type[]
-  const errors = results.filter(isError) as Error[]
+  const headers = results.filter((result) => !isError(result))
+  const errors = results.filter(isError)
   return { headers, errors }
 }
 
-const isError = function (result: Error | any) {
+const isError = function (result: Error | any): result is Error {
   return result instanceof Error
 }
 
 // Concatenate an array of `{ headers, erors }`
 export const concatResults = function (resultsArrays: { headers: Header[]; errors: Error[] }[]) {
-  const headers = resultsArrays.flatMap(getHeaders) as Header[]
-  const errors = resultsArrays.flatMap(getErrors) as Error[]
+  const headers = resultsArrays.flatMap(({ headers }) => headers)
+  const errors = resultsArrays.flatMap(({ errors }) => errors)
   return { headers, errors }
-}
-
-const getHeaders = function ({ headers }) {
-  return headers
-}
-
-const getErrors = function ({ errors }) {
-  return errors
 }
