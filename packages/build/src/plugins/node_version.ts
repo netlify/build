fb030d08ffd49a36b49464751a23b460a82fcfc7
Lukas Holzer <lukas.holzer@netlify.com>
2022-11-22 11:45:36 +0100

    feat: warn on minimum required node version for plugins

--- packages/build/src/plugins/node_version.ts
+++ packages/build/src/plugins/node_version.ts
@@ -1,32 +1,80 @@
 import { execPath, version as currentVersion } from 'process'
 
 import semver from 'semver'
+import link from 'terminal-link'
 
-// This node version is minimum required to use ESModules so if the user's preferred Node.js version is below that
-// we have to fall back to the system node version
-const MINIMUM_REQUIRED_NODE_VERSION = '^12.20.0 || ^14.14.0 || >=16.0.0'
+import { logWarning, logWarningSubHeader } from '../log/logger.js'
 
-// Local plugins and `package.json`-installed plugins use user's preferred Node.js version if higher than our minimum
-// supported version. Else default to the system Node version.
-// Local and programmatic builds use `@netlify/build` Node.js version, which is
-// usually the system's Node.js version.
-// If the user Node version does not satisfy our supported engine range use our own system Node version
-export const addPluginsNodeVersion = function ({ pluginsOptions, nodePath, userNodeVersion }) {
+export type PluginsLoadedFrom = 'auto_install' | 'local' | 'package.json'
+
+export type PluginsOptions = {
+  packageName: string
+  pluginPath?: any
+  pinnedVersion?: any
+  loadedFrom: PluginsLoadedFrom
+  origin: 'config' | string
+  inputs: Record<string, any>
+}
+
+/**
+ * @deprecated will be replaced with `MINIMUM_REQUIRED_NODE_VERSION` at the 5th. of December 2022
+ */
+const OLD_MINIMUM_REQUIRED_NODE_VERSION = '^12.20.0 || ^14.14.0 || >=16.0.0'
+/**
+ * This node version is minimum required to run the plugins code.
+ * If the users preferred Node.js version is below that we have to fall back to the system node version
+ */
+const MINIMUM_REQUIRED_NODE_VERSION = '^14.14.0 || >=16.0.0'
+
+/**
+ * Local plugins and `package.json`-installed plugins use user's preferred Node.js version if higher than our minimum
+ * supported version. Else default to the system Node version.
+ * Local and programmatic builds use `@netlify/build` Node.js version, which is
+ * usually the system's Node.js version.
+ * If the user Node version does not satisfy our supported engine range use our own system Node version
+ */
+export const addPluginsNodeVersion = function ({ pluginsOptions, nodePath, userNodeVersion, logs }) {
   const currentNodeVersion = semver.clean(currentVersion)
   return pluginsOptions.map((pluginOptions) =>
-    addPluginNodeVersion({ pluginOptions, currentNodeVersion, userNodeVersion, nodePath }),
+    addPluginNodeVersion({ pluginOptions, currentNodeVersion, userNodeVersion, nodePath, logs }),
   )
 }
 
 const addPluginNodeVersion = function ({
   pluginOptions,
-  pluginOptions: { loadedFrom },
+  pluginOptions: { loadedFrom, packageName },
   currentNodeVersion,
   userNodeVersion,
   nodePath,
+  logs,
+}: {
+  pluginOptions: PluginsOptions
+  [key: string]: any
 }) {
+  // if the plugin is a local one and the users node version does not satisfy
+  // our minimum required node version log a warning as we will default to the system node version
+  if (
+    (loadedFrom === 'local' || loadedFrom === 'package.json') &&
+    !semver.satisfies(userNodeVersion, MINIMUM_REQUIRED_NODE_VERSION)
+  ) {
+    logWarningSubHeader(
+      logs,
+      `Warning: ${packageName} will be executed with Node.js version ${currentNodeVersion} in the future`,
+    )
+    logWarning(
+      logs,
+      `  The plugin cannot be executed with your defined Node.js version ${userNodeVersion} in the future.
+  Starting December 5th 2022 our minimum required Node version range for executing plugins will be: ${MINIMUM_REQUIRED_NODE_VERSION}
+
+  Read more about our minimum required version in our ${link(
+    'forums announcement',
+    'https://answers.netlify.com/t/build-plugins-dropping-support-for-node-js-12/79421',
+  )}`,
+    )
+  }
+
   return (loadedFrom === 'local' || loadedFrom === 'package.json') &&
-    semver.satisfies(userNodeVersion, MINIMUM_REQUIRED_NODE_VERSION)
+    semver.satisfies(userNodeVersion, OLD_MINIMUM_REQUIRED_NODE_VERSION)
     ? { ...pluginOptions, nodePath, nodeVersion: userNodeVersion }
     : { ...pluginOptions, nodePath: execPath, nodeVersion: currentNodeVersion }
 }
