ce6e8b22bd0b05aa4f633b491b3ebf0a3459e7d5
Erica Pisani <pisani.erica@gmail.com>
2022-12-07 15:16:50 -0500

    refactor: remove redunant files

--- packages/framework-info/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-const { overrides } = require('@netlify/eslint-config-node/.eslintrc_esm.cjs')
-
-module.exports = {
-  extends: '@netlify/eslint-config-node/.eslintrc_esm.cjs',
-  rules: {
-    'import/no-unresolved': [2, { ignore: ['build/'] }],
-  },
-  overrides: [...overrides],
-}
