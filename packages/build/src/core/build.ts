0b17ffd9c98f0890e7062eea13ddfb53231abc33
jobala <japhethobalak@gmail.com>
2023-01-09 14:48:24 +0000

    chore: fix conditional

--- packages/build/src/core/build.ts
+++ packages/build/src/core/build.ts
@@ -128,11 +128,14 @@ const tExecBuild = async function ({
 
   if (framework) {
     const runtime = supportedRuntimes[framework]
-    const skip = childEnv[runtime.skipFlag]
-    const installedPlugins = netlifyConfig.plugins.map((plugin) => plugin.package)
 
-    if (runtime !== undefined && skip !== 'true' && installedPlugins.contains(runtime.package)) {
-      netlifyConfig.plugins.push({ package: runtime.package })
+    if (runtime !== undefined) {
+      const dontSkip = childEnv[runtime.skipFlag] !== 'true'
+      const installed = netlifyConfig.plugins.map((plugin) => plugin.package).includes(runtime.package)
+
+      if (!installed && dontSkip) {
+        netlifyConfig.plugins.push({ package: runtime.package })
+      }
     }
   }
 
