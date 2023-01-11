29c16f5788387a48f66b1ae1e2f87f5211841313
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-01-11 12:08:04 +0100

    fix: replace statsd-client with hot-shots

--- packages/build/src/report/statsd.ts
+++ packages/build/src/report/statsd.ts
@@ -1,40 +1,43 @@
 import { promisify } from 'util'
 
 import slugify from '@sindresorhus/slugify'
-import StatsdClient from 'statsd-client'
+import { StatsD } from 'hot-shots'
 
-// TODO: replace with `timers/promises` after dropping Node < 15.0.0
-const pSetTimeout = promisify(setTimeout)
-
-// See https://github.com/msiebuhr/node-statsd-client/blob/45a93ee4c94ca72f244a40b06cb542d4bd7c3766/lib/EphemeralSocket.js#L81
-const CLOSE_TIMEOUT = 11
-
-// The socket creation is delayed until the first packet is sent. In our
-// case, this happens just before `client.close()` is called, which is too
-// late and make it not send anything. We need to manually create it using
-// the internal API.
-export const startClient = async function (host: string, port: number): Promise<StatsdClient> {
-  const client = new StatsdClient({ host, port, socketTimeout: 0 })
-
-  // @ts-expect-error using internals :D
-  await promisify(client._socket._createSocket.bind(client._socket))()
-
-  return client
+export const startClient = async function (host: string, port: number): Promise<StatsD> {
+  return new StatsD({
+    host,
+    port,
+    // This caches the dns resolution for subsequent sends of metrics for this instance
+    // Because we only try to send the metrics on close, this comes only into effect if `bufferFlushInterval` time is exceeded
+    cacheDns: true,
+    // set the maxBufferSize to infinite and the bufferFlushInterval very high, so that we only
+    // send the metrics on close or if more than 10 seconds past by
+    maxBufferSize: Infinity,
+    bufferFlushInterval: 10_000,
+  })
 }
 
-// UDP packets are buffered and flushed at regular intervals by statsd-client.
-// Closing force flushing all of them.
-export const closeClient = async function (client: StatsdClient): Promise<void> {
-  client.close()
-
-  // statsd-client does not provide a way of knowing when the socket is done
-  // closing, so we need to use the following hack.
-  await pSetTimeout(CLOSE_TIMEOUT)
-  await pSetTimeout(CLOSE_TIMEOUT)
+export const closeClient = async function (client: StatsD): Promise<void> {
+  await promisify(client.close.bind(client))()
 }
 
 // Make sure the timer name does not include special characters.
 // For example, the `packageName` of local plugins includes dots.
 export const normalizeTagName = function (name: string): string {
   return slugify(name, { separator: '_' })
+}
+
+// Formats and flattens the tags as array
+// We do this because we might send the same tag with different values `{ tag: ['a', 'b'] }`
+// which cannot be represented in an flattened object and `hot-shots` also supports tags as array in the format `['tag:a', 'tag:b']`
+export const formatTags = function (tags: Record<string, string | string[]>): string[] {
+  return Object.entries(tags)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map((subValue) => `${key}:${subValue}`)
+      } else {
+        return `${key}:${value}`
+      }
+    })
+    .flat()
 }
--- packages/build/src/time/report.ts
+++ packages/build/src/time/report.ts
@@ -1,20 +1,51 @@
-import { closeClient, startClient } from '../report/statsd.js'
+import type { Tags, StatsD } from 'hot-shots'
+
+import { closeClient, formatTags, startClient } from '../report/statsd.js'
 
 import { addAggregatedTimers } from './aggregate.js'
 import { roundTimerToMillisecs } from './measure.js'
 
+interface Timer {
+  metricName: string
+  stageTag: string
+  parentTag: string
+  durationNs: number
+  tags: Record<string, string | string[]>
+}
+
+// TODO Once we have more TS coverage make host & port required and instead make `statsdOpts` below optional
+// that should be more clean
+export interface StatsDOptions {
+  host?: string
+  port?: number
+}
+
+interface TimerOptions {
+  timers: Timer[]
+  statsdOpts: StatsDOptions
+  framework?: string
+}
+
 // Record the duration of a build phase, for monitoring.
 // Sends to statsd daemon.
-export const reportTimers = async function ({ timers, statsdOpts: { host, port }, framework }) {
-  if (host === undefined) {
+export const reportTimers = async function ({
+  timers,
+  statsdOpts: { host, port },
+  framework,
+}: TimerOptions): Promise<void> {
+  if (host === undefined || port === undefined) {
     return
   }
 
   const timersA = addAggregatedTimers(timers)
-  await sendTimers({ timers: timersA, host, port, framework })
+  await sendTimers({ timers: timersA, statsdOpts: { host, port }, framework })
 }
 
-const sendTimers = async function ({ timers, host, port, framework }) {
+const sendTimers = async function ({
+  timers,
+  statsdOpts: { host, port },
+  framework,
+}: TimerOptions & { statsdOpts: Required<StatsDOptions> }): Promise<void> {
   const client = await startClient(host, port)
   timers.forEach((timer) => {
     sendTimer({ timer, client, framework })
@@ -22,8 +53,21 @@ const sendTimers = async function ({ timers, host, port, framework }) {
   await closeClient(client)
 }
 
-const sendTimer = function ({ timer: { metricName, stageTag, parentTag, durationNs, tags }, client, framework }) {
+const sendTimer = function ({
+  timer: { metricName, stageTag, parentTag, durationNs, tags },
+  client,
+  framework,
+}: {
+  timer: Timer
+  client: StatsD
+  framework?: string
+}): void {
   const durationMs = roundTimerToMillisecs(durationNs)
-  const frameworkTag = framework === undefined ? {} : { framework }
-  client.distribution(metricName, durationMs, { stage: stageTag, parent: parentTag, ...tags, ...frameworkTag })
+  const statsdTags: Tags = { stage: stageTag, parent: parentTag, ...tags }
+
+  if (framework != undefined) {
+    statsdTags.framework = framework
+  }
+
+  client.distribution(metricName, durationMs, formatTags(statsdTags))
 }
