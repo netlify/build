01e77ec2f43e93bf960515401ed613a81f835742
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-03-01 17:35:15 +0100

    chore: migrate to TS

--- packages/build/src/plugins_core/functions/feature_flags.js
+++
@@ -1,5 +0,0 @@
-export const getZisiFeatureFlags = (featureFlags) => ({
-  ...featureFlags,
-  parseWithEsbuild: featureFlags.buildbot_zisi_esbuild_parser,
-  traceWithNft: featureFlags.buildbot_zisi_trace_nft,
-})
