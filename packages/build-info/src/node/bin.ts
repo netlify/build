972c815e3a05c05cd910e0479bce5dab935bbcf6
Lukas Holzer <lukas.holzer@netlify.com>
2023-03-06 19:12:45 +0100

    feat: use dev cycle feature flagging

--- packages/build-info/src/node/bin.ts
+++ packages/build-info/src/node/bin.ts
@@ -1,14 +1,16 @@
 import { argv, exit } from 'process'
 
+import { DVCUser } from '@devcycle/nodejs-server-sdk'
 import yargs, { Arguments } from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 import { report } from '../metrics.js'
 
+import { getFeatureFlags } from './feature-flags.js'
 import { getBuildInfo } from './get-build-info.js'
 import { initializeMetrics } from './metrics.js'
 
-type Args = Arguments<{ projectDir?: string; rootDir?: string; featureFlags: Record<string, boolean> }>
+type Args = Arguments<{ projectDir?: string; rootDir?: string; devCycleUser: DVCUser }>
 
 yargs(hideBin(argv))
   .command(
@@ -20,26 +22,31 @@ yargs(hideBin(argv))
           string: true,
           describe: `The root directory of the project if different from projectDir`,
         },
-        featureFlags: {
+        devCycleUser: {
           string: true,
-          describe: 'comma separated list of feature flags',
-          coerce: (value = '') =>
-            value
-              .split(',')
-              .map((flag) => flag.trim())
-              .filter((flag) => flag.length)
-              .reduce((prev, cur) => ({ ...prev, [cur]: true }), {}),
+          default: '{}',
+          describe: 'DevCycle user that is used for the feature flag client',
+          coerce: (value) => {
+            try {
+              return JSON.parse(value)
+            } catch {
+              return {}
+            }
+          },
         },
       }),
-    async ({ projectDir, rootDir, featureFlags }: Args) => {
+    async ({ projectDir, rootDir, devCycleUser }: Args) => {
       // start bugsnag reporting
       await initializeMetrics()
+      const featureFlags = await getFeatureFlags(devCycleUser)
+
       try {
         console.log(JSON.stringify(await getBuildInfo({ projectDir, rootDir, featureFlags }), null, 2))
       } catch (error) {
         report(error)
         exit(1)
       }
+      exit(0)
     },
   )
   .strict()
