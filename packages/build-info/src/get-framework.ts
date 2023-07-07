8a1083e08db98f345b1d7816c44458f9a702c3ce
Lukas Holzer <lukas.holzer@netlify.com>
2023-07-07 16:42:26 +0200

    fix: fix return type of get framework to return the class (#5145)

--- packages/build-info/src/get-framework.ts
+++ packages/build-info/src/get-framework.ts
@@ -1,14 +1,14 @@
-import { FrameworkName, frameworks, FrameworkInfo } from './frameworks/index.js'
+import { Framework } from './frameworks/framework.js'
+import { FrameworkName, frameworks } from './frameworks/index.js'
 import { Project } from './project.js'
 
 /** Return some information about a framework used by a project. */
-export async function getFramework(frameworkId: FrameworkName, project: Project): Promise<FrameworkInfo | undefined> {
+export async function getFramework(frameworkId: FrameworkName, project: Project): Promise<Framework> {
   const frameworkList = frameworks.map((Framework) => new Framework(project))
   const framework = frameworkList.find(({ id }) => id === frameworkId)
 
   if (framework) {
-    const result = await framework.detect()
-    return result?.toJSON()
+    return framework
   }
 
   const frameworkIds = frameworkList
