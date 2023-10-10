bce899d8297826adfc65c53c6235bc0b28142dd1
Eduardo Bouças <mail@eduardoboucas.com>
2023-10-10 11:02:17 +0100

    chore: update fixtures

--- packages/build/tests/core/fixtures/functions_config_3/functions/function_one.js
+++ packages/build/tests/core/fixtures/functions_config_3/functions/function_one.js
@@ -1,4 +1,4 @@
 import module1 from '@netlify/imaginary-module-one'
 import module2 from '@netlify/imaginary-module-two'
 
-export default () => [module1, module2]
+export const handler = () => [module1, module2]
