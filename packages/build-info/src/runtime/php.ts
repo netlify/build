a09ee8570ca0fe4e0f41e90cc2bfa9131663477a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2023-10-23 12:39:39 +0200

    Update php.ts

--- packages/build-info/src/runtime/php.ts
+++ packages/build-info/src/runtime/php.ts
@@ -3,5 +3,5 @@ import { LangRuntime } from './runtime.js'
 export class Php extends LangRuntime {
   id = 'php'
   name = 'Php'
-  configFiles = ['compose.json']
+  configFiles = ['composer.json']
 }
