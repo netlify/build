bbcad095ddf5e36163d01b9a93ce2126a16dc383
Simon Knott <info@simonknott.de>
2023-11-17 15:45:54 +0100

    refactor: provide via "systemLog" function

--- packages/build/tests/plugins/fixtures/feature_flags/plugin.js
+++ packages/build/tests/plugins/fixtures/feature_flags/plugin.js
@@ -1,8 +1,4 @@
-import { appendFileSync } from  "fs"
-
-const systemLog = "/dev/fd/4" // 🪄 magic ✨
-
-export const onBuild = function ({ featureFlags }) {
-  appendFileSync(systemLog, "some system-facing logs\n")
+export const onBuild = function ({ featureFlags, systemLog }) {
+  systemLog("some system-facing logs")
   console.log(JSON.stringify(featureFlags))
 }
--- packages/build/tests/plugins/fixtures/feature_flags_untrusted/plugin.js
+++ packages/build/tests/plugins/fixtures/feature_flags_untrusted/plugin.js
@@ -1,16 +1,5 @@
-import { appendFileSync } from  "fs"
-
-const isSystemLogAccessible = () => {
-  try {
-    appendFileSync("/dev/fd/4", "some system-facing logs")
-    return true
-  } catch (error) {
-    return false
-  }
-}
-
-export const onBuild = function ({ featureFlags }) {
-  if (isSystemLogAccessible()) {
+export const onBuild = function ({ featureFlags, systemLog }) {
+  if (systemLog) {
     throw new Error("System log shouldn't be acessible from untrusted plugins")
   }
   console.log("typeof featureflags:", typeof featureFlags)
