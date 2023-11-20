a44e54caee2f87b112ed1cc0ce21a6406c5e9ca4
Esteban Pastorino <ejpastorino@gmail.com>
2023-11-20 18:56:31 -0300

    feat: add plugin for blobs upload

---
+++ packages/build/src/plugins_core/blobs_upload/index.ts
@@ -0,0 +1,62 @@
+import { getDeployStore } from '@netlify/blobs'
+import pMap from 'p-map'
+
+import { addErrorInfo } from '../../error/info.js'
+import { log, logError } from '../../log/logger.js'
+
+import { getKeysToUpload, getBlobsDir, getFileWithMetadata, anyBlobsToUpload } from './utils.js'
+
+const coreStep = async function ({
+  debug,
+  logs,
+  deployId,
+  buildDir,
+  constants: { PUBLISH_DIR, SITE_ID, NETLIFY_API_TOKEN, API_URL },
+}) {
+  const blobStore = getDeployStore({ siteID: SITE_ID, deployID: deployId, token: NETLIFY_API_TOKEN, apiURL: API_URL })
+  const blobsDir = getBlobsDir({ buildDir, publishDir: PUBLISH_DIR })
+  const keys = await getKeysToUpload(blobsDir)
+
+  // We checked earlier, but let's be extra safe
+  if (keys.length === 0) {
+    log(logs, 'No blobs to upload to deploy store.')
+    return {}
+  }
+
+  log(logs, `Uploading ${keys.length} blobs to deploy store...`)
+
+  const uploadBlob = async (key) => {
+    if (debug) {
+      log(logs, `- Uploading blob ${key}`, { indent: true })
+    }
+    const { data, metadata } = await getFileWithMetadata(blobsDir, key)
+    await blobStore.set(key, data, { metadata })
+  }
+
+  try {
+    await pMap(keys, uploadBlob, { concurrency: 10 })
+  } catch (err) {
+    logError(logs, `Error uploading blobs to deploy store: ${err.message}`)
+
+    const error = new Error(`Failed while uploading blobs to deploy store.`)
+    addErrorInfo(error, { type: 'blobsUploadError' })
+    throw error
+  }
+
+  log(logs, `Done uploading blobs to deploy store.`)
+
+  return {}
+}
+
+const deployAndBlobsPresent = async function ({ deployId, buildDir, constants: { PUBLISH_DIR } }): Promise<boolean> {
+  return deployId && (await anyBlobsToUpload({ buildDir, publishDir: PUBLISH_DIR }))
+}
+
+export const uploadBlobs = {
+  event: 'onPostBuild',
+  coreStep,
+  coreStepId: 'blobs_upload',
+  coreStepName: 'Uploading blobs',
+  coreStepDescription: () => 'Uploading blobs to deploy store',
+  condition: deployAndBlobsPresent,
+}
---
+++ packages/build/src/plugins_core/blobs_upload/utils.ts
@@ -0,0 +1,65 @@
+import fsPromises from 'node:fs/promises'
+import path from 'node:path'
+
+import { fdir } from 'fdir'
+
+const BLOBS_PATH = '.netlify/blobs/deploy'
+const METADATA_PREFIX = '$'
+const METADATA_SUFFIX = '.json'
+
+export const getBlobsDir = function ({ buildDir, publishDir }) {
+  return path.resolve(buildDir, publishDir, BLOBS_PATH)
+}
+
+export const anyBlobsToUpload = async function ({ buildDir, publishDir }) {
+  const blobsDir = getBlobsDir({ buildDir, publishDir })
+  const { files } = await new fdir().onlyCounts().crawl(blobsDir).withPromise()
+  return files > 0
+}
+
+/* Given output directory, find all file paths to upload excluding metadata files */
+export async function getKeysToUpload(blobsDir: string): Promise<string[]> {
+  const files = await new fdir()
+    .withRelativePaths() // we want the relative path from the blobsDir
+    .filter((fpath) => !path.basename(fpath).startsWith(METADATA_PREFIX))
+    .crawl(blobsDir)
+    .withPromise()
+
+  // normalize the path separators to all use the forward slash
+  return files.map((f) => f.split(path.sep).join('/'))
+}
+
+/* Read a file and its metadata file from the blobs directory */
+export async function getFileWithMetadata(
+  blobsDir: string,
+  key: string,
+): Promise<{ data: Buffer; metadata: Record<string, string> }> {
+  const contentPath = path.join(blobsDir, key)
+  const dirname = path.dirname(key)
+  const basename = path.basename(key)
+  const metadataPath = path.join(blobsDir, dirname, `${METADATA_PREFIX}${basename}${METADATA_SUFFIX}`)
+
+  const [data, metadata] = await Promise.all([fsPromises.readFile(contentPath), readMetadata(metadataPath)]).catch(
+    (err) => {
+      throw new Error(`Failed while reading '${key}' and its metadata: ${err.message}`)
+    },
+  )
+
+  return { data, metadata }
+}
+
+async function readMetadata(metadataPath: string): Promise<Record<string, string>> {
+  let metadataFile
+  try {
+    metadataFile = await fsPromises.readFile(metadataPath, { encoding: 'utf8' })
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      // no metadata file found, that's ok
+      return {}
+    }
+    throw err
+  }
+
+  const metadata = JSON.parse(metadataFile)
+  return metadata
+}
