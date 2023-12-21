f05fb5e248dbbc3ef996ba8d0101f89f216d8482
Simon Knott <info@simonknott.de>
2023-12-21 11:57:19 +0100

    Revert "fix: warn about upcoming system version update (#5451)" (#5452)
    This reverts commit ba506810e216331696926f51a98ed48ef6e8e1f6.

--- packages/build/src/plugins/node_version.ts
+++ packages/build/src/plugins/node_version.ts
@@ -21,7 +21,6 @@ export type PluginsOptions = {
  * If the users preferred Node.js version is below that we have to fall back to the system node version
  */
 const MINIMUM_REQUIRED_NODE_VERSION = '^14.14.0 || >=16.0.0'
-const UPCOMING_MINIMUM_REQUIRED_NODE_VERSION = '>=18.14.0'
 
 /**
  * Local plugins and `package.json`-installed plugins use user's preferred Node.js version if higher than our minimum
@@ -30,15 +29,14 @@ const UPCOMING_MINIMUM_REQUIRED_NODE_VERSION = '>=18.14.0'
  * usually the system's Node.js version.
  * If the user Node version does not satisfy our supported engine range use our own system Node version
  */
-export const addPluginsNodeVersion = function ({ featureFlags, pluginsOptions, nodePath, userNodeVersion, logs }) {
+export const addPluginsNodeVersion = function ({ pluginsOptions, nodePath, userNodeVersion, logs }) {
   const currentNodeVersion = semver.clean(currentVersion)
   return pluginsOptions.map((pluginOptions) =>
-    addPluginNodeVersion({ featureFlags, pluginOptions, currentNodeVersion, userNodeVersion, nodePath, logs }),
+    addPluginNodeVersion({ pluginOptions, currentNodeVersion, userNodeVersion, nodePath, logs }),
   )
 }
 
 const addPluginNodeVersion = function ({
-  featureFlags,
   pluginOptions,
   pluginOptions: { loadedFrom, packageName },
   currentNodeVersion,
@@ -51,43 +49,24 @@ const addPluginNodeVersion = function ({
 }) {
   // if the plugin is a local one and the users node version does not satisfy
   // our minimum required node version log a warning as we will default to the system node version
-  if (loadedFrom === 'local' || loadedFrom === 'package.json') {
-    if (!semver.satisfies(userNodeVersion, MINIMUM_REQUIRED_NODE_VERSION)) {
-      logWarningSubHeader(logs, `Warning: ${packageName} will be executed with Node.js version ${currentNodeVersion}`)
-      logWarning(
-        logs,
-        `  The plugin cannot be executed with your defined Node.js version ${userNodeVersion}
-  
+  if (
+    (loadedFrom === 'local' || loadedFrom === 'package.json') &&
+    !semver.satisfies(userNodeVersion, MINIMUM_REQUIRED_NODE_VERSION)
+  ) {
+    logWarningSubHeader(logs, `Warning: ${packageName} will be executed with Node.js version ${currentNodeVersion}`)
+    logWarning(
+      logs,
+      `  The plugin cannot be executed with your defined Node.js version ${userNodeVersion}
+
   Read more about our minimum required version in our ${link(
     'forums announcement',
     'https://answers.netlify.com/t/build-plugins-dropping-support-for-node-js-12/79421',
   )}`,
-      )
-
-      return { ...pluginOptions, nodePath: execPath, nodeVersion: currentNodeVersion }
-    }
-
-    if (
-      featureFlags.build_warn_upcoming_system_version_change &&
-      !semver.satisfies(userNodeVersion, UPCOMING_MINIMUM_REQUIRED_NODE_VERSION)
-    ) {
-      logWarningSubHeader(
-        logs,
-        `Warning: Starting January 30, 2024 plugin "${packageName}" will be executed with Node.js version 20.`,
-      )
-      logWarning(
-        logs,
-        `  We're upgrading our system node version on that day, which means the plugin cannot be executed with your defined Node.js version ${userNodeVersion}.
-        
-  Please make sure your plugin supports being run on Node.js 20.
-  
-  Read more about our minimum required version in our ${link(
-    'forums announcement',
-    'https://answers.netlify.com/t/build-plugin-update-system-node-js-version-upgrade-to-20/108633',
-  )}`,
-      )
-    }
+    )
   }
 
-  return { ...pluginOptions, nodePath, nodeVersion: userNodeVersion }
+  return (loadedFrom === 'local' || loadedFrom === 'package.json') &&
+    semver.satisfies(userNodeVersion, MINIMUM_REQUIRED_NODE_VERSION)
+    ? { ...pluginOptions, nodePath, nodeVersion: userNodeVersion }
+    : { ...pluginOptions, nodePath: execPath, nodeVersion: currentNodeVersion }
 }
