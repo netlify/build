9e23483ea4943552ad4fe828ea6ec134023c94a7
JGAntunes <joao@netlify.com>
2023-12-27 13:25:54 +0000

    fix: workaround for tinybench support drop for node 14

--- packages/redirect-parser/tests/merge.bench.ts
+++ packages/redirect-parser/tests/merge.bench.ts
@@ -2,21 +2,27 @@ import { expect, bench } from 'vitest'
 
 import { parseRedirects } from './helpers/main.js'
 
-bench('Merges large _redirects file with config redirects', async () => {
-  const configRedirects = [
-    {
-      from: '/base/some-1',
-      to: '/base/another-1',
-    },
-    {
-      from: 'from-unique-redirect',
-      to: 'to-unique-redirect',
-    },
-  ]
-  const { errors, redirects } = await parseRedirects({
-    redirectsFiles: ['large_45000_redirects_file'],
-    configRedirects,
+// Tinybench dropped support for Node 14 recently
+// https://github.com/tinylibs/tinybench/pull/40 and
+// https://github.com/tinylibs/tinybench/pull/43
+// This is just a quick workaround to not run the bench tests in node 14 while we still support it
+if (!process.version.startsWith('14')) {
+  bench('Merges large _redirects file with config redirects', async () => {
+    const configRedirects = [
+      {
+        from: '/base/some-1',
+        to: '/base/another-1',
+      },
+      {
+        from: 'from-unique-redirect',
+        to: 'to-unique-redirect',
+      },
+    ]
+    const { errors, redirects } = await parseRedirects({
+      redirectsFiles: ['large_45000_redirects_file'],
+      configRedirects,
+    })
+    expect(errors).toHaveLength(0)
+    expect(redirects).length.to.be.greaterThan(0)
   })
-  expect(errors).toHaveLength(0)
-  expect(redirects).length.to.be.greaterThan(0)
-})
+}
