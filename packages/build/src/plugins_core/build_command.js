797c4e51ba094d444ac0918d8979df042cb613f6
Lukas Holzer <lukas.holzer@netlify.com>
2024-03-26 14:51:47 +0100

    chore: update

--- packages/build/src/plugins_core/build_command.js
+++ packages/build/src/plugins_core/build_command.js
@@ -22,44 +22,39 @@ const coreStep = async function ({
     build: { command: buildCommand, commandOrigin: buildCommandOrigin },
   },
 }) {
-  return tracer.withActiveSpan(
-    'build.command',
-    {
-      attributes: {
-        'build.command': buildCommand,
-        'build.command.origin': buildCommandOrigin,
-        'build.cwd': buildDir,
-      },
-    },
-    async () => {
-      logBuildCommandStart(logs, buildCommand)
+  return tracer.withActiveSpan('build.command', async (span) => {
+    span.setAttributes({
+      'build.command': buildCommand,
+      'build.command.origin': buildCommandOrigin,
+      'build.cwd': buildDir,
+    })
+    logBuildCommandStart(logs, buildCommand)
 
-      const stdio = getBuildCommandStdio(logs)
-      const childProcess = execa(buildCommand, {
-        shell: SHELL,
-        cwd: buildDir,
-        preferLocal: true,
-        execPath: nodePath,
-        env: childEnv,
-        extendEnv: false,
-        stdio,
-      })
+    const stdio = getBuildCommandStdio(logs)
+    const childProcess = execa(buildCommand, {
+      shell: SHELL,
+      cwd: buildDir,
+      preferLocal: true,
+      execPath: nodePath,
+      env: childEnv,
+      extendEnv: false,
+      stdio,
+    })
 
-      try {
-        const buildCommandOutput = await childProcess
-        handleBuildCommandOutput(buildCommandOutput, logs)
-        return {}
-      } catch (error) {
-        // In our test environment we use `stdio: 'pipe'` on the build command, meaning our `stdout/stderr` output are
-        // buffered and consequently added to `error.message`. To avoid this and end up with duplicated output in our
-        // logs/snapshots we need to rely on `error.shortMessage`.
-        error.message = error.shortMessage
-        handleBuildCommandOutput(error, logs)
-        addErrorInfo(error, { type: 'buildCommand', location: { buildCommand, buildCommandOrigin, configPath } })
-        throw error
-      }
-    },
-  )
+    try {
+      const buildCommandOutput = await childProcess
+      handleBuildCommandOutput(buildCommandOutput, logs)
+      return {}
+    } catch (error) {
+      // In our test environment we use `stdio: 'pipe'` on the build command, meaning our `stdout/stderr` output are
+      // buffered and consequently added to `error.message`. To avoid this and end up with duplicated output in our
+      // logs/snapshots we need to rely on `error.shortMessage`.
+      error.message = error.shortMessage
+      handleBuildCommandOutput(error, logs)
+      addErrorInfo(error, { type: 'buildCommand', location: { buildCommand, buildCommandOrigin, configPath } })
+      throw error
+    }
+  })
 }
 
 // We use Bash on Unix and `cmd.exe` on Windows
