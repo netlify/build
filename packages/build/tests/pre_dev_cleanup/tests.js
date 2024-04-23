a0d07ed5dbc895b965098bab709d706e1b9240f4
Mateusz Bocian <mateusz.bocian@netlify.com>
2024-04-23 11:06:35 -0400

    test: fix pre-dev-cleanup tests

--- packages/build/tests/pre_dev_cleanup/tests.js
+++ packages/build/tests/pre_dev_cleanup/tests.js
@@ -1,49 +1,39 @@
 import { access } from 'node:fs/promises'
-import { join } from 'path'
+import { join, sep } from 'path'
 
 import { Fixture } from '@netlify/testing'
 import test from 'ava'
 
 test('Build removes blobs directory before starting', async (t) => {
-  const fixture = await new Fixture('./fixtures/with_preexisting_blobs').withCopyRoot({ git: false })
+  const fixture = await new Fixture('./fixtures/with_preexisting_blobs')
+    .withFlags({ debug: false, timeline: 'dev' })
+    .withCopyRoot()
 
   const blobsDir = join(fixture.repositoryRoot, '.netlify', 'deploy', 'v1', 'blobs', 'deploy')
 
   await t.notThrowsAsync(access(blobsDir))
 
-  const {
-    success,
-    logs: { stdout },
-  } = await fixture
-    .withFlags({
-      cwd: fixture.repositoryRoot,
-    })
-    .runBuildProgrammatic()
+  const output = await fixture.runDev(() => {})
 
-  t.true(success)
-  t.true(stdout.join('\n').includes('Cleaning up leftover files from previous builds'))
+  t.true(output.includes('Cleaning up leftover files from previous builds'))
+  t.true(output.includes(`Cleaned up .netlify${sep}deploy${sep}v1${sep}blobs${sep}deploy`))
 
   await t.throwsAsync(access(blobsDir))
 })
 
 test('Build removes legacy blobs directory before starting', async (t) => {
-  const fixture = await new Fixture('./fixtures/with_preexisting_legacy_blobs').withCopyRoot({ git: false })
+  const fixture = await new Fixture('./fixtures/with_preexisting_legacy_blobs')
+    .withFlags({ debug: false, timeline: 'dev' })
+    .withCopyRoot()
 
   const blobsDir = join(fixture.repositoryRoot, '.netlify', 'blobs', 'deploy')
 
   await t.notThrowsAsync(access(blobsDir))
 
-  const {
-    success,
-    logs: { stdout },
-  } = await fixture
-    .withFlags({
-      cwd: fixture.repositoryRoot,
-    })
-    .runBuildProgrammatic()
+  const output = await fixture.runDev(() => {})
 
-  t.true(success)
-  t.true(stdout.join('\n').includes('Cleaning up leftover files from previous builds'))
+  t.true(output.includes('Cleaning up leftover files from previous builds'))
+  t.true(output.includes(`Cleaned up .netlify${sep}blobs${sep}deploy`))
 
   await t.throwsAsync(access(blobsDir))
 })
