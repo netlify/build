ee659fb79d49c4be8a0380df4aba850767bb52ca
Nathan Houle <nathan@nathanhoule.com>
2024-09-18 08:49:40 -0700

    test: add regression test for symlink bug

--- packages/zip-it-and-ship-it/tests/symlinked_included_files.test.ts
+++ packages/zip-it-and-ship-it/tests/symlinked_included_files.test.ts
@@ -1,6 +1,6 @@
 import { readdir } from 'fs/promises'
 import { platform } from 'os'
-import { join } from 'path'
+import { join, sep } from 'path'
 
 import decompress from 'decompress'
 import { dir as getTmpDir } from 'tmp-promise'
@@ -66,6 +66,66 @@ test.skipIf(platform() === 'win32')('Symlinked directories from `includedFiles` 
     'function.mjs': false,
     [join('node_modules/.pnpm/crazy-dep/package.json')]: false,
     [join('node_modules/crazy-dep')]: true,
+  })
+})
+
+// Regression test for https://github.com/netlify/build/pull/5836
+test('preserves multiple symlinks that link to the same target', async () => {
+  const { path: tmpDir } = await getTmpDir({ prefix: 'zip-it-test' })
+  const basePath = join(FIXTURES_ESM_DIR, 'symlinked-deps')
+  const mainFile = join(basePath, 'function.mjs')
+
+  // Two symlinks that point at `node_modules/.pnpm/is-odd@1.0.0/node_modules/is-odd`:
+  //
+  // - `node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-odd`
+  // - `node_modules/.pnpm/is-even@1.0.0/node_modules/is-odd`
+  expect(await readDirWithType(basePath)).toEqual({
+    'function.mjs': false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even-or-odd/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even-or-odd/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-odd'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-even/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-even/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-odd'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-odd@1.0.0/node_modules/is-odd/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-odd@1.0.0/node_modules/is-odd/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/is-even-or-odd'.replace(/\//g, sep)]: true,
+  })
+
+  await zipFunction(mainFile, tmpDir, {
+    archiveFormat: ARCHIVE_FORMAT.NONE,
+    basePath,
+    config: {
+      '*': {
+        includedFiles: ['**'],
+      },
+    },
+    featureFlags: {
+      zisi_fix_symlinks: true,
+    },
+    repositoryRoot: basePath,
+    systemLog: console.log,
+    debug: true,
+    internalSrcFolder: undefined,
+  })
+
+  // Test to be sure we've made both symlinks, not just one of them
+  expect(await readDirWithType(join(tmpDir, 'function'))).toEqual({
+    '___netlify-bootstrap.mjs': false,
+    '___netlify-entry-point.mjs': false,
+    '___netlify-telemetry.mjs': false,
+    'function.mjs': false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even-or-odd/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-even-or-odd/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even-or-odd@1.0.0/node_modules/is-odd'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-even/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-even/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-even@1.0.0/node_modules/is-odd'.replace(/\//g, sep)]: true,
+    ['node_modules/.pnpm/is-odd@1.0.0/node_modules/is-odd/index.js'.replace(/\//g, sep)]: false,
+    ['node_modules/.pnpm/is-odd@1.0.0/node_modules/is-odd/package.json'.replace(/\//g, sep)]: false,
+    ['node_modules/is-even-or-odd'.replace(/\//g, sep)]: true,
   })
 })
 
