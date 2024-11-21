822299c7f44c82e6f86bb7e129ebee400f113c56
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-21 11:10:46 -0800

    feat: add tracking for netlify extensions

--- packages/build/src/plugins/internal.ts
+++ packages/build/src/plugins/internal.ts
@@ -8,5 +8,27 @@ const NETLIFY_MAINTAINED_PLUGINS = new Set([
   'netlify_plugin_gatsby',
 ])
 
+const NETLIFY_MAINTAINED_EXTENSIONS = new Set([
+  'content-security-policy-buildhooks',
+  'contentful-buildhooks',
+  'planetscale-buildhooks',
+  'okta-buildhooks',
+  'wordpress-content-buildhooks',
+  'agility-content-buildhooks',
+  'shopify-content-buildhooks',
+  'aem-content-buildhooks',
+  'drupal-content-buildhooks',
+  'supabase-buildhooks',
+  'contentstack-content-buildhooks',
+  'async-workloads-buildhooks',
+  'turso-buildhooks',
+  'lambda-test-buildhooks',
+  'bluesky-custom-domain-buildhooks',
+  'launchdarkly-buildhooks',
+  'sfcc-content-buildhooks',
+  'commercetools-content-buildhooks',
+  'optimizely-edge-delivery-buildhooks',
+])
+
 export const isNetlifyMaintainedPlugin = (pluginPackage: string): boolean =>
-  NETLIFY_MAINTAINED_PLUGINS.has(pluginPackage)
+  NETLIFY_MAINTAINED_PLUGINS.has(pluginPackage) || NETLIFY_MAINTAINED_EXTENSIONS.has(pluginPackage)
