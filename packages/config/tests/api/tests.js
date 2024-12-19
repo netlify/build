df26c5902813cc5022aa8a165bfee8cbf4039b83
YujohnNattrass <46704329+YujohnNattrass@users.noreply.github.com>
2024-12-19 11:12:17 -0800

    Revert "fix: fail build when extension failed to be retrieved (#5915)" (#5954)
    This reverts commit 20c73593deda6fc4a570fc7b71178687a693e03d.

--- packages/config/tests/api/tests.js
+++ packages/config/tests/api/tests.js
@@ -37,12 +37,6 @@ const TEAM_INSTALLATIONS_META_RESPONSE = {
   ],
 }
 
-const TEAM_INSTALLATIONS_META_RESPONSE_ERROR = {
-  path: '/team/account1/integrations/installations/meta/test',
-  response: { error: 'Internal Server Error' },
-  status: 500,
-}
-
 const SITE_INTEGRATIONS_EMPTY_RESPONSE = {
   path: '/site/test/integrations/safe',
   response: [],
@@ -417,22 +411,6 @@ test('Integrations are returned if accountId is present and mode is dev', async 
   t.assert(config.integrations[0].slug === 'test')
   t.assert(config.integrations[0].version === 'so-cool-v2')
   t.assert(config.integrations[0].has_build === true)
-})
-
-test('Integrations teamintegrationsInstallationsMeta API error', async (t) => {
-  const { output } = await new Fixture('./fixtures/base')
-    .withFlags({
-      siteId: 'test',
-      mode: 'buildbot',
-      token: 'test',
-      accountId: 'account1',
-      featureFlags: {
-        cli_integration_installations_meta: true,
-      },
-    })
-    .runConfigServer([SITE_INFO_DATA, TEAM_INSTALLATIONS_META_RESPONSE_ERROR, FETCH_INTEGRATIONS_EMPTY_RESPONSE])
-
-  t.snapshot(normalizeOutput(output))
 })
 
 test('baseRelDir is true if build.base is overridden', async (t) => {
