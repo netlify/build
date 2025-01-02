314ce08201383982f5bbefe196b6d2f465a93bcc
Mateusz Bocian <mrstork@users.noreply.github.com>
2025-01-02 10:18:33 -0500

    test: rebalance test suite (#5972)
    * test: glob no longer in use by test suite
    
    * test: remove manual balancing of tests
    
    * chore: remove tools package
    
    * test: match workflow parallelism count to matrix machine count
    
    * chore: npm audit fix
    
    * chore: update documentation around test balancing

--- packages/build/ava.config.js
+++ packages/build/ava.config.js
@@ -2,7 +2,7 @@ import baseConfig from '../../ava.base.js'
 
 const config = {
   ...baseConfig,
-  files: ['tests/*.{cjs,mjs,js}', 'tests/**/tests.{cjs,mjs,js}'],
+  files: ['tests/**/tests.{cjs,mjs,js}'],
 }
 
 export default config
