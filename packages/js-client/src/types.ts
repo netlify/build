a225a5db98e96c2712dfcc09c812e4e2c808a600
Caleb Barnes <caleb.barnes@netlify.com>
2025-01-09 15:01:41 -0800

    feat(js-client): handle snake_case and camelCase params in type definitions
    Since the API client allows using camelCased params we need to allow
    these to keep backwards compatibility.

--- packages/js-client/src/types.ts
+++ packages/js-client/src/types.ts
@@ -1,18 +1,42 @@
 import type { operations } from '@netlify/open-api'
 
+/**
+ * Converts snake_case to camelCase for TypeScript types.
+ */
+type CamelCase<S extends string> = S extends `${infer T}_${infer U}` ? `${T}${Capitalize<CamelCase<U>>}` : S
+
+/**
+ * Creates a union of both snake_case and camelCase keys with their respective types.
+ */
+type SnakeToCamel<T> = {
+  [K in keyof T as CamelCase<K & string>]: T[K]
+}
+
+/**
+ * Combines snake_case and camelCase parameters.
+ */
+type CombinedCaseParams<T> = SnakeToCamel<T> | T
+
+/**
+ * Combines `path` and `query` parameters into a single type.
+ */
 type OperationParams<K extends keyof operations> = 'parameters' extends keyof operations[K]
   ? 'path' extends keyof operations[K]['parameters']
     ? 'query' extends keyof operations[K]['parameters']
-      ? Omit<operations[K]['parameters']['path'], keyof operations[K]['parameters']['query']> & // Combine `path` and `query`
-          operations[K]['parameters']['query']
-      : operations[K]['parameters']['path'] // If no `query`, only `path`
+      ? CombinedCaseParams<
+          Omit<operations[K]['parameters']['path'], keyof operations[K]['parameters']['query']> &
+            operations[K]['parameters']['query']
+        >
+      : CombinedCaseParams<operations[K]['parameters']['path']>
     : 'query' extends keyof operations[K]['parameters']
-      ? operations[K]['parameters']['query'] // If no `path`, only `query`
-      : undefined // No `parameters`
+      ? CombinedCaseParams<operations[K]['parameters']['query']>
+      : undefined
   : undefined
 
 type SuccessHttpStatusCodes = 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226
-
+/**
+ * Extracts the response type from the operation.
+ */
 type OperationResponse<K extends keyof operations> = 'responses' extends keyof operations[K]
   ? SuccessHttpStatusCodes extends infer StatusKeys
     ? StatusKeys extends keyof operations[K]['responses']
