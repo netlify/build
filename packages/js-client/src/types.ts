189aa98c53a5e999b14b0c5d817c9ab5361a63a0
Caleb Barnes <caleb.barnes@netlify.com>
2025-01-09 14:32:00 -0800

    feat(js-client): treat path params and query params as one object
    The api client methods will assign these to the correct places but just
    accept the params as a single object.

--- packages/js-client/src/types.ts
+++ packages/js-client/src/types.ts
@@ -1,8 +1,16 @@
 import type { operations } from '@netlify/open-api'
 
 type OperationParams<K extends keyof operations> = 'parameters' extends keyof operations[K]
-  ? operations[K]['parameters']
+  ? 'path' extends keyof operations[K]['parameters']
+    ? 'query' extends keyof operations[K]['parameters']
+      ? Omit<operations[K]['parameters']['path'], keyof operations[K]['parameters']['query']> & // Combine `path` and `query`
+          operations[K]['parameters']['query']
+      : operations[K]['parameters']['path'] // If no `query`, only `path`
+    : 'query' extends keyof operations[K]['parameters']
+      ? operations[K]['parameters']['query'] // If no `path`, only `query`
+      : undefined // No `parameters`
   : undefined
+
 type SuccessHttpStatusCodes = 200 | 201 | 202 | 203 | 204 | 205 | 206 | 207 | 208 | 226
 
 type OperationResponse<K extends keyof operations> = 'responses' extends keyof operations[K]
