c9a31051daf63e9d43695ca8cac99c2f6dd2d9eb
Mateusz Bocian <mrstork@users.noreply.github.com>
2025-02-26 18:42:04 -0500

    test: add regression test for plugin resolutions in monorepos

--- packages/build/tests/plugins/tests.js
+++ packages/build/tests/plugins/tests.js
@@ -86,6 +86,12 @@ test('Resolution is relative to the build directory', async (t) => {
   t.snapshot(normalizeOutput(output))
 })
 
+test('Resolution respects monorepo node module resolution rules', async (t) => {
+  const fixture = await new Fixture('./fixtures/monorepo')
+  const output = await fixture.withFlags({ packagePath: 'apps/unpinned' }).runWithBuild()
+  t.assert(output.indexOf('@8.5.3') > 0)
+})
+
 test('Non-existing plugins', async (t) => {
   const output = await new Fixture('./fixtures/non_existing').runWithBuild()
   t.snapshot(normalizeOutput(output))
