6f0e94e18f8bd3059c3ca655950afcfe1e553521
jake champion <jake.champion@netlify.com>
2025-03-26 08:12:25 +0000

    chore: remove ansi escape sequences from the snapshot output

--- packages/edge-bundler/node/bundler.test.ts
+++ packages/edge-bundler/node/bundler.test.ts
@@ -106,10 +106,10 @@ test('Adds a custom error property to user errors during bundling', async () => 
     expect(error).toBeInstanceOf(BundleError)
     const messageBeforeStack = (error as BundleError).message
     expect(
-      messageBeforeStack.replace(
-        /file:\/\/\/(.*?\/)(build\/packages\/edge-bundler\/deno\/vendor\/deno\.land\/x\/eszip.*)/,
-        'file://$2',
-      ),
+      messageBeforeStack
+        .replace(/file:\/\/\/(.*?\/)(build\/packages\/edge-bundler\/deno\/vendor\/deno\.land\/x\/eszip.*)/, 'file://$2')
+        // eslint-disable-next-line no-control-regex
+        .replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, ''),
     ).toMatchSnapshot()
     expect((error as BundleError).customErrorInfo).toEqual({
       location: {
