bbe93132d12b11afd98dfca25b8339004d99a2b2
Jake Champion <me@jakechampion.name>
2025-04-10 12:50:22 +0100

    fix(edge-bundler): only parse a file once for it's npm specifiers (#6196)

--- packages/edge-bundler/node/bundler.test.ts
+++ packages/edge-bundler/node/bundler.test.ts
@@ -533,6 +533,33 @@ test('Loads npm modules which use package.json.exports', async () => {
   await rm(vendorDirectory.path, { force: true, recursive: true })
 })
 
+test('Loads modules which contain cycles', async () => {
+  const { basePath, cleanup, distPath } = await useFixture('imports_cycle')
+  const sourceDirectory = join(basePath, 'functions')
+  const declarations: Declaration[] = [
+    {
+      function: 'func1',
+      path: '/func1',
+    },
+  ]
+  const vendorDirectory = await tmp.dir()
+
+  await bundle([sourceDirectory], distPath, declarations, {
+    basePath,
+    vendorDirectory: vendorDirectory.path,
+  })
+
+  const manifestFile = await readFile(resolve(distPath, 'manifest.json'), 'utf8')
+  const manifest = JSON.parse(manifestFile)
+  const bundlePath = join(distPath, manifest.bundles[0].asset)
+  const { func1 } = await runESZIP(bundlePath, vendorDirectory.path)
+
+  expect(func1).toBe('magix')
+
+  await cleanup()
+  await rm(vendorDirectory.path, { force: true, recursive: true })
+})
+
 test('Loads npm modules in a monorepo setup', async () => {
   const systemLogger = vi.fn()
   const { basePath: rootPath, cleanup, distPath } = await useFixture('monorepo_npm_module')
