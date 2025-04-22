64458467e39d519a701a307793d1c24fc1a2fe4a
Suzanne Aitchison <suzanne.aitchison@netlify.com>
2025-04-22 14:01:12 +0100

    feat: log more info

--- packages/build/src/status/validations.ts
+++ packages/build/src/status/validations.ts
@@ -18,7 +18,7 @@ export const reportValidations = async function ({
   try {
     // @ts-expect-error Property 'updateDeployValidations' does not exist on type 'DynamicMethods'. This is a private/internal-only method and isn't generated in the type definitions.
     await api.updateDeployValidations({ deploy_id: deployId, body: { secrets_scan: secretScanResult } })
-  } catch {
-    systemLog('Unable to report secrets scanning results to API')
+  } catch (e) {
+    systemLog(`Unable to report secrets scanning results to API. Deploy id: ${deployId}`, e)
   }
 }
