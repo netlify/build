0b0cf3779d61ddf745ba0c8d7089b5cfcfd8bfe7
Animesh <69345507+n-ce@users.noreply.github.com>
2025-05-16 06:05:44 +0530

    use globally available RequestInit interface

--- packages/js-client/src/types.ts
+++ packages/js-client/src/types.ts
@@ -1,7 +1,6 @@
 import type { ReadStream } from 'node:fs'
 
 import type { operations } from '@netlify/open-api'
-import type { RequestInit } from '@types/node'
 
 /**
  * Determines whether all keys in T are optional.
