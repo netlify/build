209799118e70d15248c3e31de6c63506a8f1bcc4
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-06-04 17:43:54 -0700

    chore(deps): switch from fast-glob to tinyglobby

--- packages/zip-it-and-ship-it/src/utils/matching.ts
+++ packages/zip-it-and-ship-it/src/utils/matching.ts
@@ -1,15 +1,16 @@
-import originalGlob from 'fast-glob'
 import { minimatch as minimatchFunction, type MinimatchOptions } from 'minimatch'
 import normalizePath from 'normalize-path'
+import { glob as originalGlob, type GlobOptions } from 'tinyglobby'
 
 /**
  * Both glob and minimatch only support unix style slashes in patterns
  * For this reason we wrap them and ensure all patterns are always unixified
  * We use `normalize-path` here instead of `unixify` because we do not want to remove drive letters
  */
-export const glob = function (pattern: string, options: originalGlob.Options): Promise<string[]> {
-  const normalizedIgnore = options.ignore?.map((expression) => normalizePath(expression))
-  return originalGlob(normalizePath(pattern), { ...options, ignore: normalizedIgnore })
+export const glob = function (pattern: string, options: GlobOptions): Promise<string[]> {
+  const ignore: string[] = Array.isArray(options.ignore) ? options.ignore : options.ignore ? [options.ignore] : []
+  const normalizedIgnore = ignore.map((expression) => normalizePath(expression))
+  return originalGlob(normalizePath(pattern), { ...options, ignore: normalizedIgnore, expandDirectories: false })
 }
 
 export const minimatch = function (target: string, pattern: string, options?: MinimatchOptions): boolean {
