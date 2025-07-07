71327011592a5e8c7169815801858a688fd8a6b0
Karin <=>
2025-07-07 14:15:35 +0200

    fix: use readfile to read package.json

--- packages/build/src/utils/json.ts
+++ packages/build/src/utils/json.ts
@@ -1,18 +1,19 @@
+import { readFile } from 'fs/promises'
 import { fileURLToPath } from 'url'
 
 import type { PackageJson } from 'read-package-up'
-
 // We know how our package.json looks like, so we can be very specific with the type
 // and only add the properties we want to use
 export type RootPackageJson = { name: string; version: string }
 
 const ROOT_PACKAGE_JSON_PATH = fileURLToPath(new URL('../../package.json', import.meta.url))
 
+// TODO: Replace with dynamic `import()` once it is supported without
+// experimental flags
 export const importJsonFile = async function (filePath: string): Promise<PackageJson> {
-  const fileUrl = filePath.startsWith('file://') ? filePath : `file://${filePath}`
+  const fileContents = await readFile(filePath, 'utf-8')
 
-  const module = (await import(fileUrl, { assert: { type: 'json' } })) as { default: PackageJson }
-  return module.default
+  return JSON.parse(fileContents) as PackageJson
 }
 
 export const ROOT_PACKAGE_JSON = (await importJsonFile(ROOT_PACKAGE_JSON_PATH)) as RootPackageJson
