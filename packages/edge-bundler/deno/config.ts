7b56cccfad27f0f7632dc6d66dcfa40ca01f017a
Eduardo Bouças <mail@eduardoboucas.com>
2025-08-07 11:26:39 +0100

    fix: use recent version of EF bootstrap

--- packages/edge-bundler/deno/config.ts
+++ packages/edge-bundler/deno/config.ts
@@ -1,5 +1,5 @@
 // this needs to be updated whenever there's a change to globalThis.Netlify in bootstrap
-import { Netlify } from "https://64e8753eae24930008fac6d9--edge.netlify.app/bootstrap/index-combined.ts"
+import { Netlify } from "https://68945e2afae503000862a504--edge.netlify.app/bootstrap/index-combined.ts"
 
 const [functionURL, collectorURL, rawExitCodes] = Deno.args
 const exitCodes = JSON.parse(rawExitCodes)
