6779ef06ba5380fe72602d46bcf6fe68c8366477
jake champion <jake.champion@netlify.com>
2025-10-20 13:25:58 +0100

    fix: add dry-run feature flag for edge-function tarball generation
    adding a new feature flag for a dry-run mode for tarball bundling of edge-functions. this will allow us to ensure that the new bunding system functionality is not throwing errors before we look to turn it on

--- packages/edge-bundler/node/bridge.ts
+++ packages/edge-bundler/node/bridge.ts
@@ -69,9 +69,16 @@ export class DenoBridge {
     this.onAfterDownload = options.onAfterDownload
     this.onBeforeDownload = options.onBeforeDownload
     this.useGlobal = options.useGlobal ?? true
-    this.versionRange =
-      options.versionRange ??
-      (options.featureFlags?.edge_bundler_generate_tarball ? NEXT_DENO_VERSION_RANGE : DENO_VERSION_RANGE)
+    if (options.versionRange) {
+      this.versionRange = options.versionRange
+    } else if (
+      options.featureFlags?.edge_bundler_dry_run_generate_tarball ||
+      options.featureFlags?.edge_bundler_generate_tarball
+    ) {
+      this.versionRange = NEXT_DENO_VERSION_RANGE
+    } else {
+      this.versionRange = DENO_VERSION_RANGE
+    }
   }
 
   private async downloadBinary() {
