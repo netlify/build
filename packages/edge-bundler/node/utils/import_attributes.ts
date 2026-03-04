a7f56dc97a32c4a7941637df64a144949c70b5e0
Mateusz Bocian <mrstork@users.noreply.github.com>
2026-03-04 10:17:54 -0500

    fix: handle jsx parsing in acorn (#6960)

--- packages/edge-bundler/node/utils/import_attributes.ts
+++ packages/edge-bundler/node/utils/import_attributes.ts
@@ -1,8 +1,8 @@
-import { Parser } from 'acorn'
-import * as walk from 'acorn-walk'
+import { Parser, Node } from 'acorn'
+import type { ExportAllDeclaration, ExportNamedDeclaration, ImportDeclaration, ImportExpression } from 'acorn'
 import { tsPlugin } from '@sveltejs/acorn-typescript'
 
-const acorn = Parser.extend(tsPlugin())
+const acorn = Parser.extend(tsPlugin({ jsx: true }))
 
 /**
  * Given source code rewrites import assert into import with
@@ -18,38 +18,70 @@ export function rewriteSourceImportAssertions(source: string): string {
     const parsedAST = acorn.parse(source, {
       ecmaVersion: 'latest',
       sourceType: 'module',
+      locations: true,
     })
 
-    walk.simple(parsedAST, {
-      ImportDeclaration(node) {
-        const statement = source.slice(node.source.end, node.end)
-        if (statement.includes('assert')) {
-          const newStatement = statement.replace('assert', 'with')
-          modified = modified.replace(statement, newStatement)
-        }
-      },
-      ImportExpression(node) {
-        const statement = source.slice(node.source.end, node.end)
-        if (statement.includes('assert')) {
-          const newStatement = statement.replace('assert', 'with')
-          modified = modified.replace(statement, newStatement)
-        }
-      },
-      ExportNamedDeclaration(node) {
-        if (!node.source) return
-        const statement = source.slice(node.source.end, node.end)
-        if (statement.includes('assert')) {
-          const newStatement = statement.replace('assert', 'with')
-          modified = modified.replace(statement, newStatement)
-        }
-      },
-    })
+    const statements = collectImportAssertions(source, parsedAST.body)
+
+    // Bulk replacement of import assertions
+    for (const statement of statements.sort((a, b) => b.start - a.start)) {
+      modified = `${modified.slice(0, statement.start)}${statement.text}${modified.slice(statement.end)}`
+    }
 
     return modified
   } catch (error) {
     if (!modified.includes('assert')) {
       return modified
     }
+
     throw error
+  }
+}
+
+type StatementsWithAssertions = ImportDeclaration | ImportExpression | ExportAllDeclaration | ExportNamedDeclaration
+type ImportReplacement = { start: number; end: number; text: string }
+
+function collectImportAssertions(source: string, node: Node | Node[]): ImportReplacement[] {
+  const collectedNodes: ImportReplacement[] = []
+
+  if (Array.isArray(node)) {
+    return node.flatMap((n) => collectImportAssertions(source, n))
+  }
+
+  // Capture all import assertion statements
+  const assertionNodeTypes = ['ImportDeclaration', 'ImportExpression', 'ExportAllDeclaration', 'ExportNamedDeclaration']
+  if (assertionNodeTypes.includes(node.type)) {
+    const parsedImportNode = parseImportAssertion(source, node as StatementsWithAssertions)
+    if (parsedImportNode !== undefined) {
+      return [parsedImportNode]
+    }
+  }
+
+  // Fallthrough tree traversal + support for dynamic imports and JSX syntax
+  for (const [key, value] of Object.entries(node)) {
+    if (['loc', 'typeName'].includes(key)) continue
+    if (value === null) continue
+    if (typeof value === 'object') {
+      const childNodes = collectImportAssertions(source, value as Node | Node[])
+      collectedNodes.push(...childNodes)
+    }
+  }
+
+  return collectedNodes
+}
+
+function parseImportAssertion(
+  source: string,
+  node: ImportDeclaration | ImportExpression | ExportAllDeclaration | ExportNamedDeclaration,
+): ImportReplacement | undefined {
+  if (!node.source) return undefined
+
+  const statement = source.slice(node.source.end, node.end)
+  if (!statement.includes('assert')) return undefined
+
+  return {
+    start: node.source.end,
+    end: node.end,
+    text: statement.replace('assert', 'with'),
   }
 }
