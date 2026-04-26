cf7cd12c2785ce6942b33b5be612d6b650a04401
pieh <misiek.piechowiak@gmail.com>
2026-04-26 09:07:04 +0200

    fix: cleanup dist migration files

--- packages/build/src/plugins_core/pre_cleanup/index.ts
+++ packages/build/src/plugins_core/pre_cleanup/index.ts
@@ -5,8 +5,12 @@ import { getBlobsDirs } from '../../utils/blobs.js'
 import { FRAMEWORKS_API_PATH } from '../../utils/frameworks_api.js'
 import { CoreStep, CoreStepFunction } from '../types.js'
 
-const coreStep: CoreStepFunction = async ({ buildDir, packagePath }) => {
-  const paths = [...getBlobsDirs(buildDir, packagePath), resolve(buildDir, packagePath || '', FRAMEWORKS_API_PATH)]
+const coreStep: CoreStepFunction = async ({ buildDir, constants, packagePath }) => {
+  const paths = [
+    ...getBlobsDirs(buildDir, packagePath),
+    resolve(buildDir, packagePath || '', FRAMEWORKS_API_PATH),
+    constants.DB_MIGRATIONS_DIST ? resolve(buildDir, constants.DB_MIGRATIONS_DIST) : undefined,
+  ].filter((path): path is string => Boolean(path))
 
   try {
     await Promise.all(paths.map((dir) => rm(dir, { recursive: true, force: true })))
