3e968311f9b3c4e4e34ab26ade5212ad786a419e
Hrishikesh Kokate <hrishi5200@gmail.com>
2026-09-01 16:02:43 +0530

    pin nft dep to 1.3.1

---
+++ packages/zip-it-and-ship-it/tests/v2_api_env_file.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+
+import { FIXTURES_ESM_DIR, unzipFiles, zipFixture } from './helpers/main.js'
+
+test('includes a `.env` file resolved from `process.cwd()` by a dependency when bundling a v2 function', async () => {
+  const { files } = await zipFixture('v2-api-env-file', { fixtureDir: FIXTURES_ESM_DIR })
+
+  expect(files[0].bundler).toBe('nft')
+  expect(files[0].runtimeAPIVersion).toBe(2)
+
+  const [{ unzipPath }] = await unzipFiles(files)
+
+  await expect(`${unzipPath}/.env`).toPathExist()
+})
