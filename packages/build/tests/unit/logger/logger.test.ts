6b9fba5af1f62b6c36e15c3b46830da1d7daf4db
James Garbutt <43081j@users.noreply.github.com>
2026-09-03 18:24:23 +0100

    test: migrate logger tests to vitest

--- packages/build/tests/unit/logger/logger.test.ts
+++ packages/build/tests/unit/logger/logger.test.ts
@@ -1,15 +1,16 @@
 import { promises as fs } from 'fs'
 
 import { normalizeOutput } from '@netlify/testing'
-import test from 'ava'
 import tmp from 'tmp-promise'
+import { expect, test } from 'vitest'
 
-import { getSystemLogger } from '../../../lib/log/logger.js'
+import { type BufferedLogs, getSystemLogger } from '../../../lib/log/logger.js'
 
-test('System logger writes to file descriptor', async (t) => {
+test('System logger writes to file descriptor', async () => {
   const { fd, cleanup, path } = await tmp.file()
-  const mockProcess = {
+  const mockProcess: BufferedLogs = {
     stdout: [],
+    stderr: [],
   }
   const systemLog = getSystemLogger(mockProcess, false, fd)
   const error = new Error('Something went wrong')
@@ -18,16 +19,17 @@ test('System logger writes to file descriptor', async (t) => {
 
   const output = normalizeOutput(await fs.readFile(path, 'utf8'))
 
-  t.snapshot(normalizeOutput(output))
-  t.is(mockProcess.stdout.length, 0)
+  expect(normalizeOutput(output)).toMatchSnapshot()
+  expect(mockProcess.stdout).toHaveLength(0)
 
   await cleanup()
 })
 
-test('System logger does not write to file descriptor when `debug: true`', async (t) => {
+test('System logger does not write to file descriptor when `debug: true`', async () => {
   const { fd, cleanup, path } = await tmp.file()
-  const mockProcess = {
+  const mockProcess: BufferedLogs = {
     stdout: [],
+    stderr: [],
   }
   const systemLog = getSystemLogger(mockProcess, true, fd)
   const error = new Error('Something went wrong')
@@ -36,9 +38,9 @@ test('System logger does not write to file descriptor when `debug: true`', async
 
   const output = normalizeOutput(mockProcess.stdout[0])
 
-  t.is(mockProcess.stdout.length, 1)
-  t.snapshot(normalizeOutput(output))
-  t.is(await fs.readFile(path, 'utf8'), '')
+  expect(mockProcess.stdout).toHaveLength(1)
+  expect(normalizeOutput(output)).toMatchSnapshot()
+  expect(await fs.readFile(path, 'utf8')).toBe('')
 
   await cleanup()
 })
