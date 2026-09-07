ad0771b13a738451e6a31806d208967e415efa61
James Garbutt <43081j@users.noreply.github.com>
2026-09-07 20:00:30 +0100

    chore: drop ava ESLint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'node:url'
 import { includeIgnoreFile } from '@eslint/compat'
 import eslint from '@eslint/js'
 import vitest from '@vitest/eslint-plugin'
-import ava from 'eslint-plugin-ava'
 import * as importPlugin from 'eslint-plugin-import'
 import node from 'eslint-plugin-n'
 import tseslint from 'typescript-eslint'
@@ -132,13 +131,6 @@ export default tseslint.config(
       'vitest/no-disabled-tests': ['error'],
       'vitest/no-focused-tests': ['error'],
       'vitest/no-commented-out-tests': ['error'],
-    },
-  },
-  {
-    files: ['**/tests.js', '**/*.tests.js'],
-    plugins: { ava },
-    rules: {
-      'ava/no-only-test': 'error',
     },
   },
 
